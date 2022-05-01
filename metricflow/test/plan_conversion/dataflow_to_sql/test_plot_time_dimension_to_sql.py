88eadbbf4ac8608d249256818a08806b990fe747
Paul Yang <paul@transformdata.io>
2022-04-30 18:39:30 -0700

    Add test for using the plot time dimension with the DataflowPlanBuilder.

--- metricflow/test/plan_conversion/dataflow_to_sql/test_plot_time_dimension_to_sql.py
+++ metricflow/test/plan_conversion/dataflow_to_sql/test_plot_time_dimension_to_sql.py
@@ -1,13 +1,16 @@
 from _pytest.fixtures import FixtureRequest
 
+from metricflow.dataflow.builder.dataflow_plan_builder import DataflowPlanBuilder
 from metricflow.dataflow.dataflow_plan import PlotTimeDimensionTransformNode
 from metricflow.dataset.data_source_adapter import DataSourceDataSet
+from metricflow.dataset.dataset import DataSet
 from metricflow.plan_conversion.dataflow_to_sql import DataflowToSqlQueryPlanConverter
 from metricflow.protocols.sql_client import SqlClient
-from metricflow.specs import TimeDimensionReference
+from metricflow.specs import TimeDimensionReference, MetricFlowQuerySpec, MetricSpec, TimeDimensionSpec
 from metricflow.test.fixtures.model_fixtures import ConsistentIdObjectRepository
 from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
 from metricflow.test.plan_conversion.test_dataflow_to_sql_plan import convert_and_check
+from metricflow.time.time_granularity import TimeGranularity
 
 
 def test_plot_time_dimension_transform_node_using_primary_time(  # noqa: D
@@ -50,4 +53,39 @@ def test_plot_time_dimension_transform_node_using_non_primary_time(  # noqa: D
         dataflow_to_sql_converter=dataflow_to_sql_converter,
         sql_client=sql_client,
         node=plot_time_dimension_transform_node,
+    )
+
+
+def test_simple_query_with_plot_time_dimension(
+    request: FixtureRequest,
+    mf_test_session_state: MetricFlowTestSessionState,
+    dataflow_to_sql_converter: DataflowToSqlQueryPlanConverter[DataSourceDataSet],
+    consistent_id_object_repository: ConsistentIdObjectRepository,
+    sql_client: SqlClient,
+    dataflow_plan_builder: DataflowPlanBuilder,
+) -> None:
+    """Tests building a query that uses measures defined from 2 different time dimensions."""
+    dataflow_plan = dataflow_plan_builder.build_plan(
+        MetricFlowQuerySpec(
+            metric_specs=(
+                MetricSpec(element_name="bookings"),
+                MetricSpec(element_name="booking_payments"),
+            ),
+            dimension_specs=(),
+            time_dimension_specs=(
+                TimeDimensionSpec(
+                    element_name=DataSet.plot_time_dimension_name(),
+                    identifier_links=(),
+                    time_granularity=TimeGranularity.DAY,
+                ),
+            ),
+        )
+    )
+
+    convert_and_check(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        dataflow_to_sql_converter=dataflow_to_sql_converter,
+        sql_client=sql_client,
+        node=dataflow_plan.sink_output_node.parent_node,
     )
