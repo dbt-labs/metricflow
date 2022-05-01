8c2c97c1e23fa2bb4f0798e9c8935da06ec79801
Paul Yang <paul@transformdata.io>
2022-04-30 23:46:16 -0700

    Use SourceNodeBuilder to provide inputs to DataflowPlanBuilder.

--- metricflow/dataflow/builder/source_node.py
+++ metricflow/dataflow/builder/source_node.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, List
 
 from metricflow.dataflow.dataflow_plan import ReadSqlSourceNode, PlotTimeDimensionTransformNode, BaseOutput
 from metricflow.dataset.data_source_adapter import DataSourceDataSet
@@ -13,17 +13,22 @@ class SourceNodeBuilder:
 
     def create_from_data_sets(self, data_sets: Sequence[DataSourceDataSet]) -> Sequence[BaseOutput[DataSourceDataSet]]:
         """Creates source nodes from DataSourceDataSets."""
-        source_nodes = []
+        source_nodes: List[BaseOutput[DataSourceDataSet]] = []
         for data_set in data_sets:
             read_node = ReadSqlSourceNode[DataSourceDataSet](data_set)
-            for time_dimension_reference in self._semantic_model.data_source_semantics.get_aggregation_time_dimensions(
+            aggregation_time_dimensions = self._semantic_model.data_source_semantics.get_aggregation_time_dimensions(
                 data_set.data_source_reference
-            ):
-                source_nodes.append(
-                    PlotTimeDimensionTransformNode[DataSourceDataSet](
-                        parent_node=read_node,
-                        aggregation_time_dimension_reference=time_dimension_reference,
+            )
+            # Dimension sources may not have any measures -> no aggregation time dimensions.
+            if len(aggregation_time_dimensions) == 0:
+                source_nodes.append(read_node)
+            else:
+                for time_dimension_reference in aggregation_time_dimensions:
+                    source_nodes.append(
+                        PlotTimeDimensionTransformNode[DataSourceDataSet](
+                            parent_node=read_node,
+                            aggregation_time_dimension_reference=time_dimension_reference,
+                        )
                     )
-                )
 
         return source_nodes
