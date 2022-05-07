dcfc91b8adb4d7f44b10d3ef9214005b7ef2f1db
Paul Yang <paul@transformdata.io>
2022-05-06 20:34:49 -0700

    Split TimeSpineSource into TimeSpineSource and TimeSpineTableBuilder.

--- metricflow/plan_conversion/time_spine.py
+++ metricflow/plan_conversion/time_spine.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import datetime
 import logging
 import threading
+from dataclasses import dataclass
 from typing import List, Tuple
 
 import pandas as pd
@@ -16,44 +17,55 @@ from metricflow.time.time_granularity import TimeGranularity
 logger = logging.getLogger(__name__)
 
 
+@dataclass(frozen=True)
 class TimeSpineSource:
-    """Generates a source table containing all timestamps to use for cumulative metrics."""
+    """Defines a source table containing all timestamps to use for computing cumulative metrics."""
 
-    _SPINE_TABLE_NAME = "mf_time_spine"
-
-    def __init__(self, sql_client: SqlClient, schema_name: str) -> None:  # noqa
-        self._sql_client = sql_client
-        self._schema_name = schema_name
-        self._verified_spine_table_exists = False
-        self._create_table_lock = threading.Lock()
+    schema_name: str
+    table_name: str = "mf_time_spine"
+    # Name of the column in the table that contains the dates.
+    time_column_name: str = "ds"
+    # The time granularity of the dates in the spine table.
+    time_column_granularity: TimeGranularity = TimeGranularity.DAY
 
     @property
     def spine_table(self) -> SqlTable:
         """Table containing all dates"""
-        return SqlTable(schema_name=self._schema_name, table_name=TimeSpineSource._SPINE_TABLE_NAME)
+        return SqlTable(schema_name=self.schema_name, table_name=self.table_name)
+
+
+class TimeSpineTableBuilder:
+    """Helps to build the time spine table based on the definition in a TimeSpineSource."""
+
+    def __init__(  # noqa: D
+        self,
+        time_spine_source: TimeSpineSource,
+        sql_client: SqlClient,
+    ) -> None:
+        self._time_spine_source = time_spine_source
+        self._sql_client = sql_client
+        self._verified_spine_table_exists = False
+        self._create_table_lock = threading.Lock()
 
     @property
-    def time_column_name(self) -> str:
-        """Name of the column"""
-        return "ds"
+    def time_spine_source(self) -> TimeSpineSource:  # noqa: D
+        return self._time_spine_source
 
-    @property
-    def time_column_granularity(self) -> TimeGranularity:  # noqa: D
-        return TimeGranularity.DAY
-
-    def create_if_necessary(self) -> None:
-        """Creates the spine table if necessary."""
+    def create_if_necessary(self) -> None:  # noqa: D
+        """Creates the spine table if it doesn't already exist."""
         logger.info("Waiting to get the lock for the time spine table")
         with self._create_table_lock:
+            spine_table = self.time_spine_source.spine_table
             logger.info("Got the lock for the time spine table")
             if self._verified_spine_table_exists:
+                logger.info(f"Previously verified that the spine table {spine_table.sql} exists.")
                 return
-            logger.info(f"Checking if the spine table {self.spine_table.sql} exists")
-            if self._sql_client.table_exists(self.spine_table):
-                logger.info(f"Spine table {self.spine_table.sql} exists")
+            logger.info(f"Checking if the spine table {spine_table.sql} exists")
+            if self._sql_client.table_exists(spine_table):
+                logger.info(f"Spine table {spine_table.sql} exists")
                 self._verified_spine_table_exists = True
                 return
-            logger.info(f"Spine table {self.spine_table.sql} does not exist")
+            logger.info(f"Spine table {spine_table.sql} does not exist")
             start_date = TimeRangeConstraint.ALL_TIME_BEGIN()
             end_date = TimeRangeConstraint.ALL_TIME_END()
 
@@ -61,6 +73,12 @@ class TimeSpineSource:
             # Using a union type throws a type error for some reason, so going with this approach
             date_spine_table_datetime_data: List[Tuple[datetime.datetime]] = []
             date_spine_table_str_data: List[Tuple[str]] = []
+
+            if self.time_spine_source.time_column_granularity != TimeGranularity.DAY:
+                raise RuntimeError(
+                    f"A time spine source with a granularity {self.time_spine_source.time_column_granularity} is not "
+                    f"yet supported."
+                )
 
             if self._sql_client.sql_engine_attributes.timestamp_type_supported:
                 while current_date <= end_date:
@@ -71,21 +89,21 @@ class TimeSpineSource:
                     date_spine_table_str_data.append((current_date.strftime(ISO8601_PYTHON_FORMAT),))
                     current_date = current_date + datetime.timedelta(days=1)
 
-            self._sql_client.drop_table(self.spine_table)
+            self._sql_client.drop_table(spine_table)
             num_rows = (
                 len(date_spine_table_datetime_data)
                 if date_spine_table_datetime_data
                 else len(date_spine_table_str_data)
             )
 
-            logger.info(f"Creating date spine table {self.spine_table.sql} with {num_rows} rows")
+            logger.info(f"Creating date spine table {spine_table.sql} with {num_rows} rows")
             self._sql_client.create_table_from_dataframe(
-                sql_table=self.spine_table,
+                sql_table=spine_table,
                 df=pd.DataFrame(
-                    columns=[self.time_column_name],
+                    columns=[self._time_spine_source.time_column_name],
                     data=date_spine_table_datetime_data or date_spine_table_str_data,
                 ),
                 chunk_size=1000,
             )
-            logger.info(f"Created date spine table {self.spine_table.sql}")
+            logger.info(f"Created date spine table {spine_table.sql}")
             self._verified_spine_table_exists = True
--- metricflow/test/fixtures/dataflow_fixtures.py
+++ metricflow/test/fixtures/dataflow_fixtures.py
@@ -6,7 +6,7 @@ from metricflow.dataflow.builder.costing import DefaultCostFunction
 from metricflow.dataflow.builder.dataflow_plan_builder import DataflowPlanBuilder
 from metricflow.dataset.data_source_adapter import DataSourceDataSet
 from metricflow.model.semantic_model import SemanticModel
-from metricflow.plan_conversion.time_spine import TimeSpineSource
+from metricflow.plan_conversion.time_spine import TimeSpineSource, TimeSpineTableBuilder
 from metricflow.protocols.sql_client import SqlClient
 from metricflow.test.fixtures.model_fixtures import ConsistentIdObjectRepository
 from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
@@ -65,6 +65,7 @@ def multihop_dataflow_plan_builder(  # noqa: D
 def time_spine_source(  # noqa: D
     sql_client: SqlClient, mf_test_session_state: MetricFlowTestSessionState  # noqa: F811
 ) -> TimeSpineSource:
-    time_spine_source = TimeSpineSource(sql_client=sql_client, schema_name=mf_test_session_state.mf_system_schema)
-    time_spine_source.create_if_necessary()
+    time_spine_source = TimeSpineSource(schema_name=mf_test_session_state.mf_system_schema)
+    time_spine_table_builder = TimeSpineTableBuilder(time_spine_source=time_spine_source, sql_client=sql_client)
+    time_spine_table_builder.create_if_necessary()
     return time_spine_source
