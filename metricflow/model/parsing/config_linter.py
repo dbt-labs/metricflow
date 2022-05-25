1defe982a32abdc21a15c3c1dc8d9bfdd64ddff2
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-05-25 16:15:11 -0700

    Add custom config for yamllint parsing

--- metricflow/model/parsing/config_linter.py
+++ metricflow/model/parsing/config_linter.py
@@ -1,14 +1,49 @@
 import os
 from collections import OrderedDict
-from typing import List
+from typing import List, Optional
+import yaml
 from yamllint import config, linter
 
 from metricflow.model.validations.validator_helpers import ValidationIssue, ValidationError
 
+WARNING = "warning"
+ERROR = "error"
+DISABLE = "disable"
+LEVEL = "level"
+
 
 class ConfigLinter:  # noqa: D
-    def __init__(self) -> None:  # noqa: D
-        self._config = config.YamlLintConfig("extends: default")
+
+    DEFAULT_CONFIG = {
+        "yaml-files": ["*.yaml", "*.yml"],
+        "rules": {
+            "braces": DISABLE,
+            "brackets": DISABLE,
+            "colons": DISABLE,
+            "commas": DISABLE,
+            "comments": DISABLE,
+            "comments-indentation": {LEVEL: ERROR},
+            "document-start": {LEVEL: WARNING},
+            "document-end": DISABLE,
+            "empty-lines": DISABLE,
+            "empty-values": {LEVEL: ERROR},
+            "hyphens": DISABLE,
+            "indentation": {LEVEL: ERROR},
+            "key-duplicates": {LEVEL: ERROR},
+            "key-ordering": DISABLE,
+            "line-length": DISABLE,
+            "new-line-at-end-of-file": DISABLE,
+            "new-lines": DISABLE,
+            "octal-values": {LEVEL: WARNING},
+            "quoted-strings": DISABLE,
+            "trailing-spaces": {LEVEL: WARNING},
+            "truthy": DISABLE,
+        },
+    }
+
+    def __init__(self, lint_config: Optional[str] = None) -> None:  # noqa: D
+        self._lint_config = lint_config if lint_config else yaml.dump(data=self.DEFAULT_CONFIG)
+        self._config = config.YamlLintConfig(self._lint_config)
 
     def lint_file(self, file_path: str, file_name: str) -> List[ValidationIssue]:  # noqa: D
         issues: List[ValidationIssue] = []
