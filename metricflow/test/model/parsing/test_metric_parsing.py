16760d20ac5aa67ff80a70334782e803f066f95a
tlento <tom@transformdata.io>
2022-06-16 11:56:01 -0700

    Add tests for metric config parsing
    The core MetricFlow model parser converts user-defined YAML files
    into MetricFlow model objects via a combination of standard,
    jsonschema-backed YAML parsing and Pydantic BaseModel object
    parsing. This is managed by a set of custom parsers.
    
    Although all of these paths are probably tested indirectly via
    test model parsing in various other test cases, the concrete attribute
    parsing is not directly tested anywhere. This makes the expected output
    of the core parsing logic a bit harder to reason through.
    
    This is the first of three commits to add direct tests for the core parser
    logic, which will make updates and changes safer and easier to reason about.
    This commit adds test coverage for metric parsing. All tests have YAML
    inline instead of saved in a file, as the file and directory reading logic
    is outside the scope of the metric parser itself and the metric config spec
    is simple enough to comfortably fit inside of each test function.

---
+++ metricflow/test/model/parsing/test_metric_parsing.py
@@ -0,0 +1,260 @@
+import textwrap
+
+import pytest
+
+from metricflow.errors.errors import ParsingException
+from metricflow.model.objects.common import YamlConfigFile
+from metricflow.model.objects.constraints.where import WhereClauseConstraint
+from metricflow.model.objects.metric import CumulativeMetricWindow, MetricType
+from metricflow.model.parsing.dir_to_model import parse_yaml_files_to_model
+from metricflow.sql.sql_bind_parameters import SqlBindParameters
+from metricflow.time.time_granularity import TimeGranularity
+
+
+def test_legacy_measure_metric_parsing() -> None:
+    """Test for parsing a simple metric specification with the `measure` parameter instead of `measures`"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: legacy_test
+          type: measure_proxy
+          type_params:
+            measure: legacy_measure
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "legacy_test"
+    assert metric.type is MetricType.MEASURE_PROXY
+    assert metric.type_params.measure == "legacy_measure"
+    assert metric.type_params.measures is None
+
+
+def test_metric_metadata_parsing() -> None:
+    """Test for asserting that internal metadata is parsed into the Metric object"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: metadata_test
+          type: measure_proxy
+          type_params:
+            measures:
+              - metadata_test_measure
+        """
+    )
+    file = YamlConfigFile(filepath="test_dir/inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.metadata is not None
+    assert metric.metadata.repo_file_path == "test_dir/inline_for_test"
+    assert metric.metadata.file_slice.filename == "inline_for_test"
+
+
+def test_ratio_metric_parsing() -> None:
+    """Test for parsing a ratio metric specification with numerator and denominator"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: ratio_test
+          type: ratio
+          type_params:
+            numerator: numerator_measure
+            denominator: denominator_measure
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "ratio_test"
+    assert metric.type is MetricType.RATIO
+    assert metric.type_params.numerator == "numerator_measure"
+    assert metric.type_params.denominator == "denominator_measure"
+    assert metric.type_params.measures is None
+
+
+def test_expr_metric_parsing() -> None:
+    """Test for parsing a metric specification with an expr and a list of measures"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: expr_test
+          type: expr
+          type_params:
+            measures:
+              - measure_one
+              - measure_two
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "expr_test"
+    assert metric.type is MetricType.EXPR
+    assert metric.type_params.measures == ["measure_one", "measure_two"]
+
+
+def test_cumulative_window_metric_parsing() -> None:
+    """Test for parsing a metric specification with a cumulative window"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: cumulative_test
+          type: cumulative
+          type_params:
+            measures:
+              - cumulative_measure
+            window: "7 days"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "cumulative_test"
+    assert metric.type is MetricType.CUMULATIVE
+    assert metric.type_params.measures == ["cumulative_measure"]
+    assert metric.type_params.window == CumulativeMetricWindow(count=7, granularity=TimeGranularity.DAY)
+
+
+def test_grain_to_date_metric_parsing() -> None:
+    """Test for parsing a metric specification with the grain to date cumulative setting"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: grain_to_date_test
+          type: cumulative
+          type_params:
+            measures:
+              - cumulative_measure
+            grain_to_date: "week"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "grain_to_date_test"
+    assert metric.type is MetricType.CUMULATIVE
+    assert metric.type_params.measures == ["cumulative_measure"]
+    assert metric.type_params.window is None
+    assert metric.type_params.grain_to_date is TimeGranularity.WEEK
+
+
+def test_constraint_metric_parsing() -> None:
+    """Test for parsing a metric specification with a constraint included"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: constraint_test
+          type: measure_proxy
+          type_params:
+            measures:
+              - input_measure
+          constraint: "some_dimension IN ('value1', 'value2')"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    model = parse_yaml_files_to_model(files=[file])
+
+    assert len(model.metrics) == 1
+    metric = model.metrics[0]
+    assert metric.name == "constraint_test"
+    assert metric.type is MetricType.MEASURE_PROXY
+    assert metric.constraint == WhereClauseConstraint(
+        where="some_dimension IN ('value1', 'value2')",
+        linkable_names=["some_dimension"],
+        sql_params=SqlBindParameters(),
+    )
+
+
+def test_invalid_metric_type_parsing_error() -> None:
+    """Test for error detection when parsing a metric specification with an invalid MetricType input value"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: invalid_type_test
+          type: this is not a valid type
+          type_params:
+            measures:
+              - input_measure
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    with pytest.raises(ParsingException, match="'this is not a valid type' is not one of"):
+        parse_yaml_files_to_model(files=[file])
+
+
+def test_invalid_cumulative_metric_window_format_parsing_error() -> None:
+    """Test for errror detection when parsing malformed cumulative metric window entries"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: invalid_cumulative_format_test
+          type: cumulative
+          type_params:
+            measures:
+              - cumulative_measure
+            window: "7 days long"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    with pytest.raises(ParsingException, match="Invalid window"):
+        parse_yaml_files_to_model(files=[file])
+
+
+def test_invalid_cumulative_metric_window_granularity_parsing_error() -> None:
+    """Test for errror detection when parsing malformed cumulative metric window entries"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: invalid_cumulative_granularity_test
+          type: cumulative
+          type_params:
+            measures:
+              - cumulative_measure
+            window: "7 moons"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    with pytest.raises(ParsingException, match="Invalid time granularity"):
+        parse_yaml_files_to_model(files=[file])
+
+
+def test_invalid_cumulative_metric_window_count_parsing_error() -> None:
+    """Test for errror detection when parsing malformed cumulative metric window entries"""
+    yaml_contents = textwrap.dedent(
+        """\
+        metric:
+          name: invalid_cumulative_count_test
+          type: cumulative
+          type_params:
+            measures:
+              - cumulative_measure
+            window: "six days"
+        """
+    )
+    file = YamlConfigFile(filepath="inline_for_test", contents=yaml_contents)
+
+    with pytest.raises(ParsingException, match="Invalid count"):
+        parse_yaml_files_to_model(files=[file])
