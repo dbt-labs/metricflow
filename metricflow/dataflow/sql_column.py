43d61d6cb6ff1370ef2735c462a5202cdd4fcecc
serramatutu <lucas@transformdata.io>
2022-06-29 18:04:19 -0300

    remove `from_pandas_dtype` from SqlColumn and add repr to it
    - `from_pandas_dtype` was not being used anymore since we migrated to a Snowflake-specific implementation of the inference context provider.
    - `__repr__` now produces a more concise string that is useful for debugging

--- metricflow/dataflow/sql_column.py
+++ metricflow/dataflow/sql_column.py
@@ -2,14 +2,6 @@ from __future__ import annotations
 from enum import Enum
 from typing import Optional
 
-from pandas.api.types import (
-    is_datetime64_any_dtype as is_datetime_dtype,
-    is_float_dtype,
-    is_integer_dtype,
-    is_bool_dtype,
-    is_string_dtype,
-)
-
 from metricflow.dataflow.sql_table import SqlTable
 from metricflow.model.objects.utils import FrozenBaseModel
 
@@ -23,21 +15,6 @@ class SqlColumnType(str, Enum):
     FLOAT = "float"
     DATETIME = "datetime"
     UNKNOWN = "unknown"
-
-    @staticmethod
-    def from_pandas_dtype(dtype: Optional[str]) -> SqlColumnType:
-        """Get the column type from a pd.Series.dtype"""
-        if is_integer_dtype(dtype):
-            return SqlColumnType.INTEGER
-        if is_float_dtype(dtype):
-            return SqlColumnType.FLOAT
-        if is_datetime_dtype(dtype):
-            return SqlColumnType.DATETIME
-        if is_bool_dtype(dtype):
-            return SqlColumnType.BOOLEAN
-        if is_string_dtype(dtype):
-            return SqlColumnType.STRING
-        return SqlColumnType.UNKNOWN
 
 
 class SqlColumn(FrozenBaseModel):
@@ -81,3 +58,6 @@ class SqlColumn(FrozenBaseModel):
     def sql(self) -> str:
         """Return the snippet that can be used for use in SQL queries."""
         return f"{self.table.sql}.{self.column_name}"
+
+    def __repr__(self) -> str:  # noqa: D
+        return f"SqlColumn(full_name={self.sql})"
