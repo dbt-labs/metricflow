eca1b0dfa296db0d50c7441b5b82988266245649
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-06-30 12:42:17 -0700

    Add tests for ModelValidationResults class

--- metricflow/test/model/validations/test_validator_helpers.py
+++ metricflow/test/model/validations/test_validator_helpers.py
@@ -1,6 +1,6 @@
 from datetime import date
 import json
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Union
 
 import pytest
 
@@ -12,6 +12,7 @@ from metricflow.model.validations.validator_helpers import (
     MaterializationContext,
     MeasureContext,
     MetricContext,
+    ModelValidationResults,
     ValidationContextJSON,
     ValidationError,
     ValidationFatal,
@@ -19,6 +20,7 @@ from metricflow.model.validations.validator_helpers import (
     ValidationIssue,
     ValidationIssueJSON,
     ValidationIssueLevel,
+    ValidationIssueType,
     ValidationWarning,
 )
 
@@ -88,6 +90,59 @@ def base_issue_dict(file_context_dict: ValidationContextJSON) -> ValidationIssue
         "message": "An issue occured",
         "context": file_context_dict,
     }
+
+
+@pytest.fixture
+def list_of_issues(  # noqa: D
+    file_context_dict: Dict[str, Union[str, int]],
+    data_source_context_dict: Dict[str, Any],
+    dimension_context_dict: Dict[str, Any],
+    identifier_context_dict: Dict[str, Any],
+    measure_context_dict: Dict[str, Any],
+    materialization_context_dict: Dict[str, Any],
+    metric_context_dict: Dict[str, Any],
+) -> List[ValidationIssueType]:
+    issues: List[ValidationIssueType] = []
+    issues.append(
+        ValidationWarning(
+            context=DataSourceContext.parse_obj(data_source_context_dict),
+            message="Something caused a warning, problem #1",
+        )
+    )
+    issues.append(
+        ValidationWarning(
+            context=DimensionContext.parse_obj(dimension_context_dict), message="Something caused a warning, problem #2"
+        )
+    )
+    issues.append(
+        ValidationFutureError(
+            context=IdentifierContext.parse_obj(identifier_context_dict),
+            message="Something caused a future error, problem #3",
+            error_date=date(2022, 6, 13),
+        )
+    )
+    issues.append(
+        ValidationError(
+            context=MeasureContext.parse_obj(measure_context_dict), message="Something caused an error, problem #4"
+        )
+    )
+    issues.append(
+        ValidationError(
+            context=MaterializationContext.parse_obj(materialization_context_dict),
+            message="Something caused an error, problem #5",
+        )
+    )
+    issues.append(
+        ValidationFatal(
+            context=MetricContext.parse_obj(metric_context_dict), message="Something caused a fatal, problem #6"
+        )
+    )
+    issues.append(
+        ValidationFatal(
+            context=FileContext.parse_obj(file_context_dict), message="Something caused a fatal, probelm #7"
+        )
+    )
+    return issues
 
 
 def test_load_validation_context(file_context_dict: Dict[str, Union[str, int]]) -> None:  # noqa: D
@@ -193,3 +248,53 @@ def test_load_validation_warning(base_issue_dict: ValidationIssueJSON) -> None: 
     assert issue.level == ValidationIssueLevel.WARNING
     assert issue.message == base_issue_dict["message"]
     assert isinstance(issue.context, FileContext)
+
+
+def test_creating_model_validation_results_from_issue_list(  # noqa: D
+    list_of_issues: List[ValidationIssueType],
+) -> None:
+    warnings = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.WARNING]
+    future_errors = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.FUTURE_ERROR]
+    errors = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.ERROR]
+    fatals = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.FATAL]
+
+    model_validation_issues = ModelValidationResults.from_issues_sequence(list_of_issues)
+    assert len(model_validation_issues.warnings) == len(warnings)
+    assert len(model_validation_issues.future_errors) == len(future_errors)
+    assert len(model_validation_issues.errors) == len(errors)
+    assert len(model_validation_issues.fatals) == len(fatals)
+    assert model_validation_issues.has_blocking_issues
+
+    model_validation_issues = ModelValidationResults(warnings=warnings, future_errors=future_errors)
+    assert not model_validation_issues.has_blocking_issues
+
+
+def test_jsonifying_and_reloading_model_validation_results_is_equal(  # noqa: D
+    list_of_issues: List[ValidationIssueType],
+) -> None:
+    warnings = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.WARNING]
+    errors = [issue for issue in list_of_issues if issue.level == ValidationIssueLevel.ERROR]
+    set_context_types = set([issue.context.__class__ for issue in list_of_issues])
+
+    model_validation_issues = ModelValidationResults.from_issues_sequence(list_of_issues)
+    model_validation_issues_new = ModelValidationResults.parse_raw(model_validation_issues.json())
+    assert model_validation_issues_new == model_validation_issues
+    assert model_validation_issues_new != ModelValidationResults(warnings=warnings, errors=errors)
+
+    # ensure ValidationContexts were properly parsed into the differen subclasses
+    new_context_types = [issue.context.__class__ for issue in model_validation_issues_new.warnings]
+    new_context_types += [issue.context.__class__ for issue in model_validation_issues_new.future_errors]
+    new_context_types += [issue.context.__class__ for issue in model_validation_issues_new.errors]
+    new_context_types += [issue.context.__class__ for issue in model_validation_issues_new.fatals]
+    assert set_context_types == set(new_context_types)
+
+
+def test_merge_two_model_validation_results(list_of_issues: List[ValidationIssueType]) -> None:  # noqa: D
+    validation_results = ModelValidationResults.from_issues_sequence(list_of_issues)
+    validation_results_dup = ModelValidationResults.from_issues_sequence(list_of_issues)
+    merged = ModelValidationResults.merge([validation_results, validation_results_dup])
+
+    assert merged.warnings == validation_results.warnings + validation_results_dup.warnings
+    assert merged.future_errors == validation_results.future_errors + validation_results_dup.future_errors
+    assert merged.errors == validation_results.errors + validation_results_dup.errors
+    assert merged.fatals == validation_results.fatals + validation_results_dup.fatals
