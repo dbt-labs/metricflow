4ac2cf600f615d49d4be5cfd4d38277bfe264668
William Deng <william.deng14@gmail.com>
2022-07-25 16:38:14 -0400

    update snapshots

--- metricflow/test/snapshots/test_rendered_query.py/MetricFlowExplainResult/BigQuerySqlClient/test_render_query__query0.sql
+++ metricflow/test/snapshots/test_rendered_query.py/MetricFlowExplainResult/BigQuerySqlClient/test_render_query__query0.sql
@@ -6,7 +6,6 @@ SELECT
 FROM (
   -- Read Elements From Data Source 'bookings_source'
   -- Metric Time Dimension 'ds'
-  -- Constrain Time Range to [2000-01-01T00:00:00, 2040-12-31T00:00:00]
   -- Pass Only Elements:
   --   ['bookings', 'ds']
   SELECT
@@ -16,7 +15,6 @@ FROM (
     -- User Defined SQL Query
     SELECT * FROM ***************************.fct_bookings
   ) bookings_source_src_0
-  WHERE ds BETWEEN CAST('2000-01-01' AS DATETIME) AND CAST('2040-12-31' AS DATETIME)
-) subq_3
+) subq_2
 GROUP BY
   ds
