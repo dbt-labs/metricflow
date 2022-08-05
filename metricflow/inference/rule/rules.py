98fdc32e869cc4b9762b9ea90b09f66c818d73c8
serramatutu <lucas@transformdata.io>
2022-08-05 14:31:01 -0300

    Add default ruleset for inference
    This commit adds a default ruleset for data source inference. These rules use information like column names, types and count statistics to reason about types for them.
    
    See each rule for documentation on functionality.

---
+++ metricflow/inference/rule/rules.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+from abc import abstractmethod
+
+from typing import Callable, List, TypeVar
+
+from metricflow.inference.context.data_warehouse import (
+    ColumnProperties,
+    DataWarehouseInferenceContext,
+)
+from metricflow.inference.rule.base import InferenceRule
+from metricflow.inference.models import (
+    InferenceSignal,
+    InferenceSignalConfidence,
+    InferenceSignalNode,
+)
+
+T = TypeVar("T", bound="ColumnMatcherRule")
+
+ColumnMatcher = Callable[[T, ColumnProperties], bool]
+
+
+class ColumnMatcherRule(InferenceRule):
+    """Inference rule that checks for matches across all columns.
+
+    This is a useful shortcut for making rules that match columns one by one with preset confidence
+    values, types and match reasons.
+
+    If you need a more specific rule with varying confidence, column cross-checking and that outputs
+    multiple types, inherit from `InferenceRule` directly.
+
+    type_node: the `InferenceSignalNode` to produce whenever the pattern is matched
+    confidence: the `InferenceSignalConfidence` to produce whenever the pattern is matched
+    only_applies_to_parent_signal: whether the produced signal should be only taken into
+        consideration by the solver if the parent is present in the tree.
+    match_reason: a human-readable string of the reason why this was matched
+    """
+
+    type_node: InferenceSignalNode
+    confidence: InferenceSignalConfidence
+    only_applies_to_parent_signal: bool
+    match_reason: str
+
+    @abstractmethod
+    def match_column(self, props: ColumnProperties) -> bool:
+        """A function to determine whether `ColumnProperties` matches. If it does, produce the signal"""
+        raise NotImplementedError
+
+    def process(self, warehouse: DataWarehouseInferenceContext) -> List[InferenceSignal]:  # type: ignore
+        """Try to match all columns' properties with the matching function.
+
+        If they do match, produce a signal with the configured type and confidence.
+        """
+        matching_columns = [column for column, props in warehouse.columns.items() if self.match_column(props)]
+        signals = [
+            InferenceSignal(
+                column=column,
+                type_node=self.type_node,
+                reason=self.match_reason,
+                confidence=self.confidence,
+                only_applies_to_parent=self.only_applies_to_parent_signal,
+            )
+            for column in matching_columns
+        ]
+        return signals
+
+
+class LowCardinalityRatioRule(ColumnMatcherRule):
+    """Inference rule that checks for string columns with low cardinality to count ratio.
+
+    The ratio is calculated as `distinct_count/(count - null_count)`.
+    """
+
+    def __init__(self, cardinality_count_ratio_threshold: float) -> None:
+        """Initialize the rule.
+
+        cardinality_count_ratio_threshold: rations below this threshold will match.
+        """
+        assert cardinality_count_ratio_threshold >= 0 and cardinality_count_ratio_threshold <= 1
+        self.threshold = cardinality_count_ratio_threshold
+        super().__init__()
+
+    match_reason = "Column has low cardinality"
+
+    def match_column(self, props: ColumnProperties) -> bool:  # noqa: D
+        denom = props.row_count - props.null_count
+
+        # undefined ratio
+        if denom == 0:
+            return False
+
+        ratio = props.distinct_row_count / denom
+        return ratio < self.threshold
---
+++ metricflow/test/inference/rule/test_rules.py
@@ -0,0 +1,91 @@
+from metricflow.dataflow.sql_column import SqlColumn
+from metricflow.dataflow.sql_table import SqlTable
+from metricflow.inference.context.data_warehouse import (
+    DataWarehouseInferenceContext,
+    ColumnProperties,
+    InferenceColumnType,
+    TableProperties,
+)
+from metricflow.inference.models import InferenceSignalConfidence, InferenceSignalType
+from metricflow.inference.rule.rules import ColumnMatcherRule, LowCardinalityRatioRule
+
+
+def create_context_with_counts(rows: int, distinct: int, nulls: int) -> DataWarehouseInferenceContext:
+    """Get a `DataWarehouseInferenceContext` with the designated counts"""
+    return DataWarehouseInferenceContext(
+        table_props=[
+            TableProperties(
+                table=SqlTable.from_string("db.schema.table"),
+                column_props=[
+                    ColumnProperties(
+                        column=SqlColumn.from_string("db.schema.table.column"),
+                        type=InferenceColumnType.INTEGER,
+                        row_count=rows,
+                        distinct_row_count=distinct,
+                        null_count=nulls,
+                        is_nullable=nulls != 0,
+                        min_value=0,
+                        max_value=rows - 1,
+                    )
+                ],
+            )
+        ]
+    )
+
+
+class TestLowCardinalityRule(LowCardinalityRatioRule):  # noqa: D
+    type_node = InferenceSignalType.DIMENSION.CATEGORICAL
+    confidence = InferenceSignalConfidence.MEDIUM
+    only_applies_to_parent_signal = False
+
+
+def test_column_matcher(warehouse_ctx: DataWarehouseInferenceContext) -> None:  # noqa: D
+    class TestRule(ColumnMatcherRule):
+        type_node = InferenceSignalType.DIMENSION.UNKNOWN
+        confidence = InferenceSignalConfidence.MEDIUM
+        match_reason = "test reason"
+        only_applies_to_parent_signal = False
+
+        def match_column(self, props: ColumnProperties) -> bool:
+            return props.column.column_name.endswith("test_column")
+
+    signals = TestRule().process(warehouse_ctx)
+
+    assert len(signals) == 1
+    assert signals[0].confidence == InferenceSignalConfidence.MEDIUM
+    assert signals[0].type_node == InferenceSignalType.DIMENSION.UNKNOWN
+    assert signals[0].column == SqlColumn.from_string("db.schema.table.test_column")
+    assert signals[0].reason == "test reason"
+    assert not signals[0].only_applies_to_parent
+
+
+def test_low_cardinality_ratio_rule_high_cardinality_doesnt_match() -> None:  # noqa: D
+    rule = TestLowCardinalityRule(0.1)
+    ctx = create_context_with_counts(100, 100, 0)
+
+    signals = rule.process(ctx)
+    assert len(signals) == 0
+
+
+def test_low_cardinality_ratio_rule_low_cardinality_lots_of_nulls_doesnt_match() -> None:  # noqa: D
+    rule = TestLowCardinalityRule(0.1)
+    ctx = create_context_with_counts(100, 2, 99)
+
+    signals = rule.process(ctx)
+    assert len(signals) == 0
+
+
+def test_low_cardinality_ratio_rule_low_cardinality_all_nulls_doesnt_match() -> None:  # noqa: D
+    rule = TestLowCardinalityRule(0.1)
+    ctx = create_context_with_counts(100, 1, 100)
+
+    signals = rule.process(ctx)
+    assert len(signals) == 0
+
+
+def test_low_cardinality_ratio_rule_low_cardinality_matches() -> None:  # noqa: D
+    rule = TestLowCardinalityRule(0.1)
+    ctx = create_context_with_counts(100, 1, 0)
+
+    signals = rule.process(ctx)
+    assert len(signals) == 1
