05fdd0386a4a3b391e040dd45b4bd67c0017a77b
tlento <tom@transformdata.io>
2022-10-03 11:20:26 -0700

    Add test for dataflow to sql plan conversion for SCDs
    This adds the test and snapshot files for the plan conversion. Since
    the SQL rendering for the SCD condition is not yet implemented, the
    ON conditions are incorrect. A follow-up commit will add the
    rendering support and update the plans with the relevant changes.
    
    Ordinarily we'd like to have the queries be complete and correct,
    but in this case the change is sufficiently subtle that we split
    things up to highlight the difference.

---
+++ metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/RedshiftSqlClient/test_join_to_scd_dimension__plan0_optimized.sql
@@ -0,0 +1,35 @@
+-- Constrain Output with WHERE
+-- Pass Only Elements:
+--   ['bookings', 'metric_time']
+-- Aggregate Measures
+-- Compute Metrics via Expressions
+SELECT
+  metric_time
+  , SUM(bookings) AS family_bookings
+FROM (
+  -- Join Standard Outputs
+  -- Pass Only Elements:
+  --   ['bookings', 'listing__capacity', 'metric_time']
+  SELECT
+    subq_12.metric_time AS metric_time
+    , listings_src_10019.capacity AS listing__capacity
+    , subq_12.bookings AS bookings
+  FROM (
+    -- Read Elements From Data Source 'bookings_source'
+    -- Metric Time Dimension 'ds'
+    -- Pass Only Elements:
+    --   ['bookings', 'metric_time', 'listing']
+    SELECT
+      ds AS metric_time
+      , listing_id AS listing
+      , 1 AS bookings
+    FROM ***************************.fct_bookings bookings_source_src_10018
+  ) subq_12
+  LEFT OUTER JOIN
+    ***************************.dim_listings listings_src_10019
+  ON
+    subq_12.listing = listings_src_10019.listing_id
+) subq_16
+WHERE listing__capacity > 2
+GROUP BY
+  metric_time
