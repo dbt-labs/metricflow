7bdd48571d28800d7315994ec55476526a73bd3f
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-05 17:23:03 -0700

    Parse dbt project to get dbt metrics objects
    Instead of going the route of writing our own custom parser to parse
    dbt configs into python objects, we've decided to instead use dbt
    to parse the dbt configs. This makes our lives easier long term, because
    as dbt specifics change, our ability to parse is determined by specifying
    the new dbt version in the dependencies. Additionally it results in a lot
    less code on our end.
    
    **Note**: `parse_dbt_project_to_model` raises a `NotImplementedError`. This
    is expected, a future PR will build out translating the dbt Manifest obeject
    into a UserConfiguredModel.

--- metricflow/model/parsing/dbt_dir_to_model.py
+++ metricflow/model/parsing/dbt_dir_to_model.py
@@ -1,10 +1,26 @@
+from dbt.lib import get_dbt_config
+from dbt import tracking
+from dbt.parser.manifest import ManifestLoader as DbtManifestLoader, Manifest as DbtManifest
+from metricflow.model.objects.user_configured_model import UserConfiguredModel
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
 
 
-def parse_dbt_project_to_model(
-    directory: str,
-) -> ModelBuildResult:
+def get_dbt_project_manifest(directory: str) -> DbtManifest:
+    """Builds the dbt Manifest object from the dbt project"""
+
+    dbt_config = get_dbt_config(project_dir=directory)
+    # If we don't disable tracking, we have to setup a full
+    # dbt User object to build the manifest
+    tracking.disable_tracking()
+    return DbtManifestLoader.get_full_manifest(config=dbt_config)
+
+
+def parse_dbt_project_to_model(directory: str) -> ModelBuildResult:
     """Parse dbt model files in the given directory to a UserConfiguredModel."""
-    raise NotImplementedError(
-        f"Unable to parse dbt project at {directory} to a UserConfiguredModel, because we haven't implemented it"
-    )
+
+    manifest = get_dbt_project_manifest(directory=directory)  # noqa: F841
+
+    # TODO: Implement transforming dbt_metrics into a UserConfiguredModel
+    raise NotImplementedError("Transforming dbt metrics into a Metricflow UserConfiguredModel has not been implemented")
+
+    return ModelBuildResult(model=UserConfiguredModel(data_sources=[], metrics=[], materializations=[]))
