7d824f11aa722f44930001b30352296024641bcc
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-05 17:08:12 -0700

    Add scaffolding for mf cli to parse dbt projects to UserConfigureModels

--- metricflow/engine/utils.py
+++ metricflow/engine/utils.py
@@ -7,6 +7,7 @@ from metricflow.configuration.constants import CONFIG_MODEL_PATH
 from metricflow.configuration.yaml_handler import YamlFileHandler
 from metricflow.errors.errors import ModelCreationException
 from metricflow.model.objects.user_configured_model import UserConfiguredModel
+from metricflow.model.parsing.dbt_dir_to_model import parse_dbt_project_to_model
 from metricflow.model.parsing.dir_to_model import ModelBuildResult, parse_directory_of_yaml_files_to_model
 from metricflow.sql_clients.common_client import not_empty
 
@@ -37,9 +38,33 @@ def model_build_result_from_config(
         raise ModelCreationException from e
 
 
+def model_build_result_from_dbt_config(
+    handler: YamlFileHandler, raise_issues_as_exceptions: bool = True
+) -> ModelBuildResult:
+    """Given a yaml file, creates a ModelBuildResult.
+
+    Args:
+        handler: a file handler for loading the configs from
+        raise_issues_as_exceptions: determines if issues should be raised, or returned as issues
+
+    Returns:
+        ModelBuildResult that contains the UserConfigureModel and any associated ValidationIssues
+    """
+    dbt_models_path = path_to_models(handler=handler)
+    try:
+        return parse_dbt_project_to_model(dbt_models_path)
+    except Exception as e:
+        raise ModelCreationException from e
+
+
 def build_user_configured_model_from_config(handler: YamlFileHandler) -> UserConfiguredModel:
     """Given a yaml file, create a UserConfiguredModel."""
     return model_build_result_from_config(handler=handler).model
+
+
+def build_user_configured_model_from_dbt_config(handler: YamlFileHandler) -> UserConfiguredModel:
+    """Given a yaml file, create a UserConfiguredModel."""
+    return model_build_result_from_dbt_config(handler=handler).model
 
 
 def convert_to_datetime(datetime_str: Optional[str]) -> Optional[dt.datetime]:
