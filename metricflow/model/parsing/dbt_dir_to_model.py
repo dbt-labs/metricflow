c951dd87be4b122a442512b22814b81f181cc9f1
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-05 17:23:03 -0700

    Parse dbt project to get dbt metrics objects

--- metricflow/model/parsing/dbt_dir_to_model.py
+++ metricflow/model/parsing/dbt_dir_to_model.py
@@ -1,3 +1,6 @@
+from dbt.lib import get_dbt_config
+from dbt.parser.manifest import ManifestLoader as DbtManifestLoader
+from metricflow.model.objects.user_configured_model import UserConfiguredModel
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
 
 
@@ -5,6 +8,12 @@ def parse_dbt_project_to_model(
     directory: str,
 ) -> ModelBuildResult:
     """Parse dbt model files in the given directory to a UserConfiguredModel."""
-    raise NotImplementedError(
-        f"Unable to parse dbt project at {directory} to a UserConfiguredModel, because we haven't implemented it"
-    )
+    dbt_config = get_dbt_config(project_dir=directory)
+    # dbt_config = DbtRuntimeConfig.new_project(project_root=directory)
+    manifest = DbtManifestLoader.get_full_manifest(config=dbt_config)
+    dbt_metrics = list(manifest.metrics.values())  # noqa: F841
+
+    # TODO: Implement transforming dbt_metrics into a UserConfiguredModel
+    raise NotImplementedError("Transforming dbt metrics into a Metricflow UserConfiguredModel has not been implemented")
+
+    return ModelBuildResult(model=UserConfiguredModel(data_sources=[], metrics=[], materializations=[]))
