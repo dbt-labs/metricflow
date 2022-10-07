4b9222d200eb355dfab5d47da3b2e2d62881b576
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-07 11:03:00 -0700

    Handle merging of duplicate data sources generated from dbt metrics
    different dbt metrics might have the same `.model` reference. Because
    we build a data source from each dbt metric, this means we might have
    duplicate data sources. Additionally, the dbt metrics don't have to
    specify all the dimensions / timestamps that exist on a data source.
    Thus we want to gather the dimensions and measures that exist on the
    variations of the data source.

--- metricflow/model/transformations/dbt_to_metricflow.py
+++ metricflow/model/transformations/dbt_to_metricflow.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
-from typing import List
+from operator import xor
+import traceback
+from typing import Dict, List, Set
 from dbt.contracts.graph.parsed import ParsedMetric as DbtMetric, ParsedModelNode as DbtModelNode
 from dbt.exceptions import ref_invalid_args
 from dbt.contracts.graph.manifest import Manifest as DbtManifest
 from metricflow.model.objects.data_source import DataSource
 from metricflow.model.objects.elements.dimension import Dimension, DimensionType, DimensionTypeParams
+from metricflow.model.objects.elements.identifier import Identifier
+from metricflow.model.objects.elements.measure import Measure
 from metricflow.model.objects.metric import Metric
 from metricflow.model.objects.user_configured_model import UserConfiguredModel
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
+from metricflow.model.validations.validator_helpers import ModelValidationResults, ValidationError, ValidationIssue
 from metricflow.time.time_granularity import TimeGranularity
 
 
@@ -92,15 +97,94 @@ def dbt_metric_to_metricflow_elements(dbt_metric: DbtMetric, manifest: DbtManife
     return TransformedDbtMetric(data_source=data_source, metrics=[])
 
 
-def transform_manifest_into_user_configured_model(manifest: DbtManifest) -> ModelBuildResult:  # noqa: D
-    data_sources = {}
-    metrics = []
-    for dbt_metric in manifest.metrics.values():
-        transformed_dbt_metric = dbt_metric_to_metricflow_elements(dbt_metric=dbt_metric, manifest=manifest)
-        if transformed_dbt_metric.data_source.name not in data_sources:
-            data_sources[transformed_dbt_metric.data_source.name] = transformed_dbt_metric.data_source
-        else:
-            raise NotImplementedError("Merging two data sources has not been implemented")
-        metrics.extend(transformed_dbt_metric.metrics)
+def merge_data_sources(data_sources: List[DataSource]) -> DataSource:  # noqa: D
+    if len(data_sources) == 1:
+        return data_sources[0]
 
-    return ModelBuildResult(model=UserConfiguredModel(data_sources=list(data_sources.values()), metrics=metrics))
+    measures: Set[Measure] = set()
+    identifiers: Set[Identifier] = set()
+    dimensions: Set[Dimension] = set()
+    names: List[str] = []
+    descriptions: List[str] = []
+    sql_tables: List[str] = []
+    sql_queries: List[str] = []
+    dbt_models: List[str] = []
+    for data_source in data_sources:
+        names.append(data_source.name) if data_source.name else None
+        descriptions.append(data_source.description) if data_source.description else None
+        sql_tables.append(data_source.sql_table) if data_source.sql_table else None
+        sql_queries.append(data_source.sql_query) if data_source.sql_query else None
+        dbt_models.append(data_source.dbt_model) if data_source.dbt_model else None
+        measures = measures.union(set(data_source.measures)) if data_source.measures else measures
+        identifiers = identifiers.union(set(data_source.identifiers)) if data_source.identifiers else identifiers
+        dimensions = dimensions.union(set(data_source.dimensions)) if data_source.dimensions else dimensions
+
+    set_names = set(names)
+    set_descriptions = set(descriptions)
+    set_sql_tables = set(sql_tables)
+    set_sql_queries = set(sql_queries)
+    set_dbt_models = set(dbt_models)
+
+    assert len(set_names) == 1, "Cannot merge data sources, all data sources to merge must have same name"
+    assert (
+        len(set_descriptions) <= 1
+    ), "Cannot merge data sources, all data sources to merge must have same descritpion (or none)"
+    assert (
+        len(set_sql_tables) <= 1
+    ), "Cannot merge data sources, all data sources to merge must have same sql_table (or none)"
+    assert (
+        len(set_sql_queries) <= 1
+    ), "Cannot merge data sources, all data sources to merge must have same sql_query (or none)"
+    assert xor(
+        len(set_sql_tables) == 1, len(set_sql_queries) == 1
+    ), "Cannot merge data sources, definitions for both sql_table and sql_query exist"
+    assert (
+        len(set_dbt_models) <= 1
+    ), "Cannot merge data sources, all data sources to merge must have same dbt_model (or none)"
+
+    return DataSource(
+        name=list(set_names)[0],
+        description=list(set_descriptions)[0] if set_descriptions else None,
+        sql_table=list(set_sql_tables)[0] if set_sql_tables else None,
+        sql_query=list(set_sql_queries)[0] if set_sql_queries else None,
+        dbt_model=list(set_dbt_models)[0] if set_dbt_models else None,
+        dimensions=list(dimensions),
+        identifiers=list(identifiers),
+        measures=list(measures),
+    )
+
+
+def transform_manifest_into_user_configured_model(manifest: DbtManifest) -> ModelBuildResult:  # noqa: D
+    data_sources_map: Dict[str, List[DataSource]] = {}
+    metrics = []
+    issues: List[ValidationIssue] = []
+    for dbt_metric in manifest.metrics.values():
+        # TODO: Handle derived dbt metrics
+        if dbt_metric.calculation_method == "derived":
+            continue
+        else:
+            transformed_dbt_metric = dbt_metric_to_metricflow_elements(dbt_metric=dbt_metric, manifest=manifest)
+            if transformed_dbt_metric.data_source.name not in data_sources_map:
+                data_sources_map[transformed_dbt_metric.data_source.name] = [transformed_dbt_metric.data_source]
+            else:
+                data_sources_map[transformed_dbt_metric.data_source.name].append(transformed_dbt_metric.data_source)
+            metrics.extend(transformed_dbt_metric.metrics)
+
+    # As it might be the case that we generated many of the same data source,
+    # we need to merge / dedupe them
+    deduped_data_sources = []
+    for name, data_sources in data_sources_map.items():
+        try:
+            deduped_data_sources.append(merge_data_sources(data_sources))
+        except Exception as e:
+            issues.append(
+                ValidationError(
+                    message=f"Failed to merge data sources with the name `{name}`",
+                    extra_detail="".join(traceback.format_tb(e.__traceback__)),
+                )
+            )
+
+    return ModelBuildResult(
+        model=UserConfiguredModel(data_sources=list(deduped_data_sources), metrics=metrics),
+        issues=ModelValidationResults.from_issues_sequence(issues=issues),
+    )
