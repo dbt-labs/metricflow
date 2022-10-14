09dce7f3263a7877d2651d0f7c2a919ab2e8843f
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-14 10:02:45 -0700

    Apply transformations to UserConfiguredModel built from dbt project
    We must apply the transformations so that things like `COUNT` measures
    properly get transformed into `SUM` measures.

--- metricflow/model/parsing/dbt_dir_to_model.py
+++ metricflow/model/parsing/dbt_dir_to_model.py
@@ -1,6 +1,7 @@
 from dbt.lib import get_dbt_config
 from dbt import tracking
 from dbt.parser.manifest import ManifestLoader as DbtManifestLoader, Manifest as DbtManifest
+from metricflow.model.model_transformer import ModelTransformer
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
 from metricflow.model.transformations.dbt_to_metricflow import DbtManifestTransformer
 
@@ -18,4 +19,7 @@ def get_dbt_project_manifest(directory: str) -> DbtManifest:
 def parse_dbt_project_to_model(directory: str) -> ModelBuildResult:
     """Parse dbt model files in the given directory to a UserConfiguredModel."""
     manifest = get_dbt_project_manifest(directory=directory)
-    return DbtManifestTransformer(manifest=manifest).build_user_configured_model()
+    build_result = DbtManifestTransformer(manifest=manifest).build_user_configured_model()
+    transformed_model = ModelTransformer.pre_validation_transform_model(model=build_result.model)
+    transformed_model = ModelTransformer.post_validation_transform_model(model=transformed_model)
+    return ModelBuildResult(model=transformed_model, issues=build_result.issues)
