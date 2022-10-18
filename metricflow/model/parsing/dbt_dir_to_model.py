3beeca36433ea86ae34df26604c245cc03a50bc0
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-18 16:04:18 -0700

    Update `get_dbt_project_manifest` to accept `profile` and `target` args
    One can have multiple dbt profiles defined, and each dbt profile can
    have multiple targets. In most cases the default target for a profile
    is the `dev` target, not the `prod` target. Given that there can be
    multiple profiles and targets, and that the default profile/target
    combination used is likely not the `prod` combination, it's important
    to allow the specification of which `profile` and `target` to use as
    we want to support prod use cases.

--- metricflow/model/parsing/dbt_dir_to_model.py
+++ metricflow/model/parsing/dbt_dir_to_model.py
@@ -1,15 +1,33 @@
+from dataclasses import dataclass
 from dbt.lib import get_dbt_config
 from dbt import tracking
 from dbt.parser.manifest import ManifestLoader as DbtManifestLoader, Manifest as DbtManifest
 from metricflow.model.model_transformer import ModelTransformer
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
 from metricflow.model.transformations.dbt_to_metricflow import DbtManifestTransformer
+from typing import Optional
 
 
-def get_dbt_project_manifest(directory: str) -> DbtManifest:
+@dataclass
+class DbtProfileArgs:
+    """Class to represent dbt profile arguments
+
+    dbt's get_dbt_config uses `getattr` to get values out of the passed in args.
+    We cannot pass a simple dict, because `getattr` doesn't work for keys of a
+    dictionary. Thus we create a simple object that `getattr` will work on.
+    """
+
+    profile: Optional[str] = None
+    target: Optional[str] = None
+
+
+def get_dbt_project_manifest(
+    directory: str, profile: Optional[str] = None, target: Optional[str] = None
+) -> DbtManifest:
     """Builds the dbt Manifest object from the dbt project"""
 
-    dbt_config = get_dbt_config(project_dir=directory)
+    profile_args = DbtProfileArgs(profile=profile, target=target)
+    dbt_config = get_dbt_config(project_dir=directory, args=profile_args)
     # If we don't disable tracking, we have to setup a full
     # dbt User object to build the manifest
     tracking.disable_tracking()
