c47496c6c879222e1300bfef703e4c52721c9e06
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-28 14:14:59 -0700

    Define abstract `DbtTransformRule` for mapping dbt MetricNode values to Model elements
    We want to be able to build a UserConfiguredModel from a series of rules.
    Each rule will map a value from a MetricNode, or the MetricNode's sub-
    elements, to it's UserConfiguredModel counterpart. However, the
    UserConfiguredModel elements have a number of "required" values. Thus
    we cannot build the UserConfiguredModel element directly in any given
    rule because each rule will deal with one mapping. So instead, slowly
    build and modify dicts representing UserConfiguredModel elements via
    the rules, and then later we'll build the elments from the dicts.

---
+++ metricflow/model/dbt_transformations/dbt_transform_rule.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import Any, Dict, Tuple
+
+from dbt_metadata_client.dbt_metadata_api_schema import MetricNode
+from metricflow.model.validations.validator_helpers import ModelValidationResults
+
+
+@dataclass
+class DbtTransformedObjects:  # type: ignore[misc]
+    """Model elements, and sub elements, mapped by element name path"""
+
+    data_sources: Dict[str, Dict[str, Any]] = field(default_factory=dict)  # type: ignore[misc]
+    metrics: Dict[str, Dict[str, Any]] = field(default_factory=dict)  # type: ignore[misc]
+    materializations: Dict[str, Dict[str, Any]] = field(default_factory=dict)  # type: ignore[misc]
+    # access path is ["data_source_name"]["dimension_name"] -> dict dimension representation
+    dimensions: Dict[str, Dict[str, Dict[str, Any]]] = field(default_factory=dict)  # type: ignore[misc]
+    # access path is ["data_source_name"]["identifier_name"] -> dict identifier representation
+    identifiers: Dict[str, Dict[str, Dict[str, Any]]] = field(default_factory=dict)  # type: ignore[misc]
+    # access path is ["data_source_name"]["measure_name"] -> dict measure representation
+    measures: Dict[str, Dict[str, Dict[str, Any]]] = field(default_factory=dict)  # type: ignore[misc]
+
+
+@dataclass
+class DbtTransformationResult:  # noqa: D
+    transformed_objects: DbtTransformedObjects
+    validation_results: ModelValidationResults
+
+
+class DbtTransformRule(ABC):
+    """Encapsulates logic for transforming a dbt manifest. e.g. add metrics based on measures."""
+
+    @staticmethod
+    @abstractmethod
+    def run(dbt_metrics: Tuple[MetricNode, ...], objects: DbtTransformedObjects) -> ModelValidationResults:
+        """Take in a DbtTransformedObjects object, transform it in place given the metrics and the rule, return any issues"""
+        pass
