fb661786d1758ddb6070a975b01242b846d62287
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-28 14:32:46 -0700

    Define `DbtTransformer.transform` method of how to iterate over the rules

--- metricflow/model/dbt_transformer.py
+++ metricflow/model/dbt_transformer.py
@@ -1,10 +1,16 @@
 import logging
 from typing import Tuple, Type
 
-from metricflow.model.dbt_transformations.dbt_transform_rule import DbtTransformRule
+from dbt_metadata_client.dbt_metadata_api_schema import MetricNode
+from metricflow.model.dbt_transformations.dbt_transform_rule import (
+    DbtTransformRule,
+    DbtTransformationResult,
+    DbtTransformedObjects,
+)
 from metricflow.model.objects.data_source import DataSource
 from metricflow.model.objects.materialization import Materialization
 from metricflow.model.objects.metric import Metric
+from metricflow.model.validations.validator_helpers import ModelValidationResults
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +31,14 @@ class DbtTransformer:
         self.data_source_class = data_source_class
         self.metric_class = metric_class
         self.materialization_class = materialization_class
+
+    def transform(self, dbt_metrics: Tuple[MetricNode, ...]) -> DbtTransformationResult:
+        """Using a series of rules transforms dbt metrics into a mapped dict representing UserConfiguredModel objects"""
+        transformed_objects = DbtTransformedObjects()
+        validation_results = ModelValidationResults()
+
+        for rule in self.rules:
+            transformation_rule_issues = rule.run(dbt_metrics=dbt_metrics, objects=transformed_objects)
+            validation_results = ModelValidationResults.merge([validation_results, transformation_rule_issues])
+
+        return DbtTransformationResult(transformed_objects=transformed_objects, validation_results=validation_results)
