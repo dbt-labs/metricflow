23e55778c6ed4cc90d5e9d6db9dad5bcc7ae9724
William Deng <william.deng14@gmail.com>
2022-10-31 13:19:16 -0400

    added CROSS JOIN

--- metricflow/sql/sql_plan.py
+++ metricflow/sql/sql_plan.py
@@ -97,6 +97,7 @@ class SqlJoinType(Enum):
     LEFT_OUTER = "LEFT OUTER JOIN"
     FULL_OUTER = "FULL OUTER JOIN"
     INNER = "INNER JOIN"
+    CROSS_JOIN = "CROSS JOIN"
 
     def __repr__(self) -> str:  # noqa: D
         return f"{self.__class__.__name__}.{self.name}"
