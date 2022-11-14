5bbb2cb9b62aa5d25f0c9ebb724c918c1c13c6c0
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-11-13 17:23:57 -0800

    Add tests and fixtures for `dbt_metric_model_to_data_source_rules` and `dbt_metric_to_dimensions_rules`

---
+++ metricflow/test/fixtures/dbt_metadata_fixtures.py
@@ -0,0 +1,89 @@
+import pytest
+from typing import Any, Dict, List, Tuple
+
+from dbt_metadata_client.dbt_metadata_api_schema import MetricNode
+
+
+@pytest.fixture
+def dbt_json_metrics() -> List[Dict[str, Any]]:  # type: ignore[misc]
+    """A list of JSON-ish dbt metrics"""
+
+    cats_model = {
+        "name": "cats",
+        "description": "So many cats, all the cats",
+        "schema": "animals",
+        "database": "earth_data",
+    }
+
+    return [
+        {
+            "name": "num_domesticated_cats",
+            "calculation_method": "count",
+            "dimensions": ["breed", "origin", "max_age"],
+            "timestamp": "created_at",
+            "expression": "breed",
+            "filters": [{"field": "domesticated", "operation": "=", "value": "true"}],
+            "model": cats_model,
+        },
+        {
+            "name": "num_cat_origins",
+            "calculation_method": "count_distinct",
+            "dimensions": ["breed", "max_age"],
+            "timestamp": "created_at",
+            "expression": "origin",
+            "filters": None,
+            "model": cats_model,
+        },
+        {
+            "name": "sum_of_cats_max_age_not_useful",
+            "calculation_method": "sum",
+            "dimensions": ["breed", "origin"],
+            "timestamp": "created_at",
+            "expression": "max_age",
+            "filters": None,
+            "model": cats_model,
+        },
+        {
+            "name": "average_cat_breed_max_age",
+            "calculation_method": "average",
+            "dimensions": ["breed", "origin"],
+            "timestamp": "created_at",
+            "expression": "max_age",
+            "filters": None,
+            "model": cats_model,
+        },
+        {
+            "name": "min_cat_breed_max_age",
+            "calculation_method": "min",
+            "dimensions": ["breed", "origin"],
+            "timestamp": "created_at",
+            "expression": "max_age",
+            "filters": None,
+            "model": cats_model,
+        },
+        {
+            "name": "max_cat_breed_max_age",
+            "calculation_method": "max",
+            "dimensions": ["breed", "origin"],
+            "timestamp": "created_at",
+            "expression": "max_age",
+            "filters": None,
+            "model": cats_model,
+        },
+        {
+            "name": "ratio_max_to_min_cat_breed_max_age",
+            "calculation_method": "derived",
+            "dimensions": None,
+            "timestamp": "created_at",
+            "expression": "(max_cat_breed_max_age / min_cat_breed_max_age)",
+            "filters": None,
+            "model": None,
+        },
+    ]
+
+
+@pytest.fixture
+def dbt_metrics(dbt_json_metrics: List[Dict[str, Any]]) -> Tuple[MetricNode, ...]:  # type: ignore[misc]
+    """A list of dbt MetricNodes"""
+    metric_nodes: Tuple[MetricNode, ...] = tuple(MetricNode(json_data=metric_json) for metric_json in dbt_json_metrics)
+    return metric_nodes
---
+++ metricflow/test/model/dbt_cloud_parsing/test_dbt_metric_to_dimensions_rules.py
@@ -0,0 +1,104 @@
+from typing import Tuple
+
+from dbt_metadata_client.dbt_metadata_api_schema import MetricNode
+from metricflow.model.dbt_transformer import DbtTransformer
+from metricflow.model.dbt_transformations.dbt_metric_to_dimensions_rules import (
+    DbtDimensionsToDimensions,
+    DbtTimestampToDimension,
+    DbtFiltersToDimensions,
+)
+from metricflow.model.dbt_transformations.dbt_metric_to_metrics_rules import CALC_METHOD_TO_METRIC_TYPE
+from metricflow.model.dbt_transformations.dbt_transform_rule import DbtTransformedObjects, DbtTransformRule
+from metricflow.model.objects.elements.dimension import DimensionType
+from metricflow.model.objects.metric import MetricType
+
+
+def test_dbt_metric_to_dimensions_rules_skip_derived_metrics(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    derived_metrics = tuple(dbt_metric for dbt_metric in dbt_metrics if dbt_metric.calculation_method == "derived")
+    rules: Tuple[DbtTransformRule, ...] = (
+        DbtDimensionsToDimensions(),
+        DbtTimestampToDimension(),
+        DbtFiltersToDimensions(),
+    )
+    transformer = DbtTransformer(rules=rules)
+    result = transformer.transform(dbt_metrics=derived_metrics)
+    assert (
+        len(result.transformed_objects.dimensions.keys()) == 0
+    ), "Derived dbt metrics created dimensions, and they shouldn't"
+
+
+def test_dbt_dimensions_to_dimensions(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    objects = DbtTransformedObjects()
+    issues = DbtDimensionsToDimensions().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        not issues.has_blocking_issues
+    ), f"DbtDimensionsToDimensions raised blocking issues when it shouldn't have: {issues.to_pretty_json()}"
+    for dbt_metric in dbt_metrics:
+        if CALC_METHOD_TO_METRIC_TYPE[dbt_metric.calculation_method] != MetricType.DERIVED:
+            for dimension in dbt_metric.dimensions:
+                assert (
+                    objects.dimensions[dbt_metric.model.name].get(dimension) is not None
+                ), f"Failed to build dimension `{dimension}` for data source `{dbt_metric.model.name}` of dbt metric `{dbt_metric.name}`"
+
+
+def test_dbt_dimensions_to_dimensions_no_issues_when_no_dimensions(  # noqa: D
+    dbt_metrics: Tuple[MetricNode, ...]
+) -> None:
+    objects = DbtTransformedObjects()
+    dbt_metrics[0].dimensions = None
+    issues = DbtDimensionsToDimensions().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        not issues.has_blocking_issues
+    ), f"DbtDimensionsToDimensions raised blocking issues when it shouldn't have: {issues.to_pretty_json()}"
+
+
+def test_dbt_timestamp_to_dimension(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    objects = DbtTransformedObjects()
+    issues = DbtTimestampToDimension().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        not issues.has_blocking_issues
+    ), f"DbtTimestampToDimension raised blocking issues when it shouldn't have: {issues.to_pretty_json()}"
+    for dbt_metric in dbt_metrics:
+        if CALC_METHOD_TO_METRIC_TYPE[dbt_metric.calculation_method] != MetricType.DERIVED:
+            # each dbt metric timestamp creates 1 dimension, and a dbt metric must have exactly one timestamp
+            assert (
+                len(objects.dimensions[dbt_metric.model.name].keys()) == 1
+            ), f"A dbt metric timestamp should only create 1 dimension, but {len(objects.dimensions[dbt_metric.model.name].keys())} were created"
+            assert (
+                objects.dimensions[dbt_metric.model.name][dbt_metric.timestamp]["type"] == DimensionType.TIME
+            ), f"A TIME type dimension should be created from a dbt metric timestamp, but a `{objects.dimensions[dbt_metric.model.name][dbt_metric.timestamp]['type']}` was created"
+
+
+def test_dbt_timestamp_to_dimension_missing_timestamp_issue(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    objects = DbtTransformedObjects()
+    dbt_metrics[0].timestamp = None
+    issues = DbtTimestampToDimension().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        issues.has_blocking_issues
+    ), f"DbtTimestampToDimension didn't raise blocking issues when it should have: {issues.to_pretty_json()}"
+
+
+def test_dbt_filters_to_dimensions(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    objects = DbtTransformedObjects()
+    issues = DbtFiltersToDimensions().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        not issues.has_blocking_issues
+    ), f"DbtFiltersToDimension raised blocking issues when it shouldn't have: {issues.to_pretty_json()}"
+    for dbt_metric in dbt_metrics:
+        if CALC_METHOD_TO_METRIC_TYPE[dbt_metric.calculation_method] != MetricType.DERIVED and dbt_metric.filters:
+            # as many dimensions should be created as their are filters
+            assert len(objects.dimensions[dbt_metric.model.name].keys()) == len(
+                dbt_metric.filters
+            ), f"DbtFiltersToDimensions should have created as many dimensions as filters. We expected {len(dbt_metric.filters)}, but {len(objects.dimensions[dbt_metric.model.name].keys())} were created"
+
+
+def test_dbt_filters_to_dimensions_no_filters_okay(dbt_metrics: Tuple[MetricNode, ...]) -> None:  # noqa: D
+    objects = DbtTransformedObjects()
+    dbt_metrics[0].filters = None
+    issues = DbtFiltersToDimensions().run(dbt_metrics=dbt_metrics, objects=objects)
+    assert (
+        not issues.has_blocking_issues
+    ), f"DbtFiltersToDimension raised blocking issues when it shouldn't have: {issues.to_pretty_json()}"
+    assert (
+        objects.dimensions.get(dbt_metrics[0].model.name) is None
+    ), f"No filters should have been created, but {len(objects.dimensions[dbt_metrics[0].model.name].keys())} were created"
