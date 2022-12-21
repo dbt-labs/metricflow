bf3657f5754e7250cf7d872e6ba5b7d71334f136
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-12-20 16:15:40 -0800

    Move conftest inclusion of dbt cloud support testing fixtures into dbt cloud testing directory
    No tests, except the dbt cloud support tests, need the dbt_metadata fixtures.
    Additionally, dbt cloud support is optional to MetricFlow, and requires
    optional dependencies. This was problemantic if one were to run
    `poetry run pytest metricflow/test` because the root conftest would include
    the dbt_metadata fixtures, and thus break (if the optional dependencies
    weren't installed). By moving the dbt_metadata fixtures inclusion to
    a conftest config within the dbt cloud support testing dir, its now possible
    to appropriately ignore those tests without breaking, i.e.
    `poetry run pytest metricflow/test --ignore=metricflow/test/model/dbt_cloud_parsing`

--- metricflow/test/conftest.py
+++ metricflow/test/conftest.py
@@ -7,4 +7,3 @@ from metricflow.test.fixtures.setup_fixtures import *  # noqa: F401, F403
 from metricflow.test.fixtures.sql_client_fixtures import *  # noqa: F401, F403
 from metricflow.test.fixtures.sql_fixtures import *  # noqa: F401, F403
 from metricflow.test.fixtures.table_fixtures import *  # noqa: F401, F403
-from metricflow.test.fixtures.dbt_metadata_fixtures import *  # noqa: F401, F403
