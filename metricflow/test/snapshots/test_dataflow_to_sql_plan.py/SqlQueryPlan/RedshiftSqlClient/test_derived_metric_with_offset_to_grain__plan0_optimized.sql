c706728b64fae915caf339e79a271acbd6b0657a
Courtney Holcomb <courtneyeholcomb@gmail.com>
2023-01-12 15:40:16 -0800

    Update snapshots for SQL engines

---
+++ metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/RedshiftSqlClient/test_derived_metric_with_offset_to_grain__plan0_optimized.sql
@@ -0,0 +1,70 @@
+-- Compute Metrics via Expressions
+SELECT
+  metric_time
+  , (bookings - bookings_at_start_of_month) / bookings_at_start_of_month AS bookings_growth_since_start_of_month
+FROM (
+  -- Combine Metrics
+  SELECT
+    COALESCE(subq_18.metric_time, subq_26.metric_time) AS metric_time
+    , subq_18.bookings AS bookings
+    , subq_26.bookings_at_start_of_month AS bookings_at_start_of_month
+  FROM (
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
+    SELECT
+      metric_time
+      , SUM(bookings) AS bookings
+    FROM (
+      -- Read Elements From Data Source 'bookings_source'
+      -- Metric Time Dimension 'ds'
+      -- Pass Only Elements:
+      --   ['bookings', 'metric_time']
+      SELECT
+        ds AS metric_time
+        , 1 AS bookings
+      FROM (
+        -- User Defined SQL Query
+        SELECT * FROM ***************************.fct_bookings
+      ) bookings_source_src_10001
+    ) subq_16
+    GROUP BY
+      metric_time
+  ) subq_18
+  INNER JOIN (
+    -- Join to Time Spine Dataset
+    SELECT
+      subq_25.ds AS metric_time
+      , subq_23.bookings_at_start_of_month AS bookings_at_start_of_month
+    FROM ***************************.mf_time_spine subq_25
+    LEFT OUTER JOIN (
+      -- Aggregate Measures
+      -- Compute Metrics via Expressions
+      SELECT
+        metric_time
+        , SUM(bookings) AS bookings_at_start_of_month
+      FROM (
+        -- Read Elements From Data Source 'bookings_source'
+        -- Metric Time Dimension 'ds'
+        -- Pass Only Elements:
+        --   ['bookings', 'metric_time']
+        SELECT
+          ds AS metric_time
+          , 1 AS bookings
+        FROM (
+          -- User Defined SQL Query
+          SELECT * FROM ***************************.fct_bookings
+        ) bookings_source_src_10001
+      ) subq_21
+      GROUP BY
+        metric_time
+    ) subq_23
+    ON
+      DATE_TRUNC('month', subq_25.ds) = subq_23.metric_time
+  ) subq_26
+  ON
+    (
+      subq_18.metric_time = subq_26.metric_time
+    ) OR (
+      (subq_18.metric_time IS NULL) AND (subq_26.metric_time IS NULL)
+    )
+) subq_27
---
+++ metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/RedshiftSqlClient/test_derived_metric_with_offset_window_and_offset_to_grain__plan0_optimized.sql
@@ -0,0 +1,79 @@
+-- Compute Metrics via Expressions
+SELECT
+  metric_time
+  , month_start_bookings / bookings_1_month_ago AS bookings_month_start_compared_to_1_month_prior
+FROM (
+  -- Combine Metrics
+  SELECT
+    COALESCE(subq_24.metric_time, subq_32.metric_time) AS metric_time
+    , subq_24.month_start_bookings AS month_start_bookings
+    , subq_32.bookings_1_month_ago AS bookings_1_month_ago
+  FROM (
+    -- Join to Time Spine Dataset
+    SELECT
+      subq_23.ds AS metric_time
+      , subq_21.month_start_bookings AS month_start_bookings
+    FROM ***************************.mf_time_spine subq_23
+    LEFT OUTER JOIN (
+      -- Aggregate Measures
+      -- Compute Metrics via Expressions
+      SELECT
+        metric_time
+        , SUM(bookings) AS month_start_bookings
+      FROM (
+        -- Read Elements From Data Source 'bookings_source'
+        -- Metric Time Dimension 'ds'
+        -- Pass Only Elements:
+        --   ['bookings', 'metric_time']
+        SELECT
+          ds AS metric_time
+          , 1 AS bookings
+        FROM (
+          -- User Defined SQL Query
+          SELECT * FROM ***************************.fct_bookings
+        ) bookings_source_src_10001
+      ) subq_19
+      GROUP BY
+        metric_time
+    ) subq_21
+    ON
+      DATE_TRUNC('month', subq_23.ds) = subq_21.metric_time
+  ) subq_24
+  INNER JOIN (
+    -- Join to Time Spine Dataset
+    SELECT
+      subq_31.ds AS metric_time
+      , subq_29.bookings_1_month_ago AS bookings_1_month_ago
+    FROM ***************************.mf_time_spine subq_31
+    LEFT OUTER JOIN (
+      -- Aggregate Measures
+      -- Compute Metrics via Expressions
+      SELECT
+        metric_time
+        , SUM(bookings) AS bookings_1_month_ago
+      FROM (
+        -- Read Elements From Data Source 'bookings_source'
+        -- Metric Time Dimension 'ds'
+        -- Pass Only Elements:
+        --   ['bookings', 'metric_time']
+        SELECT
+          ds AS metric_time
+          , 1 AS bookings
+        FROM (
+          -- User Defined SQL Query
+          SELECT * FROM ***************************.fct_bookings
+        ) bookings_source_src_10001
+      ) subq_27
+      GROUP BY
+        metric_time
+    ) subq_29
+    ON
+      DATEADD(month, -1, subq_31.ds) = subq_29.metric_time
+  ) subq_32
+  ON
+    (
+      subq_24.metric_time = subq_32.metric_time
+    ) OR (
+      (subq_24.metric_time IS NULL) AND (subq_32.metric_time IS NULL)
+    )
+) subq_33
