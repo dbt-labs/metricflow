dcf4c6a1535799e530d6a524c435cbd27c01be45
tlento <tom@transformdata.io>
2023-01-13 18:31:11 -0800

    Stop updating enum values by reference in BigQuerySqlExpressionRenderer
    The BigQuerySqlExpressionRenderer is updating the `value` property of
    an enum value. It's possible Python passes enumeration types by value,
    or does some copy on write, but most likely they're passed by reference
    like everything else. As such, modifying properties on an enum instance
    is likely bad form.
    
    The existing logic also blocks mypy updates to 0.940 or later. So we
    fix it here.

--- metricflow/sql/render/big_query.py
+++ metricflow/sql/render/big_query.py
@@ -93,9 +93,11 @@ class BigQuerySqlExpressionRenderer(DefaultSqlExpressionRenderer):
         if node.grain_to_date:
             granularity = node.granularity
             if granularity == TimeGranularity.WEEK or granularity == TimeGranularity.YEAR:
-                granularity.value = "ISO" + granularity.value.upper()
+                granularity_value = "ISO" + granularity.value.upper()
+            else:
+                granularity_value = granularity.value
             return SqlExpressionRenderResult(
-                sql=f"DATE_TRUNC({column.sql}, {granularity.value})",
+                sql=f"DATE_TRUNC({column.sql}, {granularity_value})",
                 bind_parameters=column.bind_parameters,
             )
 
