aa2db5167037f5f4538f6ca8389dea510144acec
Callum McCann <cmccann51@gmail.com>
2023-02-17 10:05:07 -0600

    removing dbt name from manifest

--- metricflow/model/parsing/dbt_dir_to_model.py
+++ metricflow/model/parsing/dbt_dir_to_model.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 from dbt.lib import get_dbt_config
 from dbt import tracking
-from dbt.task.runnable import ManifestTask
-from dbt.parser.manifest import ManifestLoader as DbtManifestLoader, Manifest as DbtManifest
+from dbt.parser.manifest import ManifestLoader, Manifest
 from metricflow.model.model_transformer import ModelTransformer
 from metricflow.model.parsing.dir_to_model import ModelBuildResult
 from typing import Optional
@@ -23,7 +22,7 @@ class DbtProfileArgs:
 
 def get_dbt_project_manifest(
     directory: str, profile: Optional[str] = None, target: Optional[str] = None
-) -> DbtManifest:
+) -> Manifest:
     """Builds the dbt Manifest object from the dbt project"""
 
     profile_args = DbtProfileArgs(profile=profile, target=target)
@@ -31,5 +30,5 @@ def get_dbt_project_manifest(
     # If we don't disable tracking, we have to setup a full
     # dbt User object to build the manifest
     tracking.disable_tracking()
-    return DbtManifestLoader.get_full_manifest(config=dbt_config)
+    return ManifestLoader.get_full_manifest(config=dbt_config)
 
