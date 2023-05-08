a95c8f3a97256c9052d1b432bd59e02fc4ec91b1
Paul Yang <github@ardorful.com>
2023-05-08 13:14:48 -0700

    Remove WhereClauseConstraint and SpecWhereClauseConstraint.

--- metricflow/model/spec_converters.py
+++ metricflow/model/spec_converters.py
@@ -9,23 +9,10 @@ classes, rather than the perhaps more natural approach of adding a to_spec() met
 shims likely point to the need for a bit of an internal refactor, but that's a concern for another time.
 """
 
-from typing import List
-
-from metricflow.dataset.dataset import DataSet
-from dbt_semantic_interfaces.objects.constraints.where import WhereClauseConstraint
-from dbt_semantic_interfaces.objects.elements.dimension import DimensionType
 from dbt_semantic_interfaces.objects.elements.measure import Measure
-from metricflow.protocols.semantics import SemanticModelSemanticsAccessor
-from metricflow.query.query_exceptions import InvalidQueryException
 from metricflow.specs import (
-    DimensionSpec,
-    EntitySpec,
-    LinkableSpecSet,
     MeasureSpec,
     NonAdditiveDimensionSpec,
-    SpecWhereClauseConstraint,
-    StructuredLinkableSpecName,
-    TimeDimensionSpec,
 )
 
 
@@ -48,77 +35,4 @@ class MeasureConverter:
         return MeasureSpec(
             element_name=measure.name,
             non_additive_dimension_spec=non_additive_dimension_spec,
-        )
-
-
-class WhereConstraintConverter:
-    """Static class for converting WhereClauseConstraint objects to a SpecWhereClauseConstraint representation.
-
-    The WhereClauseConstraint model object contains a parsed set of element names, and as such conversion to
-    a SpecWhereClauseConstraint requires semantic resolution of linkable specs across semantic models. This resolution
-    has to happen for metrics and measures independently, as both can be constrained, so bolting this on
-    to something like SemanticModelSemantics will not be adequate.
-    """
-
-    @staticmethod
-    def _convert_to_linkable_specs(
-        semantic_model_semantics: SemanticModelSemanticsAccessor, where_constraint_names: List[str]
-    ) -> LinkableSpecSet:
-        """Processes where_clause_constraint.linkable_names into associated LinkableInstanceSpecs (dims, times, ids)
-
-        where_constraint_names: WhereConstraintClause.linkable_names
-        semantic_model_semantics: SemanticModelSemanticsAccessor from the instantiated class
-
-        output: InstanceSpecSet of Tuple(DimensionSpec), Tuple(TimeDimensionSpec), Tuple(EntitySpec)
-        """
-        where_constraint_dimensions = []
-        where_constraint_time_dimensions = []
-        where_constraint_entities = []
-        linkable_spec_names = [
-            StructuredLinkableSpecName.from_name(linkable_name) for linkable_name in where_constraint_names
-        ]
-        dimension_references = {
-            dimension_reference.element_name: dimension_reference
-            for dimension_reference in semantic_model_semantics.get_dimension_references()
-        }
-        entity_references = {
-            entity_reference.element_name: entity_reference
-            for entity_reference in semantic_model_semantics.get_entity_references()
-        }
-
-        for spec_name in linkable_spec_names:
-            if spec_name.element_name == DataSet.metric_time_dimension_name():
-                where_constraint_time_dimensions.append(TimeDimensionSpec.from_name(spec_name.qualified_name))
-            elif spec_name.element_name in dimension_references:
-                dimension = semantic_model_semantics.get_dimension(dimension_references[spec_name.element_name])
-                if dimension.type == DimensionType.CATEGORICAL:
-                    where_constraint_dimensions.append(DimensionSpec.from_name(spec_name.qualified_name))
-                elif dimension.type == DimensionType.TIME:
-                    where_constraint_time_dimensions.append(TimeDimensionSpec.from_name(spec_name.qualified_name))
-                else:
-                    raise RuntimeError(f"Unhandled type: {dimension.type}")
-            elif spec_name.element_name in entity_references:
-                where_constraint_entities.append(EntitySpec.from_name(spec_name.qualified_name))
-            else:
-                raise InvalidQueryException(f"Unknown element: {spec_name}")
-
-        return LinkableSpecSet(
-            dimension_specs=tuple(where_constraint_dimensions),
-            time_dimension_specs=tuple(where_constraint_time_dimensions),
-            entity_specs=tuple(where_constraint_entities),
-        )
-
-    @staticmethod
-    def convert_to_spec_where_constraint(
-        semantic_model_semantics: SemanticModelSemanticsAccessor, where_constraint: WhereClauseConstraint
-    ) -> SpecWhereClauseConstraint:
-        """Converts a where constraint to one using specs."""
-        return SpecWhereClauseConstraint(
-            where_condition=where_constraint.where,
-            linkable_names=tuple(where_constraint.linkable_names),
-            linkable_spec_set=WhereConstraintConverter._convert_to_linkable_specs(
-                semantic_model_semantics=semantic_model_semantics,
-                where_constraint_names=where_constraint.linkable_names,
-            ),
-            execution_parameters=where_constraint.sql_params,
         )
