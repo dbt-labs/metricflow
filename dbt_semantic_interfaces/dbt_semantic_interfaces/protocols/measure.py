ec62e2289e81f90d1ec7cde9ee5d9bb64aa4ce47
Callum McCann <cmccann51@gmail.com>
2023-05-09 10:24:58 -0500

    linting fixes to descriptions - make mypy happy

--- dbt_semantic_interfaces/dbt_semantic_interfaces/protocols/measure.py
+++ dbt_semantic_interfaces/dbt_semantic_interfaces/protocols/measure.py
@@ -6,21 +6,27 @@ from dbt_semantic_interfaces.type_enums.aggregation_type import AggregationType
 
 class NonAdditiveDimensionParameters(Protocol):
     """Describes the params for specifying non-additive dimensions in a measure."""
+
     name: str
     window_choice: AggregationType
     window_groupings: List[str]
 
 
 class MeasureAggregationParameters(Protocol):
-    """Describes parameters for aggregations""" 
+    """Describes parameters for aggregations"""
+
     percentile: Optional[float]
     use_discrete_percentile: bool
     use_approximate_percentile: bool
 
 
 class MeasureProtocol(Protocol):
-    """Describes a measure, which is a field in the underlying semantic model that can be 
-    aggregated in a specific way."""
+    """Describes a measure.
+
+    This class describes a measure, which is a field in the underlying semantic
+    model that can be aggregated in a specific way.
+    """
+
     name: str
     agg: AggregationType
     description: Optional[str]
