84cd187fb308a445496f19ae4d30b9c4f7022ccd
Callum McCann <cmccann51@gmail.com>
2023-05-10 15:23:09 -0500

    updating snapshots for simplified metric defs

--- dbt_semantic_interfaces/dbt_semantic_interfaces/objects/metric.py
+++ dbt_semantic_interfaces/dbt_semantic_interfaces/objects/metric.py
@@ -144,16 +144,6 @@ class MetricTypeParams(HashableBaseModel):
     grain_to_date: Optional[TimeGranularity]
     metrics: Optional[List[MetricInput]]
 
-    @property
-    def numerator_measure_reference(self) -> Optional[MeasureReference]:
-        """Return the measure reference, if any, associated with the metric input measure defined as the numerator"""
-        return self.numerator.measure_reference if self.numerator else None
-
-    @property
-    def denominator_measure_reference(self) -> Optional[MeasureReference]:
-        """Return the measure reference, if any, associated with the metric input measure defined as the denominator"""
-        return self.denominator.measure_reference if self.denominator else None
-
 
 class Metric(HashableBaseModel, ModelWithMetadataParsing):
     """Describes a metric"""
