1d85d4ac3974150e8cf657dbe693e6736af0f3fb
Nick Handel <nicholas.handel@gmail.com>
2023-06-15 13:24:03 -0700

    Update tutorial and provide sample for sources in tutorial

--- metricflow/cli/main.py
+++ metricflow/cli/main.py
@@ -183,26 +183,24 @@ def tutorial(ctx: click.core.Context, cfg: CLIContext, msg: bool, skip_dw: bool,
 
             1.  In '{$HOME}/.metricflow/config.yml', `model_path` should be '{$HOME}/.metricflow/sample_models'.
             2.  Try validating your data model: `mf validate-configs`
-            3.  Check out your metrics: `mf list-metrics`
-            4.  Check out dimensions for your metric `mf list-dimensions --metric-names transactions`
-            5.  Query your first metric: `mf query --metrics transactions --dimensions metric_time --order metric_time`
+            3.  Check out your metrics: `mf list metrics`
+            4.  Check out dimensions for your metric `mf list dimensions --metrics transactions`
+            5.  Query your first metric: `mf query --metrics transactions --group_by metric_time --order metric_time`
             6.  Show the SQL MetricFlow generates:
-                `mf query --metrics transactions --dimensions metric_time --order metric_time --explain`
+                `mf query --metrics transactions --group_by metric_time --order metric_time --explain`
             7.  Visualize the plan:
-                `mf query --metrics transactions --dimensions metric_time --order metric_time --explain --display-plans`
+                `mf query --metrics transactions --group_by metric_time --order metric_time --explain --display-plans`
                 * This only works if you have graphviz installed - see README.
             8.  Add another dimension:
-                `mf query --metrics transactions --dimensions metric_time,customer__country --order metric_time`
+                `mf query --metrics transactions --group_by metric_time,customer__country --order metric_time`
             9.  Add a coarser time granularity:
-                `mf query --metrics transactions --dimensions metric_time__week --order metric_time__week`
+                `mf query --metrics transactions --group_by metric_time__week --order metric_time__week`
             10. Try a more complicated query:
                 `mf query \\
-                  --metrics transactions,transaction_usd_na,transaction_usd_na_l7d --dimensions metric_time,is_large \\
+                  --metrics transactions,transaction_usd_na,transaction_usd_na_l7d --group_by metric_time,is_large \\
                   --order metric_time --start-time 2022-03-20 --end-time 2022-04-01`
                 * You can also add `--explain` or `--display-plans`.
-            11. For more ways to interact with the sample models, go to
-                ‘https://docs.transform.co/docs/metricflow/metricflow-tutorial’.
-            12. Once you’re done, run `mf tutorial --skip-dw --drop-tables` to drop the sample tables.
+            11. Once you’re done, run `mf tutorial --skip-dw --drop-tables` to drop the sample tables.
         """
     )
 
