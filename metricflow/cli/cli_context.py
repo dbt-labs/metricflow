6239eb83bc51432024a28a58d7dfce3df623fdd2
Will Deng <william.deng14@gmail.com>
2023-06-16 12:52:33 -0400

    initialize MFE in CLI using the dbt project root method

--- metricflow/cli/cli_context.py
+++ metricflow/cli/cli_context.py
@@ -125,7 +125,7 @@ class CLIContext:
     def _initialize_metricflow_engine(self) -> None:
         """Initialize the MetricFlowEngine."""
         try:
-            self._mf = MetricFlowEngine.from_config(self.config)
+            self._mf = MetricFlowEngine.from_dbt_project_root(self.config)
         except Exception as e:
             raise MetricFlowInitException from e
 
