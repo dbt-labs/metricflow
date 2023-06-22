a1f9ffbef743d7f83b4ad6c8586f9d54f091e783
Paul Yang <github@ardorful.com>
2023-06-22 13:09:46 -0700

    Refactor logic in create_source_tables fixture to a function.

--- metricflow/test/fixtures/table_fixtures.py
+++ metricflow/test/fixtures/table_fixtures.py
@@ -5,15 +5,14 @@ import os
 from pathlib import Path
 
 import pytest
-from dbt_semantic_interfaces.pretty_print import pformat_big_objects
 
 from metricflow.plan_conversion.time_spine import TimeSpineSource
 from metricflow.protocols.sql_client import SqlClient
 from metricflow.sql_clients.sql_utils import create_time_spine_table_if_necessary
 from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
+from metricflow.test.source_schema_tools import create_tables_listed_in_table_snapshot_repository
 from metricflow.test.table_snapshot.table_snapshots import (
     SqlTableSnapshotRepository,
-    SqlTableSnapshotRestorer,
 )
 
 logger = logging.getLogger(__name__)
@@ -38,26 +37,8 @@ def create_source_tables(
     # TODO: consolidate time spine operations with the rest of the table creation configuration
     create_time_spine_table_if_necessary(time_spine_source=time_spine_source, sql_client=sql_client)
 
-    schema_name = mf_test_session_state.mf_source_schema
-    # Figure out which tables are missing from the source schema.
-    expected_table_names = sorted(
-        [table_snapshot.table_name for table_snapshot in source_table_snapshot_repository.table_snapshots]
+    create_tables_listed_in_table_snapshot_repository(
+        sql_client=sql_client,
+        schema_name=mf_test_session_state.mf_source_schema,
+        table_snapshot_repository=source_table_snapshot_repository,
     )
-    logger.info(
-        f"The following tables are needed in schema {schema_name}:\n" f"{pformat_big_objects(expected_table_names)}"
-    )
-    source_schema_table_names = sorted(sql_client.list_tables(schema_name=schema_name))
-
-    missing_table_names = set(expected_table_names).difference(source_schema_table_names)
-    logger.info(
-        f"The following tables are missing and will be restored:\n"
-        f"{pformat_big_objects(sorted(missing_table_names))}"
-    )
-    # Restore the ones that are missing.
-    snapshot_restorer = SqlTableSnapshotRestorer(
-        sql_client=sql_client, schema_name=mf_test_session_state.mf_source_schema
-    )
-    for table_snapshot in source_table_snapshot_repository.table_snapshots:
-        if table_snapshot.table_name in missing_table_names:
-            logger.info(f"Restoring: {table_snapshot.table_name}")
-            snapshot_restorer.restore(table_snapshot)
