a08e6751508ee0cfc8a6269c415b71894bd719f7
Paul Yang <github@ardorful.com>
2023-06-28 10:19:34 -0700

    Add example projection configuration for tests.

---
+++ metricflow/test/model/example_project_configuration.py
@@ -0,0 +1,33 @@
+import textwrap
+
+from dbt_semantic_interfaces.implementations.project_configuration import (
+    PydanticProjectConfiguration,
+)
+from dbt_semantic_interfaces.implementations.time_spine_table_configuration import (
+    PydanticTimeSpineTableConfiguration,
+)
+from dbt_semantic_interfaces.parsing.objects import YamlConfigFile
+from dbt_semantic_interfaces.type_enums import TimeGranularity
+
+EXAMPLE_PROJECT_CONFIGURATION = PydanticProjectConfiguration(
+    time_spine_table_configurations=[
+        PydanticTimeSpineTableConfiguration(
+            location="example_schema.example_table",
+            column_name="ds",
+            grain=TimeGranularity.DAY,
+        )
+    ],
+)
+
+EXAMPLE_PROJECT_CONFIGURATION_YAML_CONFIG_FILE = YamlConfigFile(
+    filepath="projection_configuration_yaml_file_path",
+    contents=textwrap.dedent(
+        """\
+        project_configuration:
+          time_spine_table_configurations:
+            - location: example_schema.example_table
+              column_name: ds
+              grain: day
+        """
+    ),
+)
