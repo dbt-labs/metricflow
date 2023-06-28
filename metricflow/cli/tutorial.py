8acbaa150055d63ab76303b699911b890b180223
Will Deng <william.deng14@gmail.com>
2023-06-28 02:50:01 -0400

    configured tutorial to work alongside a dbt project

--- metricflow/cli/tutorial.py
+++ metricflow/cli/tutorial.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import datetime
 import os
+import pathlib
+import shutil
 from string import Template
 from typing import Dict, List, Tuple
 
@@ -196,3 +198,46 @@ def gen_sample_model_configs(dir_path: str, system_schema: str) -> None:
         )
     with open(f"{dir_path}/{COUNTRIES_TABLE}.yaml", "w") as file:
         file.write(contents)
+
+
+class dbtMetricFlowTutorialHelper:
+    """Helper class for managing tutorial related actions (ie., generating sample files)."""
+
+    SAMPLE_DBT_MODEL_DIRECTORY = "sample_dbt_models"
+    SAMPLE_MODELS_DIRECTORY = SAMPLE_DBT_MODEL_DIRECTORY + "/sample_models"
+    SAMPLE_SEED_DIRECTORY = SAMPLE_DBT_MODEL_DIRECTORY + "/seeds"
+    SAMPLE_SEMANTIC_MANIFEST = SAMPLE_DBT_MODEL_DIRECTORY + "/semantic_manifest.json"
+    SAMPLE_SOURCES_FILE = "sources.yml"
+
+    @staticmethod
+    def generate_model_files(model_path: pathlib.Path, profile_schema: str) -> None:
+        """Generates the sample model files to the given dbt model path."""
+        sample_model_path = pathlib.Path(__file__).parent / dbtMetricFlowTutorialHelper.SAMPLE_MODELS_DIRECTORY
+        shutil.copytree(src=sample_model_path, dst=model_path)
+
+        # Generate the sources.yml file with the schema given in profiles.yml
+        sample_sources_path = (
+            pathlib.Path(__file__).parent
+            / dbtMetricFlowTutorialHelper.SAMPLE_DBT_MODEL_DIRECTORY
+            / dbtMetricFlowTutorialHelper.SAMPLE_SOURCES_FILE
+        )
+        with open(sample_sources_path) as file:
+            contents = Template(file.read()).substitute({"system_schema": profile_schema})
+        dest_sources_path = pathlib.Path(model_path) / dbtMetricFlowTutorialHelper.SAMPLE_SOURCES_FILE
+        with open(dest_sources_path, "w") as file:
+            file.write(contents)
+
+    @staticmethod
+    def generate_seed_files(seed_path: pathlib.Path) -> None:
+        """Generates the sample seed files to the given dbt seed path."""
+        sample_seed_path = pathlib.Path(__file__).parent / dbtMetricFlowTutorialHelper.SAMPLE_SEED_DIRECTORY
+        shutil.copytree(src=sample_seed_path, dst=seed_path)
+
+    @staticmethod
+    def generate_semantic_manifest_file(target_path: pathlib.Path) -> None:
+        """Generates the sample semantic manifest to the given dbt target path."""
+        if not target_path.exists():
+            target_path.mkdir()
+
+        sample_manifest_path = pathlib.Path(__file__).parent / dbtMetricFlowTutorialHelper.SAMPLE_SEMANTIC_MANIFEST
+        shutil.copy(src=sample_manifest_path, dst=target_path / "semantic_manifest.json")
