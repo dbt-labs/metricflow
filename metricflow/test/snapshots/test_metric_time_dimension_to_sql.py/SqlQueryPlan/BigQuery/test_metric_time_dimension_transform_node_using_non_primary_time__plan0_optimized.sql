e04b9250071e6e4b84d426a2abe9bc38f0fba188
Paul Yang <github@ardorful.com>
2023-07-26 16:41:57 -0700

    Update SQL engine snapshots.

--- metricflow/test/snapshots/test_metric_time_dimension_to_sql.py/SqlQueryPlan/BigQuery/test_metric_time_dimension_transform_node_using_non_primary_time__plan0_optimized.sql
+++ metricflow/test/snapshots/test_metric_time_dimension_to_sql.py/SqlQueryPlan/BigQuery/test_metric_time_dimension_transform_node_using_non_primary_time__plan0_optimized.sql
@@ -16,21 +16,6 @@ SELECT
   , DATE_TRUNC(booking_paid_at, month) AS booking_paid_at__month
   , DATE_TRUNC(booking_paid_at, quarter) AS booking_paid_at__quarter
   , DATE_TRUNC(booking_paid_at, isoyear) AS booking_paid_at__year
-  , ds AS create_a_cycle_in_the_join_graph__ds
-  , DATE_TRUNC(ds, isoweek) AS create_a_cycle_in_the_join_graph__ds__week
-  , DATE_TRUNC(ds, month) AS create_a_cycle_in_the_join_graph__ds__month
-  , DATE_TRUNC(ds, quarter) AS create_a_cycle_in_the_join_graph__ds__quarter
-  , DATE_TRUNC(ds, isoyear) AS create_a_cycle_in_the_join_graph__ds__year
-  , ds_partitioned AS create_a_cycle_in_the_join_graph__ds_partitioned
-  , DATE_TRUNC(ds_partitioned, isoweek) AS create_a_cycle_in_the_join_graph__ds_partitioned__week
-  , DATE_TRUNC(ds_partitioned, month) AS create_a_cycle_in_the_join_graph__ds_partitioned__month
-  , DATE_TRUNC(ds_partitioned, quarter) AS create_a_cycle_in_the_join_graph__ds_partitioned__quarter
-  , DATE_TRUNC(ds_partitioned, isoyear) AS create_a_cycle_in_the_join_graph__ds_partitioned__year
-  , booking_paid_at AS create_a_cycle_in_the_join_graph__booking_paid_at
-  , DATE_TRUNC(booking_paid_at, isoweek) AS create_a_cycle_in_the_join_graph__booking_paid_at__week
-  , DATE_TRUNC(booking_paid_at, month) AS create_a_cycle_in_the_join_graph__booking_paid_at__month
-  , DATE_TRUNC(booking_paid_at, quarter) AS create_a_cycle_in_the_join_graph__booking_paid_at__quarter
-  , DATE_TRUNC(booking_paid_at, isoyear) AS create_a_cycle_in_the_join_graph__booking_paid_at__year
   , booking_paid_at AS metric_time
   , DATE_TRUNC(booking_paid_at, isoweek) AS metric_time__week
   , DATE_TRUNC(booking_paid_at, month) AS metric_time__month
@@ -39,11 +24,6 @@ SELECT
   , listing_id AS listing
   , guest_id AS guest
   , host_id AS host
-  , guest_id AS create_a_cycle_in_the_join_graph
-  , listing_id AS create_a_cycle_in_the_join_graph__listing
-  , guest_id AS create_a_cycle_in_the_join_graph__guest
-  , host_id AS create_a_cycle_in_the_join_graph__host
   , is_instant
-  , is_instant AS create_a_cycle_in_the_join_graph__is_instant
   , booking_value AS booking_payments
 FROM ***************************.fct_bookings bookings_source_src_10001
