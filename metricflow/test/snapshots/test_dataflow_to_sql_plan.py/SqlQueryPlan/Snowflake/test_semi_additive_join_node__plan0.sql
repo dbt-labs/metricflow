dcd3924c59005933b8bb9958e646b8d5f45ae631
Paul Yang <github@ardorful.com>
2023-07-29 13:50:45 -0700

    Update Redshift + Snowflake snapshots.

--- metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/Snowflake/test_semi_additive_join_node__plan0.sql
+++ metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/Snowflake/test_semi_additive_join_node__plan0.sql
@@ -5,8 +5,15 @@ SELECT
   , subq_0.ds__month AS ds__month
   , subq_0.ds__quarter AS ds__quarter
   , subq_0.ds__year AS ds__year
+  , subq_0.account__ds AS account__ds
+  , subq_0.account__ds__week AS account__ds__week
+  , subq_0.account__ds__month AS account__ds__month
+  , subq_0.account__ds__quarter AS account__ds__quarter
+  , subq_0.account__ds__year AS account__ds__year
   , subq_0.user AS user
+  , subq_0.account__user AS account__user
   , subq_0.account_type AS account_type
+  , subq_0.account__account_type AS account__account_type
   , subq_0.account_balance AS account_balance
   , subq_0.total_account_balance_first_day AS total_account_balance_first_day
   , subq_0.current_account_balance_by_user AS current_account_balance_by_user
@@ -22,7 +29,14 @@ FROM (
     , DATE_TRUNC('quarter', accounts_source_src_10000.ds) AS ds__quarter
     , DATE_TRUNC('year', accounts_source_src_10000.ds) AS ds__year
     , accounts_source_src_10000.account_type
+    , accounts_source_src_10000.ds AS account__ds
+    , DATE_TRUNC('week', accounts_source_src_10000.ds) AS account__ds__week
+    , DATE_TRUNC('month', accounts_source_src_10000.ds) AS account__ds__month
+    , DATE_TRUNC('quarter', accounts_source_src_10000.ds) AS account__ds__quarter
+    , DATE_TRUNC('year', accounts_source_src_10000.ds) AS account__ds__year
+    , accounts_source_src_10000.account_type AS account__account_type
     , accounts_source_src_10000.user_id AS user
+    , accounts_source_src_10000.user_id AS account__user
   FROM ***************************.fct_accounts accounts_source_src_10000
 ) subq_0
 INNER JOIN (
@@ -41,7 +55,14 @@ INNER JOIN (
       , DATE_TRUNC('quarter', accounts_source_src_10000.ds) AS ds__quarter
       , DATE_TRUNC('year', accounts_source_src_10000.ds) AS ds__year
       , accounts_source_src_10000.account_type
+      , accounts_source_src_10000.ds AS account__ds
+      , DATE_TRUNC('week', accounts_source_src_10000.ds) AS account__ds__week
+      , DATE_TRUNC('month', accounts_source_src_10000.ds) AS account__ds__month
+      , DATE_TRUNC('quarter', accounts_source_src_10000.ds) AS account__ds__quarter
+      , DATE_TRUNC('year', accounts_source_src_10000.ds) AS account__ds__year
+      , accounts_source_src_10000.account_type AS account__account_type
       , accounts_source_src_10000.user_id AS user
+      , accounts_source_src_10000.user_id AS account__user
     FROM ***************************.fct_accounts accounts_source_src_10000
   ) subq_1
 ) subq_2
