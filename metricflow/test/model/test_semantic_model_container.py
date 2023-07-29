561fcef25c4933ceac66eee79ecf771ee45515d6
Paul Yang <github@ardorful.com>
2023-07-28 23:59:50 -0700

    Update tests that depend on listing group by items to use snapshots.

--- metricflow/test/model/test_semantic_model_container.py
+++ metricflow/test/model/test_semantic_model_container.py
@@ -3,12 +3,15 @@ from __future__ import annotations
 import logging
 
 import pytest
+from _pytest.fixtures import FixtureRequest
 from dbt_semantic_interfaces.protocols.semantic_manifest import SemanticManifest
 from dbt_semantic_interfaces.references import EntityReference, MeasureReference, MetricReference
 
 from metricflow.model.semantics.linkable_element_properties import LinkableElementProperties
 from metricflow.model.semantics.metric_lookup import MetricLookup
 from metricflow.model.semantics.semantic_model_lookup import SemanticModelLookup
+from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
+from metricflow.test.snapshot_utils import assert_linkable_element_set_equal, assert_object_equal
 
 logger = logging.getLogger(__name__)
 
@@ -27,61 +30,21 @@ def metric_lookup(  # Noqa: D
     return MetricLookup(semantic_manifest=simple_semantic_manifest, semantic_model_lookup=semantic_model_lookup)
 
 
-def test_get_names(semantic_model_lookup: SemanticModelLookup) -> None:  # noqa: D
-    expected = [
-        "account_type",
-        "capacity_latest",
-        "company_name",
-        "country_latest",
-        "created_at",
-        "ds",
-        "ds_partitioned",
-        "home_state",
-        "home_state_latest",
-        "is_instant",
-        "is_lux_latest",
-        "paid_at",
-        "verification_type",
-    ]
-    assert sorted([d.element_name for d in semantic_model_lookup.get_dimension_references()]) == expected
-
-    expected = [
-        "account_balance",
-        "approximate_continuous_booking_value_p99",
-        "approximate_discrete_booking_value_p99",
-        "average_booking_value",
-        "bookers",
-        "booking_payments",
-        "booking_value",
-        "booking_value_p99",
-        "bookings",
-        "current_account_balance_by_user",
-        "discrete_booking_value_p99",
-        "identity_verifications",
-        "instant_bookings",
-        "largest_listing",
-        "listings",
-        "max_booking_value",
-        "median_booking_value",
-        "min_booking_value",
-        "referred_bookings",
-        "smallest_listing",
-        "total_account_balance_first_day",
-        "txn_revenue",
-        "views",
-    ]
-    assert sorted([m.element_name for m in semantic_model_lookup.measure_references]) == expected
-
-    expected = [
-        "company",
-        "guest",
-        "host",
-        "listing",
-        "lux_listing",
-        "user",
-        "verification",
-    ]
-    assert sorted([i.element_name for i in semantic_model_lookup.get_entity_references()]) == expected
+def test_get_names(  # noqa: D
+    request: FixtureRequest,
+    mf_test_session_state: MetricFlowTestSessionState,
+    semantic_model_lookup: SemanticModelLookup,
+) -> None:
+    assert_object_equal(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        obj_id="result0",
+        obj={
+            "dimension_references": sorted([d.element_name for d in semantic_model_lookup.get_dimension_references()]),
+            "measure_references": sorted([m.element_name for m in semantic_model_lookup.measure_references]),
+            "entity_references": sorted([i.element_name for i in semantic_model_lookup.get_entity_references()]),
+        },
+    )
 
 
 def test_get_elements(semantic_model_lookup: SemanticModelLookup) -> None:  # noqa: D
@@ -109,11 +72,16 @@ def test_get_semantic_models_for_measure(semantic_model_lookup: SemanticModelLoo
     assert listings_sources[0].name == "listings_latest"
 
 
-def test_elements_for_metric(metric_lookup: MetricLookup) -> None:  # noqa: D
-    assert set(
-        [
-            x.qualified_name
-            for x in metric_lookup.element_specs_for_metrics(
+def test_elements_for_metric(  # noqa: D
+    request: FixtureRequest, mf_test_session_state: MetricFlowTestSessionState, metric_lookup: MetricLookup
+) -> None:
+    assert_object_equal(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        obj_id="result0",
+        obj=tuple(
+            spec.qualified_name
+            for spec in metric_lookup.element_specs_for_metrics(
                 [MetricReference(element_name="views")],
                 without_any_property=frozenset(
                     {
@@ -122,65 +90,26 @@ def test_elements_for_metric(metric_lookup: MetricLookup) -> None:  # noqa: D
                     }
                 ),
             )
-        ]
-    ) == {
-        "ds",
-        "ds_partitioned",
-        "listing",
-        "listing__capacity_latest",
-        "listing__country_latest",
-        "listing__created_at",
-        "listing__ds",
-        "listing__is_lux_latest",
-        "listing__lux_listing",
-        "listing__user",
-        "listing__user__company",
-        "listing__user__company_name",
-        "listing__user__created_at",
-        "listing__user__ds_partitioned",
-        "listing__user__home_state",
-        "listing__user__home_state_latest",
-        "user",
-        "user__company",
-        "user__company_name",
-        "user__created_at",
-        "user__ds_partitioned",
-        "user__home_state",
-        "user__home_state_latest",
-    }
-
-    local_specs = metric_lookup.element_specs_for_metrics(
-        metric_references=[MetricReference(element_name="views")],
-        with_any_property=frozenset({LinkableElementProperties.LOCAL}),
-        without_any_property=frozenset({LinkableElementProperties.DERIVED_TIME_GRANULARITY}),
+        ),
     )
-    assert set([x.qualified_name for x in local_specs]) == {
-        "ds",
-        "ds_partitioned",
-        "listing",
-        "user",
-    }
 
 
-def test_local_linked_elements_for_metric(metric_lookup: MetricLookup) -> None:  # noqa: D
-    result = set(
-        [
-            x.qualified_name
-            for x in metric_lookup.element_specs_for_metrics(
+def test_local_linked_elements_for_metric(  # noqa: D
+    request: FixtureRequest, mf_test_session_state: MetricFlowTestSessionState, metric_lookup: MetricLookup
+) -> None:
+    assert_object_equal(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        obj_id="result0",
+        obj=tuple(
+            spec.qualified_name
+            for spec in metric_lookup.element_specs_for_metrics(
                 [MetricReference(element_name="listings")],
                 with_any_property=frozenset({LinkableElementProperties.LOCAL_LINKED}),
                 without_any_property=frozenset({LinkableElementProperties.DERIVED_TIME_GRANULARITY}),
             )
-        ]
+        ),
     )
-
-    assert result == {
-        "listing__created_at",
-        "listing__country_latest",
-        "listing__is_lux_latest",
-        "listing__ds",
-        "listing__capacity_latest",
-    }
 
 
 def test_get_semantic_models_for_entity(semantic_model_lookup: SemanticModelLookup) -> None:  # noqa: D
@@ -189,98 +118,42 @@ def test_get_semantic_models_for_entity(semantic_model_lookup: SemanticModelLook
     assert len(linked_semantic_models) == 8
 
 
-def test_linkable_set(metric_lookup: MetricLookup) -> None:  # noqa: D
-    linkable_set = metric_lookup.linkable_set_for_metrics(
-        (MetricReference(element_name="views"),),
-        without_any_property=frozenset(
-            {
-                LinkableElementProperties.DERIVED_TIME_GRANULARITY,
-                LinkableElementProperties.METRIC_TIME,
-            }
+def test_linkable_set(  # noqa: D
+    request: FixtureRequest, mf_test_session_state: MetricFlowTestSessionState, metric_lookup: MetricLookup
+) -> None:
+    assert_linkable_element_set_equal(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        set_id="result0",
+        linkable_element_set=metric_lookup.linkable_set_for_metrics(
+            (MetricReference(element_name="views"),),
+            without_any_property=frozenset(
+                {
+                    LinkableElementProperties.DERIVED_TIME_GRANULARITY,
+                    LinkableElementProperties.METRIC_TIME,
+                }
+            ),
         ),
     )
 
-    result_to_compare = sorted(
-        tuple(
-            (
-                # Checking a limited set of fields as the result is large due to the paths in the object.
-                tuple(entity_link.element_name for entity_link in linkable_dimension.entity_links),
-                linkable_dimension.element_name,
-                linkable_dimension.semantic_model_origin.semantic_model_name,
-            )
-            for linkable_dimensions in linkable_set.path_key_to_linkable_dimensions.values()
-            for linkable_dimension in linkable_dimensions
-        )
-    )
 
-    assert result_to_compare == [
-        ((), "ds", "views_source"),
-        ((), "ds_partitioned", "views_source"),
-        (("listing",), "capacity_latest", "listings_latest"),
-        (("listing",), "country_latest", "listings_latest"),
-        (("listing",), "created_at", "listings_latest"),
-        (("listing",), "ds", "listings_latest"),
-        (("listing",), "is_lux_latest", "listings_latest"),
-        (("listing", "user"), "company_name", "companies"),
-        (("listing", "user"), "created_at", "users_ds_source"),
-        (("listing", "user"), "ds_partitioned", "users_ds_source"),
-        (("listing", "user"), "home_state", "users_ds_source"),
-        (("listing", "user"), "home_state_latest", "users_latest"),
-        (("user",), "company_name", "companies"),
-        (("user",), "created_at", "users_ds_source"),
-        (("user",), "ds_partitioned", "users_ds_source"),
-        (("user",), "home_state", "users_ds_source"),
-        (("user",), "home_state_latest", "users_latest"),
-    ]
-
-
-def test_linkable_set_for_common_dimensions_in_different_models(metric_lookup: MetricLookup) -> None:  # noqa: D
+def test_linkable_set_for_common_dimensions_in_different_models(
+    request: FixtureRequest, mf_test_session_state: MetricFlowTestSessionState, metric_lookup: MetricLookup
+) -> None:
     """Tests case where a metric has dimensions with the same path.
 
     In this example, "ds" is defined in both "bookings_source" and "views_source".
     """
-    linkable_set = metric_lookup.linkable_set_for_metrics(
-        (MetricReference(element_name="bookings_per_view"),),
-        without_any_property=frozenset(
-            {
-                LinkableElementProperties.DERIVED_TIME_GRANULARITY,
-            }
+    assert_linkable_element_set_equal(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        set_id="result0",
+        linkable_element_set=metric_lookup.linkable_set_for_metrics(
+            (MetricReference(element_name="bookings_per_view"),),
+            without_any_property=frozenset(
+                {
+                    LinkableElementProperties.DERIVED_TIME_GRANULARITY,
+                }
+            ),
         ),
     )
-
-    result_to_compare = sorted(
-        tuple(
-            (
-                # Checking a limited set of fields as the result is large due to the paths in the object.
-                tuple(entity_link.element_name for entity_link in linkable_dimension.entity_links),
-                linkable_dimension.element_name,
-                linkable_dimension.semantic_model_origin.semantic_model_name,
-                # Abbreviated version of the join path.
-                tuple(
-                    join_path_element.semantic_model_reference.semantic_model_name
-                    for join_path_element in linkable_dimension.join_path
-                ),
-            )
-            for linkable_dimensions in linkable_set.path_key_to_linkable_dimensions.values()
-            for linkable_dimension in linkable_dimensions
-        )
-    )
-
-    assert result_to_compare == [
-        ((), "ds", "bookings_source", ()),
-        ((), "ds", "views_source", ()),
-        ((), "ds_partitioned", "bookings_source", ()),
-        ((), "ds_partitioned", "views_source", ()),
-        ((), "metric_time", "bookings_source", ()),
-        ((), "metric_time", "views_source", ()),
-        (("listing",), "capacity_latest", "listings_latest", ("listings_latest",)),
-        (("listing",), "country_latest", "listings_latest", ("listings_latest",)),
-        (("listing",), "created_at", "listings_latest", ()),
-        (("listing",), "ds", "listings_latest", ()),
-        (("listing",), "is_lux_latest", "listings_latest", ("listings_latest",)),
-        (("listing", "user"), "company_name", "companies", ("listings_latest", "companies")),
-        (("listing", "user"), "created_at", "users_ds_source", ()),
-        (("listing", "user"), "ds_partitioned", "users_ds_source", ()),
-        (("listing", "user"), "home_state", "users_ds_source", ("listings_latest", "users_ds_source")),
-        (("listing", "user"), "home_state_latest", "users_latest", ("listings_latest", "users_latest")),
-    ]
