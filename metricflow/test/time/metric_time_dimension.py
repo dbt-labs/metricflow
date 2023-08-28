84fe36c0d7e7ef720c75b3c8da01da9ad1573805
Courtney Holcomb <courtneyeholcomb@gmail.com>
2023-08-28 16:41:18 -0700

    Write dataflow plan tests

--- metricflow/test/time/metric_time_dimension.py
+++ metricflow/test/time/metric_time_dimension.py
@@ -8,5 +8,7 @@ from metricflow.dataset.dataset import DataSet
 MTD = DataSet.metric_time_dimension_name()
 MTD_REFERENCE = DataSet.metric_time_dimension_reference()
 MTD_SPEC_DAY = DataSet.metric_time_dimension_spec(TimeGranularity.DAY)
+MTD_SPEC_WEEK = DataSet.metric_time_dimension_spec(TimeGranularity.WEEK)
 MTD_SPEC_MONTH = DataSet.metric_time_dimension_spec(TimeGranularity.MONTH)
+MTD_SPEC_QUARTER = DataSet.metric_time_dimension_spec(TimeGranularity.QUARTER)
 MTD_SPEC_YEAR = DataSet.metric_time_dimension_spec(TimeGranularity.YEAR)
