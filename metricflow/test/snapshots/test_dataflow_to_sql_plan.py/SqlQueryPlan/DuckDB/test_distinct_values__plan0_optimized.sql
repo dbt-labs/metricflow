87c566e4c81e85d3d46677ac39fead5c2769e45b
Courtney Holcomb <courtneyeholcomb@gmail.com>
2023-10-10 18:38:32 -0700

    Update snapshots

--- metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/DuckDB/test_distinct_values__plan0_optimized.sql
+++ metricflow/test/snapshots/test_dataflow_to_sql_plan.py/SqlQueryPlan/DuckDB/test_distinct_values__plan0_optimized.sql
@@ -1,19 +1,17 @@
--- Join Standard Outputs
--- Pass Only Elements:
---   ['bookings', 'listing__country_latest']
--- Aggregate Measures
--- Compute Metrics via Expressions
--- Pass Only Elements:
---   ['listing__country_latest']
+-- Constrain Output with WHERE
 -- Order By ['listing__country_latest'] Limit 100
 SELECT
-  listings_latest_src_10004.country AS listing__country_latest
-FROM ***************************.fct_bookings bookings_source_src_10001
-LEFT OUTER JOIN
-  ***************************.dim_listings_latest listings_latest_src_10004
-ON
-  bookings_source_src_10001.listing_id = listings_latest_src_10004.listing_id
-GROUP BY
-  listings_latest_src_10004.country
-ORDER BY listing__country_latest
+  listing__country_latest
+FROM (
+  -- Read Elements From Semantic Model 'listings_latest'
+  -- Pass Only Elements:
+  --   ['listing__country_latest']
+  SELECT
+    country AS listing__country_latest
+  FROM ***************************.dim_listings_latest listings_latest_src_10004
+  GROUP BY
+    country
+) subq_4
+WHERE listing__country_latest = 'us'
+ORDER BY listing__country_latest DESC
 LIMIT 100
