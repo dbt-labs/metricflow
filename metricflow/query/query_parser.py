9cd2d096c84b9ca375af2b686aac80183e9f57ea
Devon Fulcher <devonfulcher3@gmail.com>
2023-10-24 19:30:22 -0500

    Re-categorize `TypeErrors` that arise from `create_from_where_filter` into `InvalidQueryException` (#817)
    Resolves https://dbt-labs.slack.com/archives/C04TRSFQN75/p1697568904350429 (internal to dbt Labs)
    
    Description
    The intent of this PR is to re-categorize TypeErrors that arise from create_from_where_filter into InvalidQueryException. That way, these errors can be caught and handled properly. Additionally, I move the try-catch block for ParseWhereFilterException to be within the body of create_from_where_er that way all call sites can benefit from that catch.

--- metricflow/query/query_parser.py
+++ metricflow/query/query_parser.py
@@ -6,7 +6,6 @@ import time
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Sequence, Tuple
 
-from dbt_semantic_interfaces.call_parameter_sets import ParseWhereFilterException
 from dbt_semantic_interfaces.implementations.filters.where_filter import (
     PydanticWhereFilter,
     PydanticWhereFilterIntersection,
@@ -445,14 +444,9 @@ class MetricFlowQueryParser:
         )
         where_filter_spec: Optional[WhereFilterSpec] = None
         if where_filter is not None:
-            try:
-                where_filter_spec = WhereSpecFactory(
-                    column_association_resolver=self._column_association_resolver,
-                ).create_from_where_filter(where_filter)
-            except ParseWhereFilterException as e:
-                raise InvalidQueryException(
-                    f"Error parsing the where filter: {where_filter.where_sql_template}. {e}"
-                ) from e
+            where_filter_spec = WhereSpecFactory(
+                column_association_resolver=self._column_association_resolver,
+            ).create_from_where_filter(where_filter)
 
             where_spec_set = QueryTimeLinkableSpecSet.create_from_linkable_spec_set(where_filter_spec.linkable_spec_set)
             requested_linkable_specs_with_requested_filter_specs = QueryTimeLinkableSpecSet.combine(
