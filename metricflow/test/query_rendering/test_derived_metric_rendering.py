5e6ea1f47f0e5fda9e25ebe6cb67fe67b21bc98c
tlento <tom@transformdata.io>
2023-11-06 19:49:56 -0800

    Add test for derived metric offsets with time filters
    Derived metric offsets handle time filters correctly provided
    the query depends on the `metric_time` dimension. Since the
    DataflowPlanBuilder has an invariant in its runtime to ensure that
    metric_time is included in the requested query specs, this is
    always true in practice. Additional tests for different query
    patterns may be warranted when we expand support.
    
    For the time being, we simply add an illustrative query rendering
    test for the behavior when filter spans are at odds with the
    offset window selection.

--- metricflow/test/query_rendering/test_derived_metric_rendering.py
+++ metricflow/test/query_rendering/test_derived_metric_rendering.py
@@ -4,14 +4,17 @@ from __future__ import annotations
 
 import pytest
 from _pytest.fixtures import FixtureRequest
+from dbt_semantic_interfaces.implementations.filters.where_filter import PydanticWhereFilter
 
 from metricflow.dataflow.builder.dataflow_plan_builder import DataflowPlanBuilder
 from metricflow.plan_conversion.dataflow_to_sql import DataflowToSqlQueryPlanConverter
 from metricflow.protocols.sql_client import SqlClient
+from metricflow.specs.column_assoc import ColumnAssociationResolver
 from metricflow.specs.specs import (
     MetricFlowQuerySpec,
     MetricSpec,
 )
+from metricflow.specs.where_filter_transform import WhereSpecFactory
 from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
 from metricflow.test.query_rendering.compare_rendered_query import convert_and_check
 from metricflow.test.time.metric_time_dimension import MTD_SPEC_DAY, MTD_SPEC_QUARTER, MTD_SPEC_WEEK, MTD_SPEC_YEAR
@@ -77,6 +80,41 @@ def test_derived_metric_with_offset_window(  # noqa: D
         MetricFlowQuerySpec(
             metric_specs=(MetricSpec(element_name="bookings_growth_2_weeks"),),
             time_dimension_specs=(MTD_SPEC_DAY,),
+        )
+    )
+
+    convert_and_check(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        dataflow_to_sql_converter=dataflow_to_sql_converter,
+        sql_client=sql_client,
+        node=dataflow_plan.sink_output_nodes[0].parent_node,
+    )
+
+
+@pytest.mark.sql_engine_snapshot
+def test_derived_metric_with_offset_window_and_time_filter(  # noqa: D
+    request: FixtureRequest,
+    mf_test_session_state: MetricFlowTestSessionState,
+    column_association_resolver: ColumnAssociationResolver,
+    dataflow_plan_builder: DataflowPlanBuilder,
+    dataflow_to_sql_converter: DataflowToSqlQueryPlanConverter,
+    sql_client: SqlClient,
+) -> None:
+    dataflow_plan = dataflow_plan_builder.build_plan(
+        MetricFlowQuerySpec(
+            metric_specs=(MetricSpec(element_name="bookings_growth_2_weeks"),),
+            time_dimension_specs=(MTD_SPEC_DAY,),
+            where_constraint=WhereSpecFactory(
+                column_association_resolver=column_association_resolver,
+            ).create_from_where_filter(
+                PydanticWhereFilter(
+                    where_sql_template=(
+                        "{{ TimeDimension('metric_time', 'day') }} = '2020-01-01' "
+                        "or {{ TimeDimension('metric_time', 'day') }} = '2020-01-14'"
+                    )
+                ),
+            ),
         )
     )
 
