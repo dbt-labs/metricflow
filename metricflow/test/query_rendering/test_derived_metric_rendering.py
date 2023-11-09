5dc9df939604039e77d461818b7f1ace091626dd
Courtney Holcomb <courtneyeholcomb@gmail.com>
2023-11-09 11:43:51 -0800

    Add test for time offset metric based on non-default granularity

--- metricflow/test/query_rendering/test_derived_metric_rendering.py
+++ metricflow/test/query_rendering/test_derived_metric_rendering.py
@@ -17,7 +17,13 @@ from metricflow.specs.specs import (
 from metricflow.specs.where_filter_transform import WhereSpecFactory
 from metricflow.test.fixtures.setup_fixtures import MetricFlowTestSessionState
 from metricflow.test.query_rendering.compare_rendered_query import convert_and_check
-from metricflow.test.time.metric_time_dimension import MTD_SPEC_DAY, MTD_SPEC_QUARTER, MTD_SPEC_WEEK, MTD_SPEC_YEAR
+from metricflow.test.time.metric_time_dimension import (
+    MTD_SPEC_DAY,
+    MTD_SPEC_MONTH,
+    MTD_SPEC_QUARTER,
+    MTD_SPEC_WEEK,
+    MTD_SPEC_YEAR,
+)
 
 
 @pytest.mark.sql_engine_snapshot
@@ -218,6 +224,30 @@ def test_derived_metric_with_offset_window_and_granularity(  # noqa: D
         request=request,
         mf_test_session_state=mf_test_session_state,
         dataflow_to_sql_converter=dataflow_to_sql_converter,
+        sql_client=sql_client,
+        node=dataflow_plan.sink_output_nodes[0].parent_node,
+    )
+
+
+@pytest.mark.sql_engine_snapshot
+def test_derived_metric_with_month_dimension_and_offset_window(  # noqa: D
+    request: FixtureRequest,
+    mf_test_session_state: MetricFlowTestSessionState,
+    extended_date_dataflow_plan_builder: DataflowPlanBuilder,
+    extended_date_dataflow_to_sql_converter: DataflowToSqlQueryPlanConverter,
+    sql_client: SqlClient,
+) -> None:
+    dataflow_plan = extended_date_dataflow_plan_builder.build_plan(
+        MetricFlowQuerySpec(
+            metric_specs=(MetricSpec(element_name="bookings_last_month"),),
+            time_dimension_specs=(MTD_SPEC_MONTH,),
+        )
+    )
+
+    convert_and_check(
+        request=request,
+        mf_test_session_state=mf_test_session_state,
+        dataflow_to_sql_converter=extended_date_dataflow_to_sql_converter,
         sql_client=sql_client,
         node=dataflow_plan.sink_output_nodes[0].parent_node,
     )
