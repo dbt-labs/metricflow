5832b742681947b6163b70c75a267ad177d3f699
Paul Yang <github@ardorful.com>
2023-11-12 17:39:32 -0800

    Add a way to print object + fields in a compact and pretty format.
    This is needed to aid debugging via logs and to make tests easier to write by
    allowing for easy generation of descriptive snapshots. Previously,
    `pformat_big_objects()` was used, but that was too verbose.

--- metricflow/formatting.py
+++ metricflow/formatting.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 import textwrap
 
 
-def indent_log_line(message: str, indent_level: int = 1) -> str:  # noqa: D
-    return textwrap.indent(message, prefix="    " * indent_level)
+def indent_log_line(message: str, indent_level: int = 1, indent_prefix: str = "  ") -> str:  # noqa: D
+    return textwrap.indent(message, prefix=indent_prefix * indent_level)
