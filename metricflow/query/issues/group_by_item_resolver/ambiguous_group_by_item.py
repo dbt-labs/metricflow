5f6dcf571dfbafceb2fb556b2856f16da652f56d
Paul Yang <github@ardorful.com>
2023-11-29 13:03:04 -0800

    Add query issue classes.
    Previously, the query parser would raise an exception for a single issue
    discovered during query parsing. To aid debugging, we're changing this behavior
    to return all errors together so that the user can fix all issues in a single
    pass. This commit adds classes that can represent those issues and also adds a
    way of encapsulating issues into groups.

---
+++ metricflow/query/issues/group_by_item_resolver/ambiguous_group_by_item.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List
+
+from typing_extensions import override
+
+from metricflow.collection_helpers.pretty_print import mf_pformat
+from metricflow.formatting import indent_log_line
+from metricflow.naming.object_builder_scheme import ObjectBuilderNamingScheme
+from metricflow.query.group_by_item.candidate_push_down.group_by_item_candidate import GroupByItemCandidateSet
+from metricflow.query.group_by_item.resolution_dag.resolution_nodes.base_node import GroupByItemResolutionNode
+from metricflow.query.group_by_item.resolution_path import MetricFlowQueryResolutionPath
+from metricflow.query.issues.issues_base import (
+    MetricFlowQueryIssueType,
+    MetricFlowQueryResolutionIssue,
+)
+from metricflow.query.resolver_inputs.base_resolver_inputs import MetricFlowQueryResolverInput
+
+
+@dataclass(frozen=True)
+class AmbiguousGroupByItemIssue(MetricFlowQueryResolutionIssue):
+    """Describes an issue with the query where the input is ambiguous and it can't be resolved."""
+
+    candidate_set: GroupByItemCandidateSet
+
+    @staticmethod
+    def from_parameters(  # noqa: D
+        candidate_set: GroupByItemCandidateSet,
+        query_resolution_path: MetricFlowQueryResolutionPath,
+    ) -> AmbiguousGroupByItemIssue:
+        return AmbiguousGroupByItemIssue(
+            issue_type=MetricFlowQueryIssueType.ERROR,
+            parent_issues=(),
+            candidate_set=candidate_set,
+            query_resolution_path=query_resolution_path,
+        )
+
+    @override
+    def ui_description(self, associated_input: MetricFlowQueryResolverInput) -> str:
+        if associated_input.input_pattern_description is not None:
+            naming_scheme = associated_input.input_pattern_description.naming_scheme
+        else:
+            naming_scheme = ObjectBuilderNamingScheme()
+        candidates_str: List[str] = []
+
+        for spec in self.candidate_set.specs:
+            input_str = naming_scheme.input_str(spec)
+            if input_str is not None:
+                candidates_str.append(input_str)
+            else:
+                candidates_str.append(str(spec))
+        return (
+            f"The given input is ambiguous and can't be resolved. The input could match:\n\n"
+            f"{indent_log_line(mf_pformat(candidates_str))}"
+        )
+
+    @override
+    def with_path_prefix(self, path_prefix_node: GroupByItemResolutionNode) -> AmbiguousGroupByItemIssue:
+        return AmbiguousGroupByItemIssue(
+            issue_type=self.issue_type,
+            parent_issues=tuple(issue.with_path_prefix(path_prefix_node) for issue in self.parent_issues),
+            query_resolution_path=self.query_resolution_path.with_path_prefix(path_prefix_node),
+            candidate_set=self.candidate_set,
+        )
---
+++ metricflow/query/issues/group_by_item_resolver/no_matching_items_for_measure.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Sequence
+
+from typing_extensions import override
+
+from metricflow.query.group_by_item.resolution_dag.resolution_nodes.base_node import GroupByItemResolutionNode
+from metricflow.query.group_by_item.resolution_path import MetricFlowQueryResolutionPath
+from metricflow.query.issues.issues_base import (
+    MetricFlowQueryIssueType,
+    MetricFlowQueryResolutionIssue,
+)
+from metricflow.query.resolver_inputs.base_resolver_inputs import MetricFlowQueryResolverInput
+
+
+@dataclass(frozen=True)
+class NoMatchingItemsForMeasure(MetricFlowQueryResolutionIssue):
+    """Describes an issue with the query where there are no matching items for a measure.
+
+    This can happen if the user specifies a group-by-item that does not exist or is not available for the measure.
+    """
+
+    @staticmethod
+    def from_parameters(  # noqa: D
+        parent_issues: Sequence[MetricFlowQueryResolutionIssue],
+        query_resolution_path: MetricFlowQueryResolutionPath,
+    ) -> NoMatchingItemsForMeasure:
+        return NoMatchingItemsForMeasure(
+            issue_type=MetricFlowQueryIssueType.ERROR,
+            parent_issues=tuple(parent_issues),
+            query_resolution_path=query_resolution_path,
+        )
+
+    @override
+    def ui_description(self, associated_input: MetricFlowQueryResolverInput) -> str:
+        # Could be useful to list the candidates, but it makes the logs very long.
+        return (
+            f"The given input does not match any of the available group-by-items for "
+            f"{self.query_resolution_path.last_item.ui_description}."
+        )
+
+    @override
+    def with_path_prefix(self, path_prefix_node: GroupByItemResolutionNode) -> NoMatchingItemsForMeasure:
+        return NoMatchingItemsForMeasure(
+            issue_type=self.issue_type,
+            parent_issues=tuple(issue.with_path_prefix(path_prefix_node) for issue in self.parent_issues),
+            query_resolution_path=self.query_resolution_path.with_path_prefix(path_prefix_node),
+        )
