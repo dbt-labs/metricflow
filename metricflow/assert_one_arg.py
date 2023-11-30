86d1a3e46ea14564a26af557cb1408675fcc9eaf
Paul Yang <github@ardorful.com>
2023-11-29 23:03:00 -0800

    Add a query resolver and update the query parser to use it.
    * The query resolver takes input classes w/ a spec pattern and maps them to
      concrete specs. Errors with the query are returned as issues.
    * The query parser converts strings / query parameter classes into resolver
      input classes and uses the query resolver to convert them into a
      MetricFlowQuerySpec to match the existing interface.
    * The interface to the query parser is retained for ease of migration, and
      issues are compiled and raised as an exception. The interface to the query
      parser can be updated later so that the individual issues can be inspected as
      objects.

--- metricflow/assert_one_arg.py
+++ metricflow/assert_one_arg.py
@@ -9,3 +9,13 @@ def assert_exactly_one_arg_set(**kwargs) -> None:  # type: ignore
             num_set += 1
 
     assert num_set == 1, f"{num_set} argument(s) set instead of 1 in arguments: {kwargs}"
+
+
+def assert_at_most_one_arg_set(**kwargs) -> None:  # type: ignore
+    """Throws an assertion error if more than 1 argument is not None."""
+    num_set = 0
+    for value in kwargs.values():
+        if value is not None:
+            num_set += 1
+
+    assert num_set <= 1, f"{num_set} argument(s) set instead of <=1 in arguments: {kwargs}"
