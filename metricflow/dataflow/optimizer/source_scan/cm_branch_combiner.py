c482a2e0d64353415d500d76648d94d9f43bd928
Paul Yang <github@ardorful.com>
2023-12-15 13:26:43 -0800

    Fix optimizer to dedupe cases where a metric is used more than once.
    If there are multiple derived metrics in a query that use the same metric, it's
    possible that the same metric is used multiple times. In such cases, the metric
    should be deduped so that we don't get a collision in the colum name.

--- metricflow/dataflow/optimizer/source_scan/cm_branch_combiner.py
+++ metricflow/dataflow/optimizer/source_scan/cm_branch_combiner.py
@@ -27,6 +27,7 @@ from metricflow.dataflow.dataflow_plan import (
     WriteToResultTableNode,
 )
 from metricflow.dataflow.optimizer.source_scan.matching_linkable_specs import MatchingLinkableSpecsTransform
+from metricflow.specs.specs import MetricSpec
 
 logger = logging.getLogger(__name__)
 
@@ -283,9 +284,17 @@ class ComputeMetricsBranchCombiner(DataflowPlanNodeVisitor[ComputeMetricsBranchC
         assert len(combined_parent_nodes) == 1
         combined_parent_node = combined_parent_nodes[0]
         assert combined_parent_node is not None
+
+        # Dedupe (preserving order for output consistency) as it's possible for multiple derived metrics to use the same
+        # metric.
+        unique_metric_specs: List[MetricSpec] = []
+        for metric_spec in self._current_left_node.metric_specs + current_right_node.metric_specs:
+            if metric_spec not in unique_metric_specs:
+                unique_metric_specs.append(metric_spec)
+
         combined_node = ComputeMetricsNode(
             parent_node=combined_parent_node,
-            metric_specs=self._current_left_node.metric_specs + current_right_node.metric_specs,
+            metric_specs=unique_metric_specs,
         )
         self._log_combine_success(
             left_node=self._current_left_node,
