c5a7a3bb72604119a507e3b2cd2a5262dbcf5ef8
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-01-26 14:42:57 -0800

    Update snapshots

---
+++ metricflow/test/snapshots/test_derived_metric_rendering.py/SqlQueryPlan/DuckDB/test_offset_to_grain_with_agg_time_dim__plan0_optimized.sql
@@ -0,0 +1,55 @@
+-- Compute Metrics via Expressions
+SELECT
+  booking__ds__day
+  , bookings - bookings_at_start_of_month AS bookings_growth_since_start_of_month
+FROM (
+  -- Combine Aggregated Outputs
+  SELECT
+    COALESCE(subq_18.booking__ds__day, subq_26.booking__ds__day) AS booking__ds__day
+    , MAX(subq_18.bookings) AS bookings
+    , MAX(subq_26.bookings_at_start_of_month) AS bookings_at_start_of_month
+  FROM (
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
+    SELECT
+      booking__ds__day
+      , SUM(bookings) AS bookings
+    FROM (
+      -- Read Elements From Semantic Model 'bookings_source'
+      -- Metric Time Dimension 'ds'
+      -- Pass Only Elements: ['bookings', 'booking__ds__day']
+      SELECT
+        DATE_TRUNC('day', ds) AS booking__ds__day
+        , 1 AS bookings
+      FROM ***************************.fct_bookings bookings_source_src_10001
+    ) subq_16
+    GROUP BY
+      booking__ds__day
+  ) subq_18
+  FULL OUTER JOIN (
+    -- Join to Time Spine Dataset
+    -- Pass Only Elements: ['bookings', 'booking__ds__day']
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
+    SELECT
+      subq_22.ds AS booking__ds__day
+      , SUM(subq_20.bookings) AS bookings_at_start_of_month
+    FROM ***************************.mf_time_spine subq_22
+    INNER JOIN (
+      -- Read Elements From Semantic Model 'bookings_source'
+      -- Metric Time Dimension 'ds'
+      SELECT
+        DATE_TRUNC('day', ds) AS booking__ds__day
+        , 1 AS bookings
+      FROM ***************************.fct_bookings bookings_source_src_10001
+    ) subq_20
+    ON
+      DATE_TRUNC('month', subq_22.ds) = subq_20.booking__ds__day
+    GROUP BY
+      subq_22.ds
+  ) subq_26
+  ON
+    subq_18.booking__ds__day = subq_26.booking__ds__day
+  GROUP BY
+    COALESCE(subq_18.booking__ds__day, subq_26.booking__ds__day)
+) subq_27
