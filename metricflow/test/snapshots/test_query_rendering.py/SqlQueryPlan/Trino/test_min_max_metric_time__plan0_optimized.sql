2aa5ea53d2f99802a9a82ee0c4f0ced745f502d7
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-02-28 11:15:38 -0800

    Tests for min/max with `metric_time` (#1057)

---
+++ metricflow/test/snapshots/test_query_rendering.py/SqlQueryPlan/Trino/test_min_max_metric_time__plan0_optimized.sql
@@ -0,0 +1,14 @@
+-- Calculate min and max
+SELECT
+  MIN(metric_time__day) AS metric_time__day__min
+  , MAX(metric_time__day) AS metric_time__day__max
+FROM (
+  -- Time Spine
+  -- Metric Time Dimension 'ds'
+  -- Pass Only Elements: ['metric_time__day',]
+  SELECT
+    DATE_TRUNC('day', ds) AS metric_time__day
+  FROM ***************************.mf_time_spine time_spine_src_28000
+  GROUP BY
+    DATE_TRUNC('day', ds)
+) subq_5
