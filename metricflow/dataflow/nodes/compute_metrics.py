30b2daac900f6e6876971aeb0da9fe9c3a33a3e5
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-03-22 17:45:41 -0700

    Add for_group_by_source_node property to ComputeMetricsNode

--- metricflow/dataflow/nodes/compute_metrics.py
+++ metricflow/dataflow/nodes/compute_metrics.py
@@ -17,20 +17,29 @@ from metricflow.visitor import VisitorOutputT
 class ComputeMetricsNode(ComputedMetricsOutput):
     """A node that computes metrics from input measures. Dimensions / entities are passed through."""
 
-    def __init__(self, parent_node: BaseOutput, metric_specs: Sequence[MetricSpec]) -> None:
+    def __init__(
+        self, parent_node: BaseOutput, metric_specs: Sequence[MetricSpec], for_group_by_source_node: bool = False
+    ) -> None:
         """Constructor.
 
         Args:
             parent_node: Node where data is coming from.
             metric_specs: The specs for the metrics that this should compute.
+            for_group_by_source_node: Whether the node is part of a dataflow plan used for a group by source node.
         """
         self._parent_node = parent_node
         self._metric_specs = tuple(metric_specs)
+        self._for_group_by_source_node = for_group_by_source_node
         super().__init__(node_id=self.create_unique_id(), parent_nodes=(self._parent_node,))
 
     @classmethod
     def id_prefix(cls) -> IdPrefix:  # noqa: D102
         return StaticIdPrefix.DATAFLOW_NODE_COMPUTE_METRICS_ID_PREFIX
+
+    @property
+    def for_group_by_source_node(self) -> bool:
+        """Whether or not this node is part of a dataflow plan used for a group by source node."""
+        return self._for_group_by_source_node
 
     @property
     def metric_specs(self) -> Sequence[MetricSpec]:
@@ -61,11 +70,16 @@ class ComputeMetricsNode(ComputedMetricsOutput):
         if other_node.metric_specs != self.metric_specs:
             return False
 
-        return isinstance(other_node, self.__class__) and other_node.metric_specs == self.metric_specs
+        return (
+            isinstance(other_node, self.__class__)
+            and other_node.metric_specs == self.metric_specs
+            and other_node.for_group_by_source_node == self.for_group_by_source_node
+        )
 
     def with_new_parents(self, new_parent_nodes: Sequence[BaseOutput]) -> ComputeMetricsNode:  # noqa: D102
         assert len(new_parent_nodes) == 1
         return ComputeMetricsNode(
             parent_node=new_parent_nodes[0],
             metric_specs=self.metric_specs,
+            for_group_by_source_node=self.for_group_by_source_node,
         )
