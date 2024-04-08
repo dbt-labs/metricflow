f62d43c60e3c40b7732721af801a8fd5c6e671b2
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-04-08 12:55:59 -0700

    Update snapshots

---
+++ tests/snapshots/test_metric_filter_rendering.py/SqlQueryPlan/BigQuery/test_filter_by_metric_in_same_semantic_model_as_queried_metric__plan0_optimized.sql
@@ -0,0 +1,39 @@
+-- Constrain Output with WHERE
+-- Pass Only Elements: ['bookers',]
+-- Aggregate Measures
+-- Compute Metrics via Expressions
+SELECT
+  COUNT(DISTINCT bookers) AS bookers
+FROM (
+  -- Join Standard Outputs
+  -- Pass Only Elements: ['bookers', 'guest__booking_value']
+  SELECT
+    subq_26.booking_value AS guest__booking_value
+    , subq_20.bookers AS bookers
+  FROM (
+    -- Read Elements From Semantic Model 'bookings_source'
+    -- Metric Time Dimension 'ds'
+    -- Pass Only Elements: ['bookers', 'guest']
+    SELECT
+      guest_id AS guest
+      , guest_id AS bookers
+    FROM ***************************.fct_bookings bookings_source_src_28000
+  ) subq_20
+  LEFT OUTER JOIN (
+    -- Read Elements From Semantic Model 'bookings_source'
+    -- Metric Time Dimension 'ds'
+    -- Pass Only Elements: ['booking_value', 'guest']
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
+    -- Pass Only Elements: ['guest', 'booking_value']
+    SELECT
+      guest_id AS guest
+      , SUM(booking_value) AS booking_value
+    FROM ***************************.fct_bookings bookings_source_src_28000
+    GROUP BY
+      guest
+  ) subq_26
+  ON
+    subq_20.guest = subq_26.guest
+) subq_28
+WHERE guest__booking_value > 1.00
