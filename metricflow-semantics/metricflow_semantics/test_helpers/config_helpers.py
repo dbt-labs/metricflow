d6d3cfc8e8b9ea6e7779fffc60a466b5aabdae21
Paul Yang <github@ardorful.com>
2024-04-24 09:37:07 -0700

    Add `DirectoryAnchor` and use new manifest YAML dir.

--- metricflow-semantics/metricflow_semantics/test_helpers/config_helpers.py
+++ metricflow-semantics/metricflow_semantics/test_helpers/config_helpers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pathlib
 from dataclasses import dataclass
 
 from metricflow_semantics.test_helpers.snapshot_helpers import SnapshotConfiguration
@@ -23,3 +24,23 @@ class MetricFlowTestConfiguration(SnapshotConfiguration):
     # is created and persisted between runs. The source schema name includes a hash of the tables that should be in
     # the schema, so
     use_persistent_source_schema: bool
+
+
+class DirectoryPathAnchor:
+    """Defines a directory inside the repo.
+
+    Using this object allows you to avoid using hard-coded paths and instead use objects that will be handled properly
+    during refactoring.
+    """
+
+    def __init__(self, path_to_file_in_directory: str) -> None:
+        """Initializer.
+
+        Args:
+            path_to_file_in_directory: To be used with `__file__`.
+        """
+        self._directory = pathlib.Path(path_to_file_in_directory).parent
+
+    @property
+    def directory(self) -> pathlib.Path:  # noqa: D102
+        return self._directory
