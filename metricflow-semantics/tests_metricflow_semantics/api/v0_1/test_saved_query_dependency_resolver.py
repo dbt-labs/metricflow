847a4d2da03add2b23b937c1efc04f3ab7114d88
Paul Yang <github@ardorful.com>
2024-04-24 15:10:37 -0700

    Add interface for `SavedQueryDependencyResolver`.

---
+++ metricflow-semantics/tests_metricflow_semantics/api/v0_1/test_saved_query_dependency_resolver.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import pytest
+from dbt_semantic_interfaces.implementations.semantic_manifest import (
+    PydanticSemanticManifest,
+)
+from dbt_semantic_interfaces.references import (
+    SemanticModelReference,
+)
+from metricflow_semantics.api.v0_1.saved_query_dependency_resolver import SavedQueryDependencyResolver
+
+
+@pytest.fixture(scope="session")
+def resolver(  # noqa: D103
+    simple_semantic_manifest: PydanticSemanticManifest,
+) -> SavedQueryDependencyResolver:
+    return SavedQueryDependencyResolver(simple_semantic_manifest)
+
+
+def test_saved_query_dependency_resolver(resolver: SavedQueryDependencyResolver) -> None:  # noqa: D103
+    dependency_set = resolver.resolve_dependencies("p0_booking")
+    assert tuple(dependency_set.semantic_model_references) == (
+        SemanticModelReference(semantic_model_name="bookings_source"),
+        SemanticModelReference(semantic_model_name="listings_latest"),
+    )
