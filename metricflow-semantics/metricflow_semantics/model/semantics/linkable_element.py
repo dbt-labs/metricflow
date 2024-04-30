3f2dbc57618a4c7b12fb63b4cd67b077e7cdd0a7
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-04-29 19:29:15 -0700

    Remove validation added in parent PR
    This PR changed how we use the semantic_model_join_path, so this validation is no longer relevant. Now, the semantic_model_join_path does not include the last entity link, since the last join is not joining to a semantic model.

--- metricflow-semantics/metricflow_semantics/model/semantics/linkable_element.py
+++ metricflow-semantics/metricflow_semantics/model/semantics/linkable_element.py
@@ -312,21 +312,10 @@ class MetricSubqueryJoinPath:
     metric_subquery_join_path_element: MetricSubqueryJoinPathElement
     semantic_model_join_path: Optional[SemanticModelJoinPath] = None
 
-    def __post_init__(self) -> None:  # noqa: D105
-        if self.semantic_model_join_path:
-            assert (
-                self.metric_subquery_join_path_element.join_on_entity == self.semantic_model_join_path.last_entity_link
-            ), (
-                "Last entity link for `semantic_model_join_path` must match `semantic_model_join_path.join_on_entity`."
-                "This is the entity used to join the metric subquery to the outer query.",
-            )
-
     @property
     def entity_links(self) -> Tuple[EntityReference, ...]:  # noqa: D102
-        return (
-            self.semantic_model_join_path.entity_links
-            if self.semantic_model_join_path
-            else (self.metric_subquery_join_path_element.join_on_entity,)
+        return (self.semantic_model_join_path.entity_links if self.semantic_model_join_path else ()) + (
+            self.metric_subquery_join_path_element.join_on_entity,
         )
 
     @property
