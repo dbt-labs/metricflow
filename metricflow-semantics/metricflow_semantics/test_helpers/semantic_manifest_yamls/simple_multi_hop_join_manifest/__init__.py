f946a57b630b42477085e73b3f4b500c5def89ea
Paul Yang <github@ardorful.com>
2024-05-05 10:33:41 -0700

    /* STACK_START p-rm-pandas 01 */ Add directory anchors for all semantics manifests.

---
+++ metricflow-semantics/metricflow_semantics/test_helpers/semantic_manifest_yamls/simple_multi_hop_join_manifest/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from metricflow_semantics.test_helpers.config_helpers import DirectoryPathAnchor
+
+SIMPLE_MULTI_HOP_JOIN_MANIFEST_ANCHOR = DirectoryPathAnchor()
