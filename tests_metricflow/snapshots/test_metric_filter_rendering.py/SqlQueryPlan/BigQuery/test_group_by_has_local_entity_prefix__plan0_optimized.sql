e6ae8516b4be75c7030932772b369bfca7fee7dc
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-05-10 10:39:50 -0700

    Add tests for metric filters using local entity prefix (#1194)
    ### Description
    Tests for metric filters where the group by entity uses local entity
    prefix syntax. By "local entity syntax" I mean an entity that is local
    to the metric's source table, but prefixed with the semantic model's
    primary entity.

---
+++ tests_metricflow/snapshots/test_metric_filter_rendering.py/SqlQueryPlan/BigQuery/test_group_by_has_local_entity_prefix__plan0_optimized.sql
@@ -0,0 +1,42 @@
+-- Constrain Output with WHERE
+-- Pass Only Elements: ['listings',]
+-- Aggregate Measures
+-- Compute Metrics via Expressions
+SELECT
+  SUM(listings) AS listings
+FROM (
+  -- Join Standard Outputs
+  -- Pass Only Elements: ['listings', 'user__listing__user__average_booking_value']
+  SELECT
+    subq_41.listing__user__average_booking_value AS user__listing__user__average_booking_value
+    , subq_30.listings AS listings
+  FROM (
+    -- Read Elements From Semantic Model 'listings_latest'
+    -- Metric Time Dimension 'ds'
+    -- Pass Only Elements: ['listings', 'user']
+    SELECT
+      user_id AS user
+      , 1 AS listings
+    FROM ***************************.dim_listings_latest listings_latest_src_28000
+  ) subq_30
+  LEFT OUTER JOIN (
+    -- Join Standard Outputs
+    -- Pass Only Elements: ['average_booking_value', 'listing__user']
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
+    -- Pass Only Elements: ['listing__user', 'listing__user__average_booking_value']
+    SELECT
+      listings_latest_src_28000.user_id AS listing__user
+      , AVG(bookings_source_src_28000.booking_value) AS listing__user__average_booking_value
+    FROM ***************************.fct_bookings bookings_source_src_28000
+    LEFT OUTER JOIN
+      ***************************.dim_listings_latest listings_latest_src_28000
+    ON
+      bookings_source_src_28000.listing_id = listings_latest_src_28000.listing_id
+    GROUP BY
+      listing__user
+  ) subq_41
+  ON
+    subq_30.user = subq_41.listing__user
+) subq_43
+WHERE user__listing__user__average_booking_value > 1
