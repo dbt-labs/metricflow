03d76506fd1348e87883a0b5f58a281481c204ca
Paul Yang <github@ardorful.com>
2024-05-15 13:33:40 -0700

    Update `MetricFlowQueryResolution.queried_semantic_models` for Conversion Metrics (#1204)
    ### Description
    
    I noticed that the `queried_semantic_models` field was not correct for
    conversion metrics and also for queries without any group-by-items. This
    PR addresses those cases.
    
    <!--- 
      Before requesting review, please make sure you have:
    1. read [the contributing
    guide](https://github.com/dbt-labs/metricflow/blob/main/CONTRIBUTING.md),
    2. signed the
    [CLA](https://docs.getdbt.com/docs/contributor-license-agreements)
    3. run `changie new` to [create a changelog
    entry](https://github.com/dbt-labs/metricflow/blob/main/CONTRIBUTING.md#adding-a-changelog-entry)
    -->

--- metricflow-semantics/metricflow_semantics/query/group_by_item/resolution_dag/resolution_nodes/base_node.py
+++ metricflow-semantics/metricflow_semantics/query/group_by_item/resolution_dag/resolution_nodes/base_node.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+import itertools
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Generic, Sequence
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Generic, Sequence, Tuple
 
+from typing_extensions import override
+
+from metricflow_semantics.collection_helpers.merger import Mergeable
 from metricflow_semantics.dag.mf_dag import DagNode, NodeId
 from metricflow_semantics.visitor import Visitable, VisitorOutputT
 
@@ -46,6 +51,22 @@ class GroupByItemResolutionNode(DagNode, Visitable, ABC):
     def parent_nodes(self) -> Sequence[GroupByItemResolutionNode]:  # noqa: D102
         raise NotImplementedError
 
+    @abstractmethod
+    def _self_set(self) -> GroupByItemResolutionNodeSet:
+        """Return a `GroupByItemResolutionNodeInclusiveAncestorSet` only containing self.
+
+        Use to simplify implementation of `inclusive_ancestors`
+        """
+        raise NotImplementedError
+
+    def inclusive_ancestors(self) -> GroupByItemResolutionNodeSet:
+        """Return a set containing itself and all its ancestors."""
+        return GroupByItemResolutionNodeSet.merge_iterable(
+            itertools.chain(
+                [self._self_set()], (parent_node.inclusive_ancestors() for parent_node in self.parent_nodes)
+            )
+        )
+
 
 class GroupByItemResolutionNodeVisitor(Generic[VisitorOutputT], ABC):
     """Visitor for traversing GroupByItemResolutionNodes."""
@@ -65,3 +86,27 @@ class GroupByItemResolutionNodeVisitor(Generic[VisitorOutputT], ABC):
     @abstractmethod
     def visit_query_node(self, node: QueryGroupByItemResolutionNode) -> VisitorOutputT:  # noqa: D102
         raise NotImplementedError
+
+
+@dataclass(frozen=True)
+class GroupByItemResolutionNodeSet(Mergeable):
+    """Set containing nodes in a group-by-item resolution DAG."""
+
+    measure_nodes: Tuple[MeasureGroupByItemSourceNode, ...] = ()
+    no_metrics_query_nodes: Tuple[NoMetricsGroupByItemSourceNode, ...] = ()
+    metric_nodes: Tuple[MetricGroupByItemResolutionNode, ...] = ()
+    query_nodes: Tuple[QueryGroupByItemResolutionNode, ...] = ()
+
+    @override
+    def merge(self, other: GroupByItemResolutionNodeSet) -> GroupByItemResolutionNodeSet:
+        return GroupByItemResolutionNodeSet(
+            measure_nodes=self.measure_nodes + other.measure_nodes,
+            no_metrics_query_nodes=self.no_metrics_query_nodes + other.no_metrics_query_nodes,
+            metric_nodes=self.metric_nodes + other.metric_nodes,
+            query_nodes=self.query_nodes + other.query_nodes,
+        )
+
+    @classmethod
+    @override
+    def empty_instance(cls) -> GroupByItemResolutionNodeSet:
+        return GroupByItemResolutionNodeSet()
