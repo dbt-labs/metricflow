88fb69c1f4c6fd7bfc185db0417597dfb22af6c0
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-05-31 17:11:24 -0700

    Cleanup

--- metricflow-semantics/metricflow_semantics/time/time_constants.py
+++ metricflow-semantics/metricflow_semantics/time/time_constants.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from dbt_semantic_interfaces.type_enums.time_granularity import TimeGranularity
-
 # Python formatting string to use for converting datetime to ISO8601
 ISO8601_PYTHON_FORMAT = "%Y-%m-%d"
 ISO8601_PYTHON_TS_FORMAT = "%Y-%m-%d %H:%M:%S"
