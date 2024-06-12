a92ef2f900c445e8d228118bcad898bdbdc65415
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-06-12 16:07:38 -0700

    Write tests for cumulative metrics queried with multiple agg time dimensions

---
+++ tests_metricflow/snapshots/test_cumulative_metric_rendering.py/SqlQueryPlan/DuckDB/test_cumulative_metric_with_multiple_agg_time_dimensions__plan0_optimized.sql
@@ -0,0 +1,20 @@
+-- Join Self Over Time Range
+-- Pass Only Elements: ['txn_revenue', 'revenue_instance__ds__day', 'revenue_instance__ds__month']
+-- Aggregate Measures
+-- Compute Metrics via Expressions
+SELECT
+  subq_10.ds AS revenue_instance__ds__day
+  , DATE_TRUNC('month', revenue_src_28000.created_at) AS revenue_instance__ds__month
+  , SUM(revenue_src_28000.revenue) AS trailing_2_months_revenue
+FROM ***************************.mf_time_spine subq_10
+INNER JOIN
+  ***************************.fct_revenue revenue_src_28000
+ON
+  (
+    DATE_TRUNC('day', revenue_src_28000.created_at) <= subq_10.ds
+  ) AND (
+    DATE_TRUNC('day', revenue_src_28000.created_at) > subq_10.ds - INTERVAL 2 month
+  )
+GROUP BY
+  subq_10.ds
+  , DATE_TRUNC('month', revenue_src_28000.created_at)
