7c019e039c6504df7784cd1dfcd3c0eccd66f1b7
Will Deng <william.deng14@gmail.com>
2024-06-12 23:35:02 -0400

    changelog

--- tests_metricflow/snapshots/test_data_warehouse_tasks.py/str/BigQuery/test_build_saved_query_tasks__query0.sql
+++ tests_metricflow/snapshots/test_data_warehouse_tasks.py/str/BigQuery/test_build_saved_query_tasks__query0.sql
@@ -1,23 +1,15 @@
--- Constrain Output with WHERE
--- Aggregate Measures
--- Compute Metrics via Expressions
 SELECT
   metric_time__day
   , listing__capacity_latest
   , SUM(bookings) AS bookings
   , SUM(instant_bookings) AS instant_bookings
 FROM (
-  -- Join Standard Outputs
-  -- Pass Only Elements: ['bookings', 'instant_bookings', 'listing__capacity_latest', 'metric_time__day']
   SELECT
     subq_2.metric_time__day AS metric_time__day
     , listings_latest_src_10000.capacity AS listing__capacity_latest
     , subq_2.bookings AS bookings
     , subq_2.instant_bookings AS instant_bookings
   FROM (
-    -- Read Elements From Semantic Model 'bookings_source'
-    -- Metric Time Dimension 'ds'
-    -- Pass Only Elements: ['bookings', 'instant_bookings', 'metric_time__day', 'listing']
     SELECT
       DATE_TRUNC(ds, day) AS metric_time__day
       , listing_id AS listing
