29118c8d2f07ca79c9eed9fe1a711be51c75b720
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-06-20 16:16:54 -0700

    Optimizer bug fix: don't remove select columns that are used in the group by
    Paul had a TODO up for this already, but this appears to be the first circumstance that made handling it necessary.

--- tests_metricflow/snapshots/test_cumulative_metric_rendering.py/SqlQueryPlan/DuckDB/test_window_metric_with_non_default_grain__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_cumulative_metric_rendering.py/SqlQueryPlan/DuckDB/test_window_metric_with_non_default_grain__plan0_optimized.sql
@@ -3,18 +3,19 @@ SELECT
   metric_time__year
   , trailing_2_months_revenue
 FROM (
+  -- Compute Metrics via Expressions
   -- Window Function for Metric Re-aggregation
   SELECT
     metric_time__year
-    , AVG(trailing_2_months_revenue) OVER (PARTITION BY metric_time__year) AS trailing_2_months_revenue
+    , AVG(txn_revenue) OVER (PARTITION BY metric_time__year) AS trailing_2_months_revenue
   FROM (
     -- Join Self Over Time Range
     -- Pass Only Elements: ['txn_revenue', 'metric_time__year', 'metric_time__day']
     -- Aggregate Measures
-    -- Compute Metrics via Expressions
     SELECT
-      subq_11.metric_time__year AS metric_time__year
-      , SUM(revenue_src_28000.revenue) AS trailing_2_months_revenue
+      subq_11.metric_time__day AS metric_time__day
+      , subq_11.metric_time__year AS metric_time__year
+      , SUM(revenue_src_28000.revenue) AS txn_revenue
     FROM (
       -- Time Spine
       SELECT
@@ -36,7 +37,7 @@ FROM (
     GROUP BY
       subq_11.metric_time__day
       , subq_11.metric_time__year
-  ) subq_16
+  ) subq_15
 ) subq_17
 GROUP BY
   metric_time__year
