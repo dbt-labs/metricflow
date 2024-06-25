61de8161b7415b3743a8cdb0f3d0bfc4355b4cf0
tlento <tom@transformdata.io>
2024-06-25 14:55:31 -0700

    Update SQL engine snapshots

--- tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlQueryPlan/BigQuery/test_derived_metric__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlQueryPlan/BigQuery/test_derived_metric__plan0_optimized.sql
@@ -3,49 +3,22 @@ SELECT
   metric_time__day
   , (bookings - ref_bookings) * 1.0 / bookings AS non_referred_bookings_pct
 FROM (
-  -- Combine Aggregated Outputs
+  -- Aggregate Measures
+  -- Compute Metrics via Expressions
   SELECT
-    COALESCE(subq_15.metric_time__day, subq_20.metric_time__day) AS metric_time__day
-    , MAX(subq_15.ref_bookings) AS ref_bookings
-    , MAX(subq_20.bookings) AS bookings
+    metric_time__day
+    , SUM(referred_bookings) AS ref_bookings
+    , SUM(bookings) AS bookings
   FROM (
-    -- Aggregate Measures
-    -- Compute Metrics via Expressions
+    -- Read Elements From Semantic Model 'bookings_source'
+    -- Metric Time Dimension 'ds'
+    -- Pass Only Elements: ['referred_bookings', 'bookings', 'metric_time__day']
     SELECT
-      metric_time__day
-      , SUM(referred_bookings) AS ref_bookings
-    FROM (
-      -- Read Elements From Semantic Model 'bookings_source'
-      -- Metric Time Dimension 'ds'
-      -- Pass Only Elements: ['referred_bookings', 'metric_time__day']
-      SELECT
-        DATETIME_TRUNC(ds, day) AS metric_time__day
-        , CASE WHEN referrer_id IS NOT NULL THEN 1 ELSE 0 END AS referred_bookings
-      FROM ***************************.fct_bookings bookings_source_src_28000
-    ) subq_13
-    GROUP BY
-      metric_time__day
-  ) subq_15
-  FULL OUTER JOIN (
-    -- Aggregate Measures
-    -- Compute Metrics via Expressions
-    SELECT
-      metric_time__day
-      , SUM(bookings) AS bookings
-    FROM (
-      -- Read Elements From Semantic Model 'bookings_source'
-      -- Metric Time Dimension 'ds'
-      -- Pass Only Elements: ['bookings', 'metric_time__day']
-      SELECT
-        DATETIME_TRUNC(ds, day) AS metric_time__day
-        , 1 AS bookings
-      FROM ***************************.fct_bookings bookings_source_src_28000
-    ) subq_18
-    GROUP BY
-      metric_time__day
-  ) subq_20
-  ON
-    subq_15.metric_time__day = subq_20.metric_time__day
+      DATETIME_TRUNC(ds, day) AS metric_time__day
+      , 1 AS bookings
+      , CASE WHEN referrer_id IS NOT NULL THEN 1 ELSE 0 END AS referred_bookings
+    FROM ***************************.fct_bookings bookings_source_src_28000
+  ) subq_13
   GROUP BY
     metric_time__day
-) subq_21
+) subq_15
--- tests_metricflow/snapshots/test_metric_filter_rendering.py/SqlQueryPlan/Redshift/test_query_with_ratio_metric_in_where_filter__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_metric_filter_rendering.py/SqlQueryPlan/Redshift/test_query_with_ratio_metric_in_where_filter__plan0_optimized.sql
@@ -8,7 +8,7 @@ FROM (
   -- Join Standard Outputs
   -- Pass Only Elements: ['listings', 'listing__bookings_per_booker']
   SELECT
-    CAST(subq_34.bookings AS DOUBLE PRECISION) / CAST(NULLIF(subq_34.bookers, 0) AS DOUBLE PRECISION) AS listing__bookings_per_booker
+    CAST(subq_28.bookings AS DOUBLE PRECISION) / CAST(NULLIF(subq_28.bookers, 0) AS DOUBLE PRECISION) AS listing__bookings_per_booker
     , subq_23.listings AS listings
   FROM (
     -- Read Elements From Semantic Model 'listings_latest'
@@ -20,48 +20,26 @@ FROM (
     FROM ***************************.dim_listings_latest listings_latest_src_28000
   ) subq_23
   LEFT OUTER JOIN (
-    -- Combine Aggregated Outputs
+    -- Aggregate Measures
+    -- Compute Metrics via Expressions
     SELECT
-      COALESCE(subq_28.listing, subq_33.listing) AS listing
-      , MAX(subq_28.bookings) AS bookings
-      , MAX(subq_33.bookers) AS bookers
+      listing
+      , SUM(bookings) AS bookings
+      , COUNT(DISTINCT bookers) AS bookers
     FROM (
-      -- Aggregate Measures
-      -- Compute Metrics via Expressions
-      SELECT
-        listing
-        , SUM(bookings) AS bookings
-      FROM (
-        -- Read Elements From Semantic Model 'bookings_source'
-        -- Metric Time Dimension 'ds'
-        -- Pass Only Elements: ['bookings', 'listing']
-        SELECT
-          listing_id AS listing
-          , 1 AS bookings
-        FROM ***************************.fct_bookings bookings_source_src_28000
-      ) subq_26
-      GROUP BY
-        listing
-    ) subq_28
-    FULL OUTER JOIN (
       -- Read Elements From Semantic Model 'bookings_source'
       -- Metric Time Dimension 'ds'
-      -- Pass Only Elements: ['bookers', 'listing']
-      -- Aggregate Measures
-      -- Compute Metrics via Expressions
+      -- Pass Only Elements: ['bookings', 'bookers', 'listing']
       SELECT
         listing_id AS listing
-        , COUNT(DISTINCT guest_id) AS bookers
+        , 1 AS bookings
+        , guest_id AS bookers
       FROM ***************************.fct_bookings bookings_source_src_28000
-      GROUP BY
-        listing_id
-    ) subq_33
-    ON
-      subq_28.listing = subq_33.listing
+    ) subq_26
     GROUP BY
-      COALESCE(subq_28.listing, subq_33.listing)
-  ) subq_34
+      listing
+  ) subq_28
   ON
-    subq_23.listing = subq_34.listing
-) subq_38
+    subq_23.listing = subq_28.listing
+) subq_32
 WHERE listing__bookings_per_booker > 1
