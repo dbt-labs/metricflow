e2c22712d1ae452a86cb838fc809739a9dfc3bfb
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-07-22 11:59:02 -0700

    Tests for `Metric.time_granularity` (#1325)
    Adds `default_granularity` tests for query parsing, SQL rendering, query
    output, and check query tests.
    Also cleans up a couple of comments due to product decisions that
    changed the expected implementation of this feature.
    
    I will add more tests once we finalize the expected behavior for more
    specific scenarios.

--- tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/BigQuery/test_join_to_time_spine_with_filters__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/BigQuery/test_join_to_time_spine_with_filters__plan0_optimized.sql
@@ -3,50 +3,42 @@ SELECT
   metric_time__day
   , COALESCE(bookings, 0) AS bookings_fill_nulls_with_0
 FROM (
-  -- Constrain Output with WHERE
+  -- Join to Time Spine Dataset
   -- Constrain Time Range to [2020-01-03T00:00:00, 2020-01-05T00:00:00]
   SELECT
-    metric_time__day
-    , bookings
+    subq_18.metric_time__day AS metric_time__day
+    , subq_17.bookings AS bookings
   FROM (
-    -- Join to Time Spine Dataset
+    -- Time Spine
     SELECT
-      subq_17.metric_time__day AS metric_time__day
-      , subq_16.bookings AS bookings
+      ds AS metric_time__day
+    FROM ***************************.mf_time_spine subq_19
+    WHERE ds BETWEEN '2020-01-03' AND '2020-01-05'
+  ) subq_18
+  LEFT OUTER JOIN (
+    -- Constrain Output with WHERE
+    -- Pass Only Elements: ['bookings', 'metric_time__day']
+    -- Aggregate Measures
+    SELECT
+      metric_time__day
+      , SUM(bookings) AS bookings
     FROM (
-      -- Time Spine
+      -- Read Elements From Semantic Model 'bookings_source'
+      -- Metric Time Dimension 'ds'
+      -- Constrain Time Range to [2020-01-03T00:00:00, 2020-01-05T00:00:00]
+      -- Pass Only Elements: ['bookings', 'metric_time__day', 'metric_time__week']
       SELECT
-        ds AS metric_time__day
-      FROM ***************************.mf_time_spine subq_18
-      WHERE ds BETWEEN '2020-01-03' AND '2020-01-05'
-    ) subq_17
-    LEFT OUTER JOIN (
-      -- Constrain Output with WHERE
-      -- Aggregate Measures
-      SELECT
-        metric_time__day
-        , SUM(bookings) AS bookings
-      FROM (
-        -- Read Elements From Semantic Model 'bookings_source'
-        -- Metric Time Dimension 'ds'
-        -- Constrain Time Range to [2020-01-03T00:00:00, 2020-01-05T00:00:00]
-        -- Pass Only Elements: ['bookings', 'metric_time__day']
-        SELECT
-          DATETIME_TRUNC(ds, day) AS metric_time__day
-          , 1 AS bookings
-        FROM ***************************.fct_bookings bookings_source_src_28000
-        WHERE DATETIME_TRUNC(ds, day) BETWEEN '2020-01-03' AND '2020-01-05'
-      ) subq_14
-      WHERE metric_time__day > '2020-01-01'
-      GROUP BY
-        metric_time__day
-    ) subq_16
-    ON
-      subq_17.metric_time__day = subq_16.metric_time__day
-  ) subq_19
-  WHERE (
-    metric_time__day BETWEEN '2020-01-03' AND '2020-01-05'
-  ) AND (
-    metric_time__day > '2020-01-01'
-  )
+        DATETIME_TRUNC(ds, day) AS metric_time__day
+        , DATETIME_TRUNC(ds, isoweek) AS metric_time__week
+        , 1 AS bookings
+      FROM ***************************.fct_bookings bookings_source_src_28000
+      WHERE DATETIME_TRUNC(ds, day) BETWEEN '2020-01-03' AND '2020-01-05'
+    ) subq_14
+    WHERE metric_time__week > '2020-01-01'
+    GROUP BY
+      metric_time__day
+  ) subq_17
+  ON
+    subq_18.metric_time__day = subq_17.metric_time__day
+  WHERE subq_18.metric_time__day BETWEEN '2020-01-03' AND '2020-01-05'
 ) subq_21
