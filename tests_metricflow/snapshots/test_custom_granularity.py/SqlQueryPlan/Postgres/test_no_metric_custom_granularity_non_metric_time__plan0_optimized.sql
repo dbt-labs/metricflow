d6b83dd2b977b0229828f02633df64d8bd49aef3
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-09-18 15:38:49 -0700

    Update SQL engine snapshots

---
+++ tests_metricflow/snapshots/test_custom_granularity.py/SqlQueryPlan/Postgres/test_no_metric_custom_granularity_non_metric_time__plan0_optimized.sql
@@ -0,0 +1,12 @@
+-- Read Elements From Semantic Model 'bookings_source'
+-- Join to Custom Granularity Dataset
+-- Pass Only Elements: ['booking__ds__martian_day',]
+SELECT
+  subq_2.martian_day AS booking__ds__martian_day
+FROM ***************************.fct_bookings bookings_source_src_28000
+LEFT OUTER JOIN
+  ***************************.mf_time_spine subq_2
+ON
+  bookings_source_src_28000.booking__ds__day = subq_2.ds
+GROUP BY
+  subq_2.martian_day
