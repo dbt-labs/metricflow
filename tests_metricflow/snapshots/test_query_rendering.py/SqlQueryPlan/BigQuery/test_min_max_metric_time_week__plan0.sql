2a1775fae23e8610ad34594da45cfb084d7f0c87
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-09-18 13:01:00 -0700

    Update SQL engine snapshots

--- tests_metricflow/snapshots/test_query_rendering.py/SqlQueryPlan/BigQuery/test_min_max_metric_time_week__plan0.sql
+++ tests_metricflow/snapshots/test_query_rendering.py/SqlQueryPlan/BigQuery/test_min_max_metric_time_week__plan0.sql
@@ -20,6 +20,7 @@ FROM (
       , subq_0.ds__extract_day
       , subq_0.ds__extract_dow
       , subq_0.ds__extract_doy
+      , subq_0.ds__martian_day
       , subq_0.ds__day AS metric_time__day
       , subq_0.ds__week AS metric_time__week
       , subq_0.ds__month AS metric_time__month
@@ -31,6 +32,7 @@ FROM (
       , subq_0.ds__extract_day AS metric_time__extract_day
       , subq_0.ds__extract_dow AS metric_time__extract_dow
       , subq_0.ds__extract_doy AS metric_time__extract_doy
+      , subq_0.ds__martian_day AS metric_time__martian_day
     FROM (
       -- Time Spine
       SELECT
@@ -45,6 +47,7 @@ FROM (
         , EXTRACT(day FROM time_spine_src_28006.ds) AS ds__extract_day
         , IF(EXTRACT(dayofweek FROM time_spine_src_28006.ds) = 1, 7, EXTRACT(dayofweek FROM time_spine_src_28006.ds) - 1) AS ds__extract_dow
         , EXTRACT(dayofyear FROM time_spine_src_28006.ds) AS ds__extract_doy
+        , time_spine_src_28006.martian_day AS ds__martian_day
       FROM ***************************.mf_time_spine time_spine_src_28006
     ) subq_0
   ) subq_1
