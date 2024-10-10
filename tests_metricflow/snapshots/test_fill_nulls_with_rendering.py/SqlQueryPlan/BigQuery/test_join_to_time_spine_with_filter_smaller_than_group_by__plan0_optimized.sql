83b0de7536d3ef2ba1a3cb1b5147f7ad80ba46d4
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-10-09 18:54:49 -0700

    fixup! Add SQL engine snapshots for new tests

--- tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/BigQuery/test_join_to_time_spine_with_filter_smaller_than_group_by__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/BigQuery/test_join_to_time_spine_with_filter_smaller_than_group_by__plan0_optimized.sql
@@ -8,12 +8,11 @@ FROM (
   SELECT
     ts AS metric_time__hour
     , DATETIME_TRUNC(ts, day) AS metric_time__day
-    , DATETIME_TRUNC(ts, year) AS metric_time__year
   FROM ***************************.mf_time_spine_hour subq_16
   WHERE (
-    metric_time__hour > '2020-01-01 00:00:00'
+    metric_time__hour > '2020-01-01 00:09:00'
   ) AND (
-    metric_time__year < '2021-01-01'
+    metric_time__day = '2020-01-01'
   )
 ) subq_15
 LEFT OUTER JOIN (
@@ -26,17 +25,19 @@ LEFT OUTER JOIN (
   FROM (
     -- Read Elements From Semantic Model 'users_ds_source'
     -- Metric Time Dimension 'archived_at'
-    -- Pass Only Elements: ['archived_users', 'metric_time__day', 'metric_time__hour', 'metric_time__year']
+    -- Pass Only Elements: ['archived_users', 'metric_time__day', 'metric_time__hour']
     SELECT
       DATETIME_TRUNC(archived_at, hour) AS metric_time__hour
       , DATETIME_TRUNC(archived_at, day) AS metric_time__day
-      , DATETIME_TRUNC(archived_at, year) AS metric_time__year
       , 1 AS archived_users
     FROM ***************************.dim_users users_ds_source_src_28000
   ) subq_11
-  WHERE (metric_time__hour > '2020-01-01 00:00:00') AND (metric_time__year < '2021-01-01')
+  WHERE (metric_time__hour > '2020-01-01 00:09:00') AND (metric_time__day = '2020-01-01')
   GROUP BY
     metric_time__day
 ) subq_14
 ON
   subq_15.metric_time__day = subq_14.metric_time__day
+GROUP BY
+  metric_time__day
+  , archived_users_join_to_time_spine
--- tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/Snowflake/test_join_to_time_spine_with_filter_smaller_than_group_by__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlQueryPlan/Snowflake/test_join_to_time_spine_with_filter_smaller_than_group_by__plan0_optimized.sql
@@ -8,12 +8,11 @@ FROM (
   SELECT
     ts AS metric_time__hour
     , DATE_TRUNC('day', ts) AS metric_time__day
-    , DATE_TRUNC('year', ts) AS metric_time__year
   FROM ***************************.mf_time_spine_hour subq_16
   WHERE (
-    metric_time__hour > '2020-01-01 00:00:00'
+    metric_time__hour > '2020-01-01 00:09:00'
   ) AND (
-    metric_time__year < '2021-01-01'
+    metric_time__day = '2020-01-01'
   )
 ) subq_15
 LEFT OUTER JOIN (
@@ -26,17 +25,19 @@ LEFT OUTER JOIN (
   FROM (
     -- Read Elements From Semantic Model 'users_ds_source'
     -- Metric Time Dimension 'archived_at'
-    -- Pass Only Elements: ['archived_users', 'metric_time__day', 'metric_time__hour', 'metric_time__year']
+    -- Pass Only Elements: ['archived_users', 'metric_time__day', 'metric_time__hour']
     SELECT
       DATE_TRUNC('hour', archived_at) AS metric_time__hour
       , DATE_TRUNC('day', archived_at) AS metric_time__day
-      , DATE_TRUNC('year', archived_at) AS metric_time__year
       , 1 AS archived_users
     FROM ***************************.dim_users users_ds_source_src_28000
   ) subq_11
-  WHERE (metric_time__hour > '2020-01-01 00:00:00') AND (metric_time__year < '2021-01-01')
+  WHERE (metric_time__hour > '2020-01-01 00:09:00') AND (metric_time__day = '2020-01-01')
   GROUP BY
     metric_time__day
 ) subq_14
 ON
   subq_15.metric_time__day = subq_14.metric_time__day
+GROUP BY
+  subq_15.metric_time__day
+  , subq_14.archived_users
