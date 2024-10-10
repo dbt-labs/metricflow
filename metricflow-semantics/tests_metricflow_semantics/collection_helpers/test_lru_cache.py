403cfe3f01cab3c0ae963932330b2c89ed44f0b3
Paul Yang <github@ardorful.com>
2024-10-10 09:39:07 -0700

    Add LRU cache to resolve issues with `fuctools.lru_cache` (#1446)
    There is a potential memory leak when decorating class instance methods
    with `fuctools.lru_cache` as `fuctools.lru_cache` holds a reference to
    the class instance. This PR adds a LRU cache class that can be declared
    within the instance so that it's GCed when the instance is GCed. This
    will be used in later PRs to update existing caches / add new caches.

---
+++ metricflow-semantics/tests_metricflow_semantics/collection_helpers/test_lru_cache.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from metricflow_semantics.collection_helpers.lru_cache import LruCache
+
+
+def test_lru_cache() -> None:  # noqa: D103
+    cache = LruCache[str, str](max_cache_items=2)
+    cache.set("key_0", "value_0")
+    cache.set("key_1", "value_1")
+    cache.set("key_2", "value_2")
+
+    # This should evict "key_0".
+    assert cache.get("key_0") is None
+
+    # Get "key_1" so that it's not evicted next.
+    assert cache.get("key_1") == "value_1"
+
+    # This should evict "key_2".
+    cache.set("key_0", "value_0")
+    assert cache.get("key_2") is None
