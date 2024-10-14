751d64dc0b1815790cd6014db74c5513ecfd0c79
Paul Yang <github@ardorful.com>
2024-10-14 13:43:01 -0700

    Add rendering test for CTEs.

---
+++ tests_metricflow/snapshots/test_render_cte.py/SqlQueryPlan/test_render_cte__plan_0.sql
@@ -0,0 +1,23 @@
+-- cte_test
+WITH cte_0 AS (
+  -- cte_select_0
+  SELECT
+    cte_source_table_0.col_0
+  FROM demo.cte_source_table_0 cte_source_table_0
+)
+
+, cte_1 AS (
+  -- cte_select_1
+  SELECT
+    cte_source_table_1.col_1
+  FROM demo.cte_source_table_1 cte_source_table_1
+)
+
+SELECT
+  cte_0.col_0 AS col_0
+  , cte_1.col_1 AS col_1
+FROM cte_0 cte_0
+LEFT OUTER JOIN
+  cte_1 cte_1
+ON
+  cte_0.col_0 = cte_1.col_1
