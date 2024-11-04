6eca34b71d98f2cb4da5b6b070cb74a72a472604
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-11-04 15:49:31 -0800

    Add tests for join_to_timespine metrics with custom grain in the group by

--- tests_metricflow/query_rendering/test_custom_granularity.py
+++ tests_metricflow/query_rendering/test_custom_granularity.py
@@ -532,3 +532,27 @@ def test_conversion_metric_with_custom_granularity_filter_not_in_group_by(  # no
         dataflow_plan_builder=dataflow_plan_builder,
         query_spec=query_spec,
     )
+
+
+@pytest.mark.sql_engine_snapshot
+def test_join_to_time_spine_metric_grouped_by_custom_grain(  # noqa: D103
+    request: FixtureRequest,
+    mf_test_configuration: MetricFlowTestConfiguration,
+    dataflow_plan_builder: DataflowPlanBuilder,
+    dataflow_to_sql_converter: DataflowToSqlQueryPlanConverter,
+    sql_client: SqlClient,
+    query_parser: MetricFlowQueryParser,
+) -> None:
+    query_spec = query_parser.parse_and_validate_query(
+        metric_names=("bookings_join_to_time_spine",),
+        group_by_names=("metric_time__martian_day",),
+    ).query_spec
+
+    render_and_check(
+        request=request,
+        mf_test_configuration=mf_test_configuration,
+        dataflow_to_sql_converter=dataflow_to_sql_converter,
+        sql_client=sql_client,
+        dataflow_plan_builder=dataflow_plan_builder,
+        query_spec=query_spec,
+    )
