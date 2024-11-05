2fe92cc879d4fc3f7e7d2207b55fb071ac1ac5a4
Paul Yang <github@ardorful.com>
2024-11-05 12:04:32 -0800

    /* PR_START p--misc-improvements 02 */ Include SQL engine name in snapshot headers.

--- tests_metricflow/sql/compare_sql_plan.py
+++ tests_metricflow/sql/compare_sql_plan.py
@@ -5,6 +5,7 @@ from metricflow_semantics.dag.mf_dag import DagId
 from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
 from metricflow_semantics.test_helpers.snapshot_helpers import (
     assert_plan_snapshot_text_equal,
+    assert_snapshot_text_equal,
     make_schema_replacement_function,
 )
 
@@ -12,6 +13,10 @@ from metricflow.protocols.sql_client import SqlClient
 from metricflow.sql.render.sql_plan_renderer import DefaultSqlQueryPlanRenderer
 from metricflow.sql.sql_plan import SqlQueryPlan, SqlQueryPlanNode
 from tests_metricflow.fixtures.setup_fixtures import check_sql_engine_snapshot_marker
+from tests_metricflow.snapshot_utils import (
+    _EXCLUDE_TABLE_ALIAS_REGEX,
+    SQL_ENGINE_HEADER_NAME,
+)
 
 
 def assert_default_rendered_sql_equal(
@@ -63,16 +68,20 @@ def assert_rendered_sql_from_plan_equal(
 
     rendered_sql = sql_client.sql_query_plan_renderer.render_sql_query_plan(sql_query_plan).sql
 
-    assert_plan_snapshot_text_equal(
+    sql_engine = sql_client.sql_engine_type
+    assert_snapshot_text_equal(
         request=request,
         mf_test_configuration=mf_test_configuration,
-        plan=sql_query_plan,
-        plan_snapshot_text=rendered_sql,
-        plan_snapshot_file_extension=".sql",
+        group_id=sql_query_plan.__class__.__name__,
+        snapshot_id=sql_query_plan.dag_id.id_str,
+        snapshot_text=rendered_sql,
+        snapshot_file_extension=".sql",
         incomparable_strings_replacement_function=make_schema_replacement_function(
             system_schema=mf_test_configuration.mf_system_schema, source_schema=mf_test_configuration.mf_source_schema
         ),
-        additional_sub_directories_for_snapshots=(sql_client.sql_engine_type.value,) if sql_client else (),
+        exclude_line_regex=_EXCLUDE_TABLE_ALIAS_REGEX,
+        additional_sub_directories_for_snapshots=(sql_engine.value,),
+        additional_header_fields={SQL_ENGINE_HEADER_NAME: sql_engine.value},
     )
 
 
