797b854a147fea30342abdf16583c8f38d800b09
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-11-22 17:00:49 -0800

    Update DuckDB snapshots

--- tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlQueryPlan/DuckDB/test_derived_offset_metric_with_agg_time_dim__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlQueryPlan/DuckDB/test_derived_offset_metric_with_agg_time_dim__plan0_optimized.sql
@@ -19,25 +19,25 @@ SELECT
 FROM (
   -- Combine Aggregated Outputs
   SELECT
-    COALESCE(subq_21.booking__ds__day, subq_25.booking__ds__day) AS booking__ds__day
-    , MAX(subq_21.booking_value) AS booking_value
-    , MAX(subq_25.bookers) AS bookers
+    COALESCE(subq_23.booking__ds__day, subq_27.booking__ds__day) AS booking__ds__day
+    , MAX(subq_23.booking_value) AS booking_value
+    , MAX(subq_27.bookers) AS bookers
   FROM (
     -- Join to Time Spine Dataset
     -- Pass Only Elements: ['booking_value', 'booking__ds__day']
     -- Aggregate Measures
     -- Compute Metrics via Expressions
     SELECT
-      subq_17.ds AS booking__ds__day
+      time_spine_src_28006.ds AS booking__ds__day
       , SUM(sma_28009_cte.booking_value) AS booking_value
-    FROM ***************************.mf_time_spine subq_17
+    FROM ***************************.mf_time_spine time_spine_src_28006
     INNER JOIN
       sma_28009_cte sma_28009_cte
     ON
-      subq_17.ds - INTERVAL 1 week = sma_28009_cte.booking__ds__day
+      time_spine_src_28006.ds - INTERVAL 1 week = sma_28009_cte.booking__ds__day
     GROUP BY
-      subq_17.ds
-  ) subq_21
+      time_spine_src_28006.ds
+  ) subq_23
   FULL OUTER JOIN (
     -- Read From CTE For node_id=sma_28009
     -- Pass Only Elements: ['bookers', 'booking__ds__day']
@@ -49,9 +49,9 @@ FROM (
     FROM sma_28009_cte sma_28009_cte
     GROUP BY
       booking__ds__day
-  ) subq_25
+  ) subq_27
   ON
-    subq_21.booking__ds__day = subq_25.booking__ds__day
+    subq_23.booking__ds__day = subq_27.booking__ds__day
   GROUP BY
-    COALESCE(subq_21.booking__ds__day, subq_25.booking__ds__day)
-) subq_26
+    COALESCE(subq_23.booking__ds__day, subq_27.booking__ds__day)
+) subq_28
--- tests_metricflow/snapshots/test_granularity_date_part_rendering.py/SqlQueryPlan/DuckDB/test_subdaily_offset_window_metric__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_granularity_date_part_rendering.py/SqlQueryPlan/DuckDB/test_subdaily_offset_window_metric__plan0_optimized.sql
@@ -12,9 +12,9 @@ FROM (
   -- Aggregate Measures
   -- Compute Metrics via Expressions
   SELECT
-    subq_11.ts AS metric_time__hour
-    , SUM(subq_9.archived_users) AS archived_users
-  FROM ***************************.mf_time_spine_hour subq_11
+    time_spine_src_28005.ts AS metric_time__hour
+    , SUM(subq_10.archived_users) AS archived_users
+  FROM ***************************.mf_time_spine_hour time_spine_src_28005
   INNER JOIN (
     -- Read Elements From Semantic Model 'users_ds_source'
     -- Metric Time Dimension 'archived_at'
@@ -22,9 +22,9 @@ FROM (
       DATE_TRUNC('hour', archived_at) AS metric_time__hour
       , 1 AS archived_users
     FROM ***************************.dim_users users_ds_source_src_28000
-  ) subq_9
+  ) subq_10
   ON
-    subq_11.ts - INTERVAL 1 hour = subq_9.metric_time__hour
+    time_spine_src_28005.ts - INTERVAL 1 hour = subq_10.metric_time__hour
   GROUP BY
-    subq_11.ts
-) subq_15
+    time_spine_src_28005.ts
+) subq_17
