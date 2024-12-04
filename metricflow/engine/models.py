0d38d3f859e20bd6ad3ea1adb7875124b73a4a6c
Will Deng <william.deng14@gmail.com>
2024-12-04 12:04:57 -0500

    Updated to use correct values

--- metricflow/engine/models.py
+++ metricflow/engine/models.py
@@ -16,7 +16,12 @@ from dbt_semantic_interfaces.protocols.export import Export
 from dbt_semantic_interfaces.protocols.measure import MeasureAggregationParameters
 from dbt_semantic_interfaces.protocols.metadata import Metadata
 from dbt_semantic_interfaces.protocols.metric import Metric as SemanticManifestMetric
-from dbt_semantic_interfaces.protocols.metric import MetricConfig, MetricInputMeasure, MetricType, MetricTypeParams
+from dbt_semantic_interfaces.protocols.metric import (
+    MetricInputMeasure,
+    MetricType,
+    MetricTypeParams,
+    SemanticLayerElementConfig,
+)
 from dbt_semantic_interfaces.protocols.saved_query import (
     SavedQuery as SemanticManifestSavedQuery,
 )
@@ -44,7 +49,7 @@ class Metric:
     metadata: Optional[Metadata]
     dimensions: List[Dimension]
     label: Optional[str]
-    config: Optional[MetricConfig]
+    config: Optional[SemanticLayerElementConfig]
 
     @classmethod
     def from_pydantic(cls, pydantic_metric: SemanticManifestMetric, dimensions: List[Dimension]) -> Metric:
