c142551356c08b457b9975b90639bd8dbc566c5b
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-12-18 09:44:54 -0800

    Update SQL engine snapshots for add time expr test
    Not totally relevant to this PR, but these seem to have been removed somehow.

--- tests_metricflow/snapshots/test_engine_specific_rendering.py/SqlPlan/BigQuery/test_add_time_expr__plan0.sql
+++ tests_metricflow/snapshots/test_engine_specific_rendering.py/SqlPlan/BigQuery/test_add_time_expr__plan0.sql
@@ -6,5 +6,5 @@ sql_engine: BigQuery
 ---
 -- Test Add Time Expression
 SELECT
-  DATE_ADD(CAST('2020-01-01' AS DATETIME), INTERVAL SqlExpressionRenderResult(sql='1', bind_parameter_set=SqlBindParameterSet(param_items=())) quarter) AS add_time
+  DATE_ADD(CAST('2020-01-01' AS DATETIME), INTERVAL 1 quarter) AS add_time
 FROM foo.bar a
