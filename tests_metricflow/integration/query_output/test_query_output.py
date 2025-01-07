dfc47434ed5a334ca39739a7edaa1575d471fd94
Will Deng <william.deng14@gmail.com>
2025-01-07 16:06:51 -0500

    added more test to handle multiple metric alias and error cases

--- tests_metricflow/integration/query_output/test_query_output.py
+++ tests_metricflow/integration/query_output/test_query_output.py
@@ -40,6 +40,36 @@ def test_metric_alias(  # noqa: D103
 
 @pytest.mark.sql_engine_snapshot
 @pytest.mark.duckdb_only
+def test_multiple_metrics_with_alias(  # noqa: D103
+    request: FixtureRequest,
+    mf_test_configuration: MetricFlowTestConfiguration,
+    sql_client: SqlClient,
+    it_helpers: IntegrationTestHelpers,
+) -> None:
+    query_result = it_helpers.mf_engine.query(
+        MetricFlowQueryRequest.create_with_random_request_id(
+            metrics=(
+                MetricParameter(name="bookings", alias="bookings_alias"),
+                MetricParameter(name="booking_fees", alias="bookings_fees_alias"),
+            ),
+            group_by_names=["metric_time__day"],
+            order_by_names=["metric_time__day"],
+            where_constraints=("{{ Metric('bookings', ['listing']) }} > 2",),
+        )
+    )
+    assert query_result.result_df is not None, "Unexpected empty result."
+
+    assert_str_snapshot_equal(
+        request=request,
+        mf_test_configuration=mf_test_configuration,
+        snapshot_id="query_output",
+        snapshot_str=query_result.result_df.text_format(),
+        sql_engine=sql_client.sql_engine_type,
+    )
+
+
+@pytest.mark.sql_engine_snapshot
+@pytest.mark.duckdb_only
 def test_derived_metric_alias(  # noqa: D103
     request: FixtureRequest,
     mf_test_configuration: MetricFlowTestConfiguration,
