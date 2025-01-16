734946a10e322c076aa04c37ccb8af35a642890d
Courtney Holcomb <courtneyeholcomb@gmail.com>
2025-01-15 20:28:57 -0800

    Update snapshots for other engines

---
+++ tests_metricflow/snapshots/test_granularity_date_part_rendering.py/SqlPlan/Redshift/test_metric_time_date_part__plan0_optimized.sql
@@ -0,0 +1,12 @@
+test_name: test_metric_time_date_part
+test_filename: test_granularity_date_part_rendering.py
+sql_engine: Redshift
+---
+-- Read From Time Spine 'mf_time_spine'
+-- Metric Time Dimension 'ds'
+-- Pass Only Elements: ['metric_time__extract_year',]
+SELECT
+  EXTRACT(year FROM ds) AS metric_time__extract_year
+FROM ***************************.mf_time_spine time_spine_src_28006
+GROUP BY
+  EXTRACT(year FROM ds)
