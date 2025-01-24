d10d2638e5b1382c974594a4e097a6e47704a1a3
Lucas Valente <serramatutu@users.noreply.github.com>
2025-01-24 15:05:53 -0300

    Add performance tracking machinery (#1509)
    NOTE: I had to significantly refactor the original version of this PR to
    make it use cProfile, so I changed the description and comments to
    match.
    
    ## Summary
    
    This PR introduces some new testing functionality that we can use to
    avoid performance regressions. From a user perspective, the performance
    benchmarking workflow is:
    1. Create a new test in `tests_metricflow/performance/` that does
    something we want to track performance for. You can use
    `measure_compilation_performance` fixtures to measure the compilation of
    some SQL based on a query spec and a manifest.
    3. Run `make perf` to generate a `performance-report.json`.
    4. Run `make perf-compare A=base-report.json B=other-report.json` to
    generate a Markdown file with a report on changes.
    
    ## The performance report
    
    The `performance-report.json` file contains the "raw" data for a given
    run. It looks like this:
    ```json
    {
        "sessions": {
            "test_simple_manifest.py::test_simple_query": {
                "session_id": "test_simple_manifest.py::test_simple_query",
                "total_time": 0.04,
                "functions": {
                    "source_scan_optimizer.py:115 :: __init__": {
                        "function_name": "source_scan_optimizer.py:115 :: __init__",
                        "base_calls": 1,
                        "total_calls":1,
                        "body_time": 0.01,
                        "per_call_body_time": 0.01,
                        "total_time": 0.03,
                        "per_call_total_time": 0.03
                    },
                    ...
                }
            },
            ...
        }
     }
    ```
    
    Each "report" contains a list of "sessions". A session is a run where
    multiple measurements have been taken, i.e a pytest test. Each session
    contains a variety of "functions", which contain runtime statistics
    about each one of them.
    
    
    ## Report comparison
    
    I created a script called `compare_reports.py` which takes in two
    different reports files and calculates the difference between them (in
    absolute values and also percent). It then generates a markdown file
    called `performance-comparison.md` with a nice little table like the one
    on my first comment.
    
    I added a `time.sleep(1)` to `build_plan()` so that I could get a
    non-negligible result on the first comment.
    
    
    ## How it works
    
    When you run `make perf`, the `measure_compilation_performance` fixture
    instantiates a `PerformanceTracker` and yields a `_measure()` function
    that will compile a query while running cProfile.
    
    The tests then all run, and at the end of it all the fixture compiles
    all reports by calling `perf_tracker.report_set`. This will return all
    the gathered `SessionReport`s. These reports contain _all_ the data from
    _all_ functions.
    
    After this, it writes the report to the JSON file.
    
    Then, when you run `make perf-compare` the script loads both the JSON
    files and uses the `compare()` methods in each of the report classes.
    These will generate `Comparison` classes with the percent and absolute
    differences. The script then renders these classes to markdown and
    writes it to a file. This script filters the comparisons to only show
    the top 10 most relevant comparisons, if their total runtime difference
    non-negligible (i.e the percent difference is higher than `10e-7`). If
    any total runtime has increased by over 10%, it will add warnings (
    :warning: ) to the file.
    
    ## Next steps
    Make a Github Actions bot that runs this on PRs to compare performance
    against `main`, and comments PRs with the generated markdown.

---
+++ tests_metricflow/performance/compare_reports.py
@@ -0,0 +1,162 @@
+from __future__ import annotations
+
+import argparse
+import json
+from io import StringIO
+
+from metricflow_semantics.test_helpers.performance_helpers import (
+    PerformanceMetricComparison,
+    SessionReportSet,
+    SessionReportSetComparison,
+)
+
+REPORT_TOP = 10
+
+NEGLIGIBLE_DELTA = 10e-7
+
+WARNING_PERCENT_THRESHOLD = 0.1
+
+LEGEND_HEADER = f"""
+## Legend
+- **Function**: the function name
+- **Base calls**: number of calls, excluding recursion
+- **Total calls**: number of calls, including recursion
+- **Body time**: time spent on the function body, excluding sub-function calls
+- **Total time**: time spent on the function body and in other sub-functions it calls
+
+`n.d.` means "negligible difference", i.e the difference between measured outputs is negligible, and any differences could be down to imprecision, so the reported value is only the absolute measured value
+
+Warnings ( :warning: ) will be emitted whenever there is more than {WARNING_PERCENT_THRESHOLD*100:.0f}% increase in total runtime of a test.
+""".strip()
+
+
+def _load_report_file(filename: str) -> SessionReportSet:
+    with open(filename, "r") as f:
+        raw = f.read()
+    return SessionReportSet.parse_obj(json.loads(raw))
+
+
+def _is_negligible(val: int | float) -> bool:
+    return abs(val) < NEGLIGIBLE_DELTA
+
+
+def _format_num(num: int | float) -> str:
+    return f"{num:+.3f}" if isinstance(num, float) else str(num)
+
+
+def _format_comp(comp: PerformanceMetricComparison[int] | PerformanceMetricComparison[float], unit: str = "") -> str:
+    """Return a float percent value as (val_as_percent%)."""
+    a_fmt = _format_num(comp.a)
+    b_fmt = _format_num(comp.b)
+    abs_fmt = _format_num(comp.abs)
+
+    if _is_negligible(comp.abs):
+        return f"{a_fmt}{unit} (n.d.)"
+
+    return f"{a_fmt}{unit} - {b_fmt}{unit} = {abs_fmt}{unit} ({comp.pct * 100:+.3f}%)"
+
+
+# I hate this code but there's no real elegant way of creating a markdown file
+def _report_comparison_markdown(
+    base_name: str, other_name: str, comp: SessionReportSetComparison, report_top: int
+) -> str:
+    buf = StringIO()
+
+    buf.write("# Performance comparison\n")
+    buf.write(f"Comparing `{base_name}` against `{other_name}`\n\n")
+    buf.write(
+        f"Reporting top {report_top} highest non-negligible absolute total time differences for each session.\n\n"
+    )
+
+    buf.write(LEGEND_HEADER + "\n\n")
+    buf.write("-------------\n\n")
+
+    for session, session_comp in comp.sessions.items():
+        warn_str = (
+            " :warning: :warning:"
+            if session_comp is not None and session_comp.total_time.pct > WARNING_PERCENT_THRESHOLD
+            else ""
+        )
+
+        buf.write(f"### `{session}`{warn_str}\n\n")
+        if session_comp is None:
+            buf.write("Comparison not available since there's no data for this session in one of the reports.\n\n")
+            continue
+
+        total_time_fmt = _format_comp(session_comp.total_time, "s")
+        buf.write(f"**Total time:** {total_time_fmt}\n\n")
+
+        buf.write(
+            "| i | Function | Base calls | Total calls | Body time (cumulative) | Body time (per call) | Total time (cumulative) | Total time (per call) |\n"
+        )
+        buf.write(
+            "| - | -------- | ---------- | ----------- | ---------------------- | -------------------- | ----------------------- | --------------------- |\n"
+        )
+
+        top_functions = sorted(
+            (
+                (func_name, abs(func_comp.total_time.abs))
+                for func_name, func_comp in session_comp.functions.items()
+                if func_comp is not None and not _is_negligible(func_comp.total_time.abs)
+            ),
+            key=lambda tup: tup[1],
+            reverse=True,
+        )
+
+        for i, (func_name, _) in enumerate(top_functions[0:report_top]):
+            buf.write(f"| #{i+1} | `{func_name}` ")
+
+            func_comp = session_comp.functions[func_name]
+
+            if func_comp is None:
+                buf.write(" | n/a" * 6)
+            else:
+                buf.write("| ")
+                buf.write(
+                    " | ".join(
+                        [
+                            _format_comp(func_comp.base_calls),
+                            _format_comp(func_comp.total_calls),
+                            _format_comp(func_comp.body_time, "s"),
+                            _format_comp(func_comp.per_call_body_time, "s"),
+                            _format_comp(func_comp.total_time, "s"),
+                            _format_comp(func_comp.per_call_total_time, "s"),
+                        ]
+                    )
+                )
+
+            buf.write(" |\n")
+        buf.write("\n\n")
+
+    return buf.getvalue().strip()
+
+
+def main() -> None:  # noqa: D103
+    parser = argparse.ArgumentParser()
+    parser.add_argument("a", help="The base report for the comparison")
+    parser.add_argument("b", help="The other report for the comparison")
+    parser.add_argument("output", help="The output file for the comparison")
+    parser.add_argument(
+        "--top", type=int, default=REPORT_TOP, help="Number of functions to report, sorted by percent difference"
+    )
+
+    args = parser.parse_args()
+
+    a = _load_report_file(args.a)
+    b = _load_report_file(args.b)
+
+    comparison = a.compare(b)
+    md = _report_comparison_markdown(
+        base_name=args.a,
+        other_name=args.b,
+        comp=comparison,
+        report_top=args.top,
+    )
+    with open(args.output, "w") as f:
+        f.write(md)
+
+    print(args.output)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests_metricflow/performance/conftest.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import inspect
+from pathlib import Path
+from typing import Iterator, Protocol
+
+import pytest
+from metricflow_semantics.dag.mf_dag import DagId
+from metricflow_semantics.specs.query_spec import MetricFlowQuerySpec
+from metricflow_semantics.test_helpers.config_helpers import DirectoryPathAnchor
+from metricflow_semantics.test_helpers.performance_helpers import (
+    PerformanceTracker,
+    SessionReport,
+)
+
+from metricflow.dataflow.builder.dataflow_plan_builder import DataflowPlanBuilder
+from metricflow.dataflow.optimizer.dataflow_optimizer_factory import DataflowPlanOptimization
+from metricflow.plan_conversion.dataflow_to_sql import DataflowToSqlPlanConverter
+from metricflow.protocols.sql_client import SqlClient
+from metricflow.sql.optimizer.optimization_levels import SqlOptimizationLevel
+
+ANCHOR = DirectoryPathAnchor()
+
+GLOBAL_TRACKING_CONTEXT = "global"
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    """Add option for performance report file."""
+    parser.addoption(
+        "--output-json",
+        action="store",
+        default="performance-report.json",
+        help="where to store performance results as JSON",
+    )
+
+
+@pytest.fixture(scope="session")
+def perf_output_json(request: pytest.FixtureRequest) -> str:
+    """The output json for the performance test."""
+    return request.config.getoption("--output-json")  # type: ignore
+
+
+class MeasureFixture(Protocol):  # noqa: D101
+    def __call__(  # noqa: D102
+        self,
+        dataflow_plan_builder: DataflowPlanBuilder,
+        dataflow_to_sql_converter: DataflowToSqlPlanConverter,
+        sql_client: SqlClient,
+        query_spec: MetricFlowQuerySpec,
+    ) -> SessionReport:
+        ...
+
+
+@pytest.fixture(scope="session")
+def measure_compilation_performance(
+    perf_output_json: str,
+) -> Iterator[MeasureFixture]:
+    """Fixture that returns a function which measures compilation performance for a given query."""
+    perf_tracker = PerformanceTracker()
+
+    def _measure(
+        dataflow_plan_builder: DataflowPlanBuilder,
+        dataflow_to_sql_converter: DataflowToSqlPlanConverter,
+        sql_client: SqlClient,
+        query_spec: MetricFlowQuerySpec,
+    ) -> SessionReport:
+        caller = inspect.stack()[1]
+        caller_filename = Path(caller.filename).relative_to(ANCHOR.directory)
+        session_id = f"{caller_filename}::{caller.function}"
+
+        with perf_tracker.session(session_id):
+            is_distinct_values_plan = not query_spec.metric_specs
+            if is_distinct_values_plan:
+                optimized_plan = dataflow_plan_builder.build_plan_for_distinct_values(
+                    query_spec, optimizations=DataflowPlanOptimization.enabled_optimizations()
+                )
+            else:
+                optimized_plan = dataflow_plan_builder.build_plan(
+                    query_spec, optimizations=DataflowPlanOptimization.enabled_optimizations()
+                )
+            _ = dataflow_to_sql_converter.convert_to_sql_plan(
+                sql_engine_type=sql_client.sql_engine_type,
+                dataflow_plan_node=optimized_plan.sink_node,
+                optimization_level=SqlOptimizationLevel.O4,
+                sql_query_plan_id=DagId.from_str("plan0_optimized"),
+            )
+
+        return perf_tracker.last_session_report
+
+    yield _measure
+
+    with open(perf_output_json, "w") as f:
+        f.write(perf_tracker.report_set.to_pretty_json())
