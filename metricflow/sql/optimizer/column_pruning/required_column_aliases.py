969eae1a013fadb7dd5f6acbd3ed0c6ee44c8c7b
Paul Yang <github@ardorful.com>
2025-02-20 18:30:46 -0800

    /* PR_START p--sql 02 */ Split loop to reduce nesting.

--- metricflow/sql/optimizer/column_pruning/required_column_aliases.py
+++ metricflow/sql/optimizer/column_pruning/required_column_aliases.py
@@ -234,25 +234,33 @@ class SqlMapRequiredColumnAliasesVisitor(SqlPlanNodeVisitor[None]):
                         column_aliases=aliases_required_in_parent,
                     )
 
-        # For all string columns, assume that they are needed from all sources since we don't have a table alias
-        # in SqlStringExpression.used_columns
+        # Find all column aliases used in string expressions e.g. `col_0 + col_1` represented as a string instead of
+        # `SqlFunctionExpression`.
+        column_aliases_to_retain: Set[str] = set()
         for string_expr in exprs_used_in_this_node.string_exprs:
             if string_expr.used_columns:
-                for column_alias in string_expr.used_columns:
-                    for node_to_retain_columns in (node.from_source,) + tuple(
-                        join_desc.right_source for join_desc in node.join_descs
-                    ):
-                        self._current_required_column_alias_mapping.add_alias(node_to_retain_columns, column_alias)
+                column_aliases_to_retain.update(column_alias for column_alias in string_expr.used_columns)
+
+        # Assume those column aliases are needed from all sources since we don't have a table alias in
+        # `SqlStringExpression.used_columns`.
+        for node_to_retain_columns in (node.from_source,) + tuple(
+            join_desc.right_source for join_desc in node.join_descs
+        ):
+            for column_alias in column_aliases_to_retain:
+                self._current_required_column_alias_mapping.add_alias(node_to_retain_columns, column_alias)
 
         # Same with unqualified column references - it's hard to tell which source it came from, so it's safest to say
         # it's required from all parents.
         # An unqualified column reference expression is like `SELECT col_0` whereas a qualified column reference
         # expression is like `SELECT table_0.col_0`.
+        column_aliases_in_unqualified_column_references: Set[str] = set()
         for unqualified_column_reference_expr in exprs_used_in_this_node.column_alias_reference_exprs:
-            column_alias = unqualified_column_reference_expr.column_alias
-            for node_to_retain_columns in (node.from_source,) + tuple(
-                join_desc.right_source for join_desc in node.join_descs
-            ):
+            column_aliases_in_unqualified_column_references.add(unqualified_column_reference_expr.column_alias)
+
+        for node_to_retain_columns in (node.from_source,) + tuple(
+            join_desc.right_source for join_desc in node.join_descs
+        ):
+            for column_alias in column_aliases_to_retain:
                 self._current_required_column_alias_mapping.add_alias(node_to_retain_columns, column_alias)
 
         # Visit recursively.
