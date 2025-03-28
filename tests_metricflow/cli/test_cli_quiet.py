856a71c2ab3e75e9a962a336d3845186184ef936
Paul Yang <github@ardorful.com>
2025-03-28 15:34:56 -0700

    Use snapshots for CLI tests (#1690)
    This PR updates CLI tests to use snapshots to better capture the
    behavior of the CLI. Snapshots will also facilitate review of later PRs
    that change CLI behavior.

--- tests_metricflow/cli/test_cli_quiet.py
+++ tests_metricflow/cli/test_cli_quiet.py
@@ -8,21 +8,18 @@ from _pytest.fixtures import FixtureRequest
 from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
 
 from dbt_metricflow.cli.main import query
-from metricflow.protocols.sql_client import SqlClient
-from tests_metricflow.cli.conftest import run_and_check_cli_command
+from tests_metricflow.cli.cli_test_helpers import run_and_check_cli_command
 from tests_metricflow.fixtures.cli_fixtures import MetricFlowCliRunner
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.sql_engine_snapshot
 @pytest.mark.duckdb_only
 def test_query(
     mf_test_configuration: MetricFlowTestConfiguration,
     request: FixtureRequest,
     capsys: CaptureFixture,
     cli_runner: MetricFlowCliRunner,
-    sql_client: SqlClient,
 ) -> None:
     """Test that the `--quiet` flag only shows the table when running a query."""
     run_and_check_cli_command(
@@ -32,18 +29,15 @@ def test_query(
         cli_runner=cli_runner,
         command=query,
         args=["--metrics", "bookings", "--group-by", "metric_time", "--order", "metric_time", "--quiet"],
-        sql_client=sql_client,
     )
 
 
-@pytest.mark.sql_engine_snapshot
 @pytest.mark.duckdb_only
 def test_explain(
     mf_test_configuration: MetricFlowTestConfiguration,
     request: FixtureRequest,
     capsys: CaptureFixture,
     cli_runner: MetricFlowCliRunner,
-    sql_client: SqlClient,
 ) -> None:
     """Test that the `--quiet` flag only shows the SQL when explaining a query."""
     run_and_check_cli_command(
@@ -53,5 +47,4 @@ def test_explain(
         cli_runner=cli_runner,
         command=query,
         args=["--metrics", "bookings", "--group-by", "metric_time", "--order", "metric_time", "--explain", "--quiet"],
-        sql_client=sql_client,
     )
