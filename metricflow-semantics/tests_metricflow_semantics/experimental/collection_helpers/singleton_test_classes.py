17096aeddb4d8f6f13300d3f546670359cf252a3
Paul Yang <github@ardorful.com>
2025-04-23 10:03:13 -0700

    Add `Singleton` base class (#1723)
    This experimental PR adds a `Singleton` base class that could improve
    performance of ID-like dataclasses (see tests). This might need some
    additional refinement / performance verification.

---
+++ metricflow-semantics/tests_metricflow_semantics/experimental/collection_helpers/singleton_test_classes.py
@@ -0,0 +1,96 @@
+"""Classes used in tests that benchmark `Singleton` performance."""
+from __future__ import annotations
+
+from collections.abc import Set
+from pathlib import Path
+
+from metricflow_semantics.experimental.dataclass_helpers import fast_frozen_dataclass
+from metricflow_semantics.experimental.singleton import Singleton
+from typing_extensions import Self
+
+PATH_TO_SINGLETON_TEST_CLASS_PY_FILE = Path(__file__)
+
+
+@fast_frozen_dataclass()
+class IdElement:  # noqa: D101
+    int_value: int
+
+
+@fast_frozen_dataclass()
+class CompositeId:  # noqa: D101
+    id_0: IdElement
+    id_1: IdElement
+
+
+@fast_frozen_dataclass()
+class SingletonIdElement(Singleton):  # noqa: D101
+    int_value: int
+
+    @classmethod
+    def get_instance(cls, int_value: int) -> Self:  # noqa: D102
+        return cls._get_singleton_by_kwargs(int_value=int_value)
+
+
+@fast_frozen_dataclass()
+class SingletonCompositeId(Singleton):  # noqa: D101
+    id_0: SingletonIdElement
+    id_1: SingletonIdElement
+
+    @classmethod
+    def get_instance(cls, id_0: SingletonIdElement, id_1: SingletonIdElement) -> Self:  # noqa: D102
+        return cls._get_singleton_by_kwargs(
+            id_0=id_0,
+            id_1=id_1,
+        )
+
+
+def create_id_set(size: int) -> Set[CompositeId]:  # noqa: D103
+    return {
+        CompositeId(
+            id_0=IdElement(int_value=i),
+            id_1=IdElement(int_value=i + 1),
+        )
+        for i in range(size)
+    }
+
+
+def create_singleton_id_set(size: int) -> Set[SingletonCompositeId]:  # noqa: D103
+    return {
+        SingletonCompositeId.get_instance(
+            id_0=SingletonIdElement.get_instance(int_value=i),
+            id_1=SingletonIdElement.get_instance(int_value=i + 1),
+        )
+        for i in range(size)
+    }
+
+
+FIRST_ID = CompositeId(
+    id_0=IdElement(int_value=0),
+    id_1=IdElement(int_value=1),
+)
+
+
+FIRST_SINGLETON_ID = SingletonCompositeId.get_instance(
+    id_0=SingletonIdElement.get_instance(0),
+    id_1=SingletonIdElement.get_instance(1),
+)
+
+
+def create_id_tuple(size: int) -> tuple[CompositeId, ...]:  # noqa: D103
+    return tuple(
+        CompositeId(
+            id_0=IdElement(int_value=i),
+            id_1=IdElement(int_value=i + 1),
+        )
+        for i in range(size)
+    )
+
+
+def create_singleton_id_tuple(size: int) -> tuple[SingletonCompositeId, ...]:  # noqa: D103
+    return tuple(
+        SingletonCompositeId.get_instance(
+            id_0=SingletonIdElement.get_instance(int_value=i),
+            id_1=SingletonIdElement.get_instance(int_value=i + 1),
+        )
+        for i in range(size)
+    )
