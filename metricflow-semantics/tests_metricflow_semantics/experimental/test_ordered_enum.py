38b29d590f07b931cdfec8e7b7c9c1fd6275d5cc
Paul Yang <github@ardorful.com>
2025-05-13 09:43:28 -0700

    Add `OrderedEnum`.

---
+++ metricflow-semantics/tests_metricflow_semantics/experimental/test_ordered_enum.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from metricflow_semantics.experimental.orderd_enum import OrderedEnum
+
+
+class Alphabet(OrderedEnum):  # noqa: D101
+    A = "a"
+    B = "b"
+    C = "c"
+
+
+def test_ordered_enum() -> None:  # noqa: D103
+    assert sorted([Alphabet.C, Alphabet.B, Alphabet.A]) == [Alphabet.A, Alphabet.B, Alphabet.C]
+    assert tuple(Alphabet) == (Alphabet.A, Alphabet.B, Alphabet.C)
