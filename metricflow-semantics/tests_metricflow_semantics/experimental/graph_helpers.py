19ce242b0266961310eec8cbea8b963f7d517b9a
Paul Yang <github@ardorful.com>
2025-05-27 15:41:58 -0700

    /* PR_START p/semantic_graph 32 */ Add graph snapshot.

---
+++ metricflow-semantics/tests_metricflow_semantics/experimental/graph_helpers.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import logging
+from typing import Callable, Optional
+
+from _pytest.fixtures import FixtureRequest
+from metricflow_semantics.experimental.mf_graph.mf_graph import MetricflowGraph
+from metricflow_semantics.mf_logging.pretty_print import mf_pformat_dict
+from metricflow_semantics.test_helpers.snapshot_helpers import (
+    SnapshotConfiguration,
+    assert_str_snapshot_equal,
+)
+
+from tests_metricflow_semantics.experimental.mf_graph.formatting.dot_formatter import DotNotationFormatter
+
+logger = logging.getLogger(__name__)
+
+
+def assert_graph_snapshot_equal(
+    request: FixtureRequest,
+    snapshot_configuration: SnapshotConfiguration,
+    graph: MetricflowGraph,
+    snapshot_id: str = "result",
+    expectation_description: Optional[str] = None,
+    incomparable_strings_replacement_function: Optional[Callable[[str], str]] = None,
+) -> None:
+    """Generate / compare a snapshot of the graph in different formats."""
+    sorted_graph = graph.as_sorted()
+    assert_str_snapshot_equal(
+        request=request,
+        snapshot_configuration=snapshot_configuration,
+        snapshot_str=mf_pformat_dict(
+            description=None,
+            obj_dict={
+                "dot_notation": sorted_graph.format(DotNotationFormatter()),
+                "pretty_format": sorted_graph.format(),
+            },
+        ),
+        snapshot_id=snapshot_id,
+        expectation_description=expectation_description,
+        incomparable_strings_replacement_function=incomparable_strings_replacement_function,
+    )
