a5d07ba0850575a3df74038d1f7a4c2ffca9515f
Paul Yang <github@ardorful.com>
2025-06-02 09:43:18 -0700

    Add SVG formatter.

---
+++ metricflow-semantics/tests_metricflow_semantics/experimental/mf_graph/formatting/svg_formatter.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+import typing
+
+from metricflow_semantics.experimental.metricflow_exception import GraphvizException
+from metricflow_semantics.experimental.mf_graph.formatting.graph_formatter import MetricflowGraphFormatter
+from metricflow_semantics.mf_logging.lazy_formattable import LazyFormat
+from tests_metricflow_semantics.experimental.mf_graph.formatting.mf_to_graphical_dot import (
+    MetricflowGraphToGraphicalDotConverter,
+)
+from typing_extensions import override
+
+if typing.TYPE_CHECKING:
+    from metricflow_semantics.experimental.mf_graph.mf_graph import (
+        MetricflowGraph,
+    )
+
+logger = logging.getLogger(__name__)
+
+
+class SvgFormatter(MetricflowGraphFormatter):
+    """Format a graph as an SVG that can be displayed in a browser."""
+
+    def __init__(self) -> None:  # noqa: D107
+        self._mf_to_dot_graph_converter = MetricflowGraphToGraphicalDotConverter()
+        self._verbose_debug_logs = False
+
+    @override
+    def format_graph(self, graph: MetricflowGraph) -> str:
+        """Format the graph to the SVG image format using `graphviz`."""
+        result = self._mf_to_dot_graph_converter.convert_graph(graph)
+        if self._verbose_debug_logs:
+            logger.debug(
+                LazyFormat(
+                    "Converted graph to graphical DOT",
+                    dot_source=result.dot_graph.source,
+                    dot_elements=result.dot_element_set,
+                )
+            )
+
+        try:
+            return result.dot_graph.pipe(format="svg").decode("utf-8")
+        except Exception as e:
+            raise GraphvizException(
+                LazyFormat(
+                    "Error generating SVG",
+                    result=result,
+                )
+            ) from e
