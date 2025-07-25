e38e7cb3375f19a5672645a8c053ce6140def584
Paul Yang <github@ardorful.com>
2025-07-25 08:21:34 -0700

    Add measure subgraph generator.

---
+++ metricflow-semantics/metricflow_semantics/experimental/semantic_graph/builder/measure_subgraph.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import logging
+
+from typing_extensions import override
+
+from metricflow_semantics.experimental.dsi.measure_model_object_lookup import MeasureContainingModelObjectLookup
+from metricflow_semantics.experimental.semantic_graph.attribute_resolution.attribute_recipe_step import (
+    AttributeRecipeStep,
+)
+from metricflow_semantics.experimental.semantic_graph.builder.subgraph_generator import (
+    SemanticSubgraphGenerator,
+)
+from metricflow_semantics.experimental.semantic_graph.edges.sg_edges import EntityRelationshipEdge
+from metricflow_semantics.experimental.semantic_graph.model_id import SemanticModelId
+from metricflow_semantics.experimental.semantic_graph.nodes.entity_nodes import (
+    LocalModelNode,
+    MeasureNode,
+    MetricTimeNode,
+)
+from metricflow_semantics.experimental.semantic_graph.sg_interfaces import (
+    SemanticGraphEdge,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class MeasureSubgraphGenerator(SemanticSubgraphGenerator):
+    """Generate the subgraph that relates measures to other entities.
+
+    For each measure, this adds the following edges:
+
+    * MeasureNode -> LocalModelNode (corresponding to the semantic model where the measure is defined)
+    * MeasureNode -> MetricTimeNode (edge describes aggregation time grain)
+    """
+
+    @override
+    def add_edges_for_manifest(self, edge_list: list[SemanticGraphEdge]) -> None:
+        for measure_lookup in self._manifest_object_lookup.measure_containing_model_lookups:
+            self._add_edges_for_measure_model(measure_lookup, edge_list)
+
+    def _add_edges_for_measure_model(
+        self, lookup: MeasureContainingModelObjectLookup, edge_list: list[SemanticGraphEdge]
+    ) -> None:
+        model_id = SemanticModelId.get_instance(model_name=lookup.semantic_model.name)
+        local_model_node = LocalModelNode.get_instance(model_id)
+
+        metric_time_node = MetricTimeNode.get_instance()
+        # For each unique aggregation time grain:
+        for aggregation_configuration, measures in lookup.aggregation_configuration_to_measures.items():
+            # For all measures:
+            for measure in measures:
+                measure_name = measure.name
+
+                measure_node = MeasureNode.get_instance(
+                    measure_name=measure_name,
+                    source_model_id=model_id,
+                )
+                # Add an edge from the measure node to the metric-time node.
+                edge_list.append(
+                    EntityRelationshipEdge.create(
+                        tail_node=measure_node,
+                        head_node=metric_time_node,
+                        recipe_update=AttributeRecipeStep(
+                            set_min_time_grain=aggregation_configuration.time_grain,
+                            add_model_join=model_id,
+                        ),
+                    )
+                )
+                # Add an edge from the measure node to the model node.
+                edge_list.append(
+                    EntityRelationshipEdge.create(
+                        tail_node=measure_node,
+                        head_node=local_model_node,
+                    )
+                )
