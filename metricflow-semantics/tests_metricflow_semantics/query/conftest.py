63228333801f3d0f7cd598a781e8eda10d691b9c
Paul Yang <github@ardorful.com>
2025-08-04 14:03:31 -0700

    Add tester for the query parser.

--- metricflow-semantics/tests_metricflow_semantics/query/conftest.py
+++ metricflow-semantics/tests_metricflow_semantics/query/conftest.py
@@ -4,15 +4,19 @@ import textwrap
 from typing import List
 
 import pytest
+from _pytest.fixtures import FixtureRequest
 from dbt_semantic_interfaces.parsing.dir_to_model import parse_yaml_files_to_validation_ready_semantic_manifest
 from dbt_semantic_interfaces.parsing.objects import YamlConfigFile
 from dbt_semantic_interfaces.protocols import SemanticManifest
 from dbt_semantic_interfaces.validations.semantic_manifest_validator import SemanticManifestValidator
 from metricflow_semantics.model.semantic_manifest_lookup import SemanticManifestLookup
 from metricflow_semantics.query.query_parser import MetricFlowQueryParser
+from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
 from metricflow_semantics.test_helpers.example_project_configuration import (
     EXAMPLE_PROJECT_CONFIGURATION_YAML_CONFIG_FILE,
 )
+
+from tests_metricflow_semantics.query.parser_helpers import QueryParserTester
 
 BOOKINGS_YAML = textwrap.dedent(
     """\
@@ -56,13 +60,41 @@ def bookings_query_parser() -> MetricFlowQueryParser:  # noqa
     return query_parser_from_yaml([EXAMPLE_PROJECT_CONFIGURATION_YAML_CONFIG_FILE, bookings_yaml_file])
 
 
-def query_parser_from_yaml(yaml_contents: List[YamlConfigFile]) -> MetricFlowQueryParser:
-    """Given yaml files, return a query parser using default source nodes, resolvers and time spine source."""
+def create_manifest_from_yaml_files(yaml_files: List[YamlConfigFile]) -> SemanticManifest:  # noqa: D103
     semantic_manifest = parse_yaml_files_to_validation_ready_semantic_manifest(
-        yaml_contents, apply_transformations=True
+        yaml_files, apply_transformations=True
     ).semantic_manifest
-    semantic_manifest_lookup = SemanticManifestLookup(semantic_manifest)
-    SemanticManifestValidator[SemanticManifest]().checked_validations(semantic_manifest)
+    SemanticManifestValidator().checked_validations(semantic_manifest)
+    return semantic_manifest
+
+
+def query_parser_from_yaml(yaml_contents: List[YamlConfigFile]) -> MetricFlowQueryParser:
+    """Given YAML files, return a query parser using default source nodes, resolvers and time spine source."""
+    semantic_manifest_lookup = SemanticManifestLookup(create_manifest_from_yaml_files(yaml_contents))
     return MetricFlowQueryParser(
         semantic_manifest_lookup=semantic_manifest_lookup,
+    )
+
+
+@pytest.fixture(scope="module")
+def scd_query_parser(scd_semantic_manifest: SemanticManifest) -> MetricFlowQueryParser:  # noqa: D103
+    return MetricFlowQueryParser(SemanticManifestLookup(scd_semantic_manifest))
+
+
+@pytest.fixture
+def parser_tester_for_scd_manifest(  # noqa: D103
+    request: FixtureRequest, mf_test_configuration: MetricFlowTestConfiguration, scd_semantic_manifest: SemanticManifest
+) -> QueryParserTester:
+    return QueryParserTester(request, mf_test_configuration, scd_semantic_manifest)
+
+
+@pytest.fixture
+def parser_tester_for_bookings_manifest(  # noqa: D103
+    request: FixtureRequest, mf_test_configuration: MetricFlowTestConfiguration
+) -> QueryParserTester:
+    bookings_yaml_file = YamlConfigFile(filepath="inline_for_test_1", contents=BOOKINGS_YAML)
+    return QueryParserTester(
+        request,
+        mf_test_configuration,
+        create_manifest_from_yaml_files([EXAMPLE_PROJECT_CONFIGURATION_YAML_CONFIG_FILE, bookings_yaml_file]),
     )
