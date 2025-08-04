e8114ac1f4b117196f5d45b386a92a2b133313cd
Paul Yang <github@ardorful.com>
2025-08-04 14:05:18 -0700

    Add parser test cases for SCDs.

--- metricflow-semantics/tests_metricflow_semantics/query/errors/test_messages.py
+++ metricflow-semantics/tests_metricflow_semantics/query/errors/test_messages.py
@@ -28,3 +28,11 @@ def test_long_invalid_metric(parser_tester_for_bookings_manifest: QueryParserTes
         group_by_names=["metric_time__day"],
         where_constraint_strs=["{{ Dimension('booking__invalid_dim') }} = '1'"],
     )
+
+
+def test_join_scd_model_to_scd_model(parser_tester_for_scd_manifest: QueryParserTester) -> None:
+    """Check that a join from a measure model -> SCD model -> SCD model raises an error."""
+    parser_tester_for_scd_manifest.assert_error_snapshot(
+        metric_names=["bookings"],
+        group_by_names=["metric_time__day", "listing__user__account_type"],
+    )
