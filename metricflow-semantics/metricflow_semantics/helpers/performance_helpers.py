5eab399c7d349a758ff47ea71bdc27b5a20b3dfc
Paul Yang <github@ardorful.com>
2025-08-06 10:07:20 -0700

    /* PR_START p/semantic_graph 60 */ Allow `LazyFormat` to be passed into `ExecutionTimer`.

--- metricflow-semantics/metricflow_semantics/helpers/performance_helpers.py
+++ metricflow-semantics/metricflow_semantics/helpers/performance_helpers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 import time
-from typing import ContextManager, Optional, Type
+from typing import ContextManager, Optional, Type, Union
 
 from metricflow_semantics.collection_helpers.mf_type_aliases import ExceptionTracebackAnyType
 from metricflow_semantics.helpers.time_helpers import PrettyTimeDelta
@@ -18,14 +18,15 @@ class ExecutionTimer(ContextManager["ExecutionTimer"]):
     This and associates classes are a WIP and may be removed.
     """
 
-    def __init__(self, description: Optional[str] = None) -> None:  # noqa: D107
+    def __init__(self, description: Optional[Union[str, LazyFormat]] = None) -> None:  # noqa: D107
         self._start_time = 0.0
         self._execution_time = 0.0
         self._description = description
 
     def __enter__(self) -> ExecutionTimer:  # noqa: D105
-        if self._description is not None:
-            logger.info(LazyFormat(lambda: f"[  BEGIN  ] {self._description}"))
+        description = self._description
+        if description is not None:
+            logger.info(LazyFormat(lambda: f"[  BEGIN  ] {description}"))
         self._start_time = time.perf_counter()
         return self
 
@@ -39,11 +40,10 @@ class ExecutionTimer(ContextManager["ExecutionTimer"]):
             raise RuntimeError("Context manager shouldn't exit without first entering.")
 
         self._execution_time += time.perf_counter() - self._start_time
-        if self._description is not None:
+        description = self._description
+        if description is not None:
             logger.info(
-                LazyFormat(
-                    lambda: f"[   END   ] {self._description} in {mf_pformat(PrettyTimeDelta(self._execution_time))}s"
-                )
+                LazyFormat(lambda: f"[   END   ] {description} in {mf_pformat(PrettyTimeDelta(self._execution_time))}")
             )
 
     @property
