b1a599e14b959ff5c629b21689f7f4f903777516
Paul Yang <github@ardorful.com>
2025-08-07 11:11:55 -0700

    Add query-parser tester and additional cases for SCDs (#1814)
    This PR adds a tester for the query parser to reduce boilerplate. In
    addition, it adds couple of SCD-related test cases. The cases were
    originally added to check the results with the semantic graph.

--- metricflow-semantics/tests_metricflow_semantics/query/errors/test_messages.py
+++ metricflow-semantics/tests_metricflow_semantics/query/errors/test_messages.py
@@ -4,77 +4,35 @@ TODO: These tests may be better parameterized.
 """
 from __future__ import annotations
 
-from typing import Optional, Sequence
-
-import pytest
-from _pytest.fixtures import FixtureRequest
-from metricflow_semantics.query.query_parser import MetricFlowQueryParser
-from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
-from metricflow_semantics.test_helpers.snapshot_helpers import assert_str_snapshot_equal
+from tests_metricflow_semantics.query.parser_helpers import QueryParserTester
 
 
-def _check_error_message(
-    request: FixtureRequest,
-    mf_test_configuration: MetricFlowTestConfiguration,
-    query_parser: MetricFlowQueryParser,
-    metric_names: Optional[Sequence[str]] = None,
-    group_by_names: Optional[Sequence[str]] = None,
-    where_constraint_strs: Optional[Sequence[str]] = None,
-) -> None:
-    with pytest.raises(Exception) as e:
-        query_parser.parse_and_validate_query(
-            metric_names=metric_names,
-            group_by_names=group_by_names,
-            where_constraint_strs=where_constraint_strs,
-        )
-    assert_str_snapshot_equal(
-        request=request,
-        snapshot_configuration=mf_test_configuration,
-        snapshot_id="result",
-        snapshot_str=str(e.value),
-    )
-
-
-def test_empty_query(
-    request: FixtureRequest,
-    mf_test_configuration: MetricFlowTestConfiguration,
-    bookings_query_parser: MetricFlowQueryParser,
-) -> None:
+def test_empty_query(parser_tester_for_bookings_manifest: QueryParserTester) -> None:
     """Test the error message for an empty query."""
-    _check_error_message(
-        request=request,
-        mf_test_configuration=mf_test_configuration,
-        query_parser=bookings_query_parser,
-    )
+    parser_tester_for_bookings_manifest.assert_error_snapshot()
 
 
-def test_invalid_where_filter(
-    request: FixtureRequest,
-    mf_test_configuration: MetricFlowTestConfiguration,
-    bookings_query_parser: MetricFlowQueryParser,
-) -> None:
+def test_invalid_where_filter(parser_tester_for_bookings_manifest: QueryParserTester) -> None:
     """Test the error message for an invalid where-filter."""
-    _check_error_message(
-        request=request,
-        mf_test_configuration=mf_test_configuration,
-        query_parser=bookings_query_parser,
+    parser_tester_for_bookings_manifest.assert_error_snapshot(
         metric_names=["bookings"],
         group_by_names=["metric_time__day"],
         where_constraint_strs=["{{ Dimension('booking__invalid_dim') }} = '1'"],
     )
 
 
-def test_long_invalid_metric(
-    request: FixtureRequest,
-    mf_test_configuration: MetricFlowTestConfiguration,
-    bookings_query_parser: MetricFlowQueryParser,
-) -> None:
+def test_long_invalid_metric(parser_tester_for_bookings_manifest: QueryParserTester) -> None:
     """Test the error message for an empty query."""
-    _check_error_message(
-        request=request,
-        mf_test_configuration=mf_test_configuration,
-        query_parser=bookings_query_parser,
+    parser_tester_for_bookings_manifest.assert_error_snapshot(
         metric_names=["bookings", "long_invalid_metric_name" + "_" * 50],
         group_by_names=["metric_time__day"],
         where_constraint_strs=["{{ Dimension('booking__invalid_dim') }} = '1'"],
+    )
+
+
+def test_join_scd_model_to_scd_model(parser_tester_for_scd_manifest: QueryParserTester) -> None:
+    """Check that a join from a measure model -> SCD model -> SCD model raises an error."""
+    parser_tester_for_scd_manifest.assert_error_snapshot(
+        metric_names=["bookings"],
+        group_by_names=["metric_time__day", "listing__user__account_type"],
     )
