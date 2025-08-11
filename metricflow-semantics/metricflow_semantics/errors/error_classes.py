3d677a07f26a20a757c5901518805b55a2ba41f8
Paul Yang <github@ardorful.com>
2025-08-11 16:01:43 -0700

    /* PR_START p/semantic_graph 62 */ Rename `UnknownMetricLinkingError`.

--- metricflow-semantics/metricflow_semantics/errors/error_classes.py
+++ metricflow-semantics/metricflow_semantics/errors/error_classes.py
@@ -69,7 +69,7 @@ class SqlBindParametersNotSupportedError(Exception):
     """Raised when a SqlClient that does not have support for bind parameters receives a non-empty set of params."""
 
 
-class UnknownMetricLinkingError(InformativeUserError):
+class UnknownMetricError(InformativeUserError):
     """Raised during linking when a user attempts to use a metric that isn't specified."""
 
 
