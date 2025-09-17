37d5e356cb41a2587d5454b665c970878596c42a
Paul Yang <github@ardorful.com>
2025-09-17 10:51:25 -0700

    Check filtering when checking outputs

--- metricflow-semantics/tests_metricflow_semantics/experimental/semantic_graph/resolver/test_sg_resolver_output.py
+++ metricflow-semantics/tests_metricflow_semantics/experimental/semantic_graph/resolver/test_sg_resolver_output.py
@@ -8,6 +8,7 @@ from dbt_semantic_interfaces.protocols import SemanticManifest
 from dbt_semantic_interfaces.references import MeasureReference, MetricReference
 from metricflow_semantics.model.linkable_element_property import LinkableElementProperty
 from metricflow_semantics.model.semantics.element_filter import LinkableElementFilter
+from metricflow_semantics.model.semantics.linkable_element_set_base import BaseLinkableElementSet
 from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
 
 from tests_metricflow_semantics.experimental.semantic_graph.sg_fixtures import SemanticGraphTestFixture
@@ -41,35 +42,32 @@ def test_set_for_measures(sg_tester: SemanticGraphTester) -> None:
 def test_set_filtering_for_measure(sg_tester: SemanticGraphTester) -> None:
     """Check filtering of the set for a measure."""
     measure_reference = MeasureReference("bookings")
-    complete_set = sg_tester.sg_resolver.get_linkable_element_set_for_measure(measure_reference)
-    for element_property in LinkableElementProperty:
-        with_any_of_filter = LinkableElementFilter(with_any_of=frozenset((element_property,)))
-        filtered_set = sg_tester.sg_resolver.get_linkable_element_set_for_measure(measure_reference, with_any_of_filter)
-        # The resolver uses the filter to limit graph traversal, so this is not the same logic.
-        expected_items = set(complete_set.filter(with_any_of_filter).annotated_specs)
-        actual_items = set(filtered_set.annotated_specs)
-
-        assert expected_items == actual_items
-
-        without_any_of_filter = LinkableElementFilter(without_any_of=frozenset((element_property,)))
-        filtered_set = sg_tester.sg_resolver.get_linkable_element_set_for_measure(
-            measure_reference, without_any_of_filter
-        )
-        expected_items = set(complete_set.filter(without_any_of_filter).annotated_specs)
-        actual_items = set(filtered_set.annotated_specs)
-
-        assert expected_items == actual_items
+    sg_resolver = sg_tester.sg_resolver
+    complete_set = sg_resolver.get_linkable_element_set_for_measure(measure_reference)
+    sg_tester.check_set_filtering(
+        complete_set=complete_set,
+        filtered_set_callable=lambda set_filter: sg_resolver.get_linkable_element_set_for_measure(
+            measure_reference, set_filter
+        ),
+    )
 
 
 def test_set_for_metrics(sg_tester: SemanticGraphTester) -> None:
     """Check the set for a few multi-metric cases."""
-    cases = ((), ("bookings",), ("bookings_per_view",))
-    description_to_set = {
-        str(metric_names): sg_tester.sg_resolver.get_linkable_elements_for_metrics(
-            [MetricReference(metric_name) for metric_name in metric_names]
+    sg_resolver = sg_tester.sg_resolver
+    description_to_set: dict[str, BaseLinkableElementSet] = {}
+
+    for metric_names in ((), ("bookings",), ("bookings_per_view",)):
+        metric_references = [MetricReference(metric_name) for metric_name in metric_names]
+        complete_set = sg_resolver.get_linkable_elements_for_metrics(metric_references)
+        description_to_set[str(metric_names)] = complete_set
+        sg_tester.check_set_filtering(
+            complete_set=complete_set,
+            filtered_set_callable=lambda set_filter: sg_resolver.get_linkable_elements_for_metrics(
+                metric_references, set_filter
+            ),
         )
-        for metric_names in cases
-    }
+
     sg_tester.assert_attribute_set_snapshot_equal(description_to_set)
 
 
