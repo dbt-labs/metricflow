aa9a21d9becc21909c465ec7f1ae440e2cab6fec
Paul Yang <github@ardorful.com>
2025-09-25 14:50:04 -0700

    Remove unused methods

--- metricflow-semantics/metricflow_semantics/model/semantics/linkable_spec_resolver.py
+++ metricflow-semantics/metricflow_semantics/model/semantics/linkable_spec_resolver.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from abc import ABC, abstractmethod
-from typing import Iterable, Optional, Sequence
+from typing import Iterable, Optional
 
 from dbt_semantic_interfaces.references import (
     MeasureReference,
@@ -19,15 +19,6 @@ class GroupByItemSetResolver(ABC):
     """Resolves available group-by items for measures and metrics."""
 
     @abstractmethod
-    def get_linkable_element_set_for_measure(
-        self,
-        measure_reference: MeasureReference,
-        element_filter: GroupByItemSetFilter,
-    ) -> BaseGroupByItemSet:
-        """Get the valid linkable elements for the given measure."""
-        raise NotImplementedError
-
-    @abstractmethod
     def get_set_for_distinct_values_query(
         self,
         element_filter: GroupByItemSetFilter,
@@ -35,20 +26,6 @@ class GroupByItemSetResolver(ABC):
         """Returns queryable items for a distinct group-by-item values query.
 
         A distinct group-by-item values query does not include any metrics.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_linkable_elements_for_metrics(
-        self,
-        metric_references: Sequence[MetricReference],
-        element_filter: GroupByItemSetFilter = GroupByItemSetFilter(),
-    ) -> BaseGroupByItemSet:
-        """Gets the valid linkable elements that are common to all requested metrics.
-
-        The results of this method don't actually match what will be allowed for the metric because resolution goes
-        through a separate and more comprehensive resolution process (`GroupByItemResolver`).
-        # TODO: Consolidate resolution processes.
         """
         raise NotImplementedError
 
