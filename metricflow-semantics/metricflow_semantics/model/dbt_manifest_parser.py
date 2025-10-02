59c000f2a28231f97a9fd2258f97535ab17b6af4
Patrick Yost <patrick.yost@dbtlabs.com>
2025-10-02 12:31:06 -0700

    Get MF using the new dsi transformations

--- metricflow-semantics/metricflow_semantics/model/dbt_manifest_parser.py
+++ metricflow-semantics/metricflow_semantics/model/dbt_manifest_parser.py
@@ -3,16 +3,8 @@ from __future__ import annotations
 from dbt_semantic_interfaces.implementations.semantic_manifest import (
     PydanticSemanticManifest,
 )
-from dbt_semantic_interfaces.transformations.boolean_measure import (
-    BooleanMeasureAggregationRule,
-)
-from dbt_semantic_interfaces.transformations.convert_count import ConvertCountToSumRule
-from dbt_semantic_interfaces.transformations.convert_median import (
-    ConvertMedianToPercentileRule,
-)
-from dbt_semantic_interfaces.transformations.cumulative_type_params import SetCumulativeTypeParamsRule
 from dbt_semantic_interfaces.transformations.names import LowerCaseNamesRule
-from dbt_semantic_interfaces.transformations.proxy_measure import CreateProxyMeasureRule
+from dbt_semantic_interfaces.transformations.pydantic_rule_set import PydanticSemanticManifestTransformRuleSet
 from dbt_semantic_interfaces.transformations.semantic_manifest_transformer import (
     PydanticSemanticManifestTransformer,
 )
@@ -27,17 +19,16 @@ def parse_manifest_from_dbt_generated_manifest(manifest_json_string: str) -> Pyd
     # this time, which causes failures with input measure resolution.
     # TODO: remove this transform call once the upstream changes are integrated into our dependency tree
     # TODO: align rules between DSI, here, and MFS (if possible!)
+    rule_set = PydanticSemanticManifestTransformRuleSet()
     rules = (
         # Primary
         (LowerCaseNamesRule(),),
-        # Secondary
+        # Secondary - broken out into groups because we run DedupeMetricInputMeasuresRule in the middle.
         (
-            CreateProxyMeasureRule(),
-            BooleanMeasureAggregationRule(),
-            ConvertCountToSumRule(),
-            ConvertMedianToPercentileRule(),
+            *rule_set.legacy_measure_update_rules,
             DedupeMetricInputMeasuresRule(),  # Remove once fix is in core
-            SetCumulativeTypeParamsRule(),
+            *rule_set.convert_legacy_measures_to_metrics_rules,
+            *rule_set.general_metric_update_rules,
         ),
     )
     model = PydanticSemanticManifestTransformer.transform(raw_model, rules)
