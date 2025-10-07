41101bdce846170755917bedb54f20fab3e80749
Paul Yang <github@ardorful.com>
2025-10-07 13:01:20 -0700

    /* PR_START p/measure_migration 13 */ Revert "Update snapshots for query updates"
    This reverts commit 958e1e39ed2c8dc44d24848879a37a1dbb86e456.

--- tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlPlan/Postgres/test_join_to_time_spine_with_filter_not_in_group_by_using_agg_time__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_fill_nulls_with_rendering.py/SqlPlan/Postgres/test_join_to_time_spine_with_filter_not_in_group_by_using_agg_time__plan0_optimized.sql
@@ -22,7 +22,7 @@ FROM (
       , DATE_TRUNC('month', ds) AS booking__ds__month
     FROM ***************************.mf_time_spine time_spine_src_28006
   ) subq_17
-  WHERE ((metric_time__day >= '2020-01-02') AND (metric_time__day <= '2020-01-02')) AND (booking__ds__month > '2020-01-01')
+  WHERE (metric_time__day <= '2020-01-02') AND (booking__ds__month > '2020-01-01')
 ) subq_19
 LEFT OUTER JOIN (
   -- Constrain Output with WHERE
