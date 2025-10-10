27d90baf5b877ffa9393e72f738ddfc30e2cbcff
Paul Yang <github@ardorful.com>
2025-10-09 17:00:51 -0700

    Remove unused code

--- metricflow-semantics/metricflow_semantics/aggregation_properties.py
+++ metricflow-semantics/metricflow_semantics/aggregation_properties.py
@@ -2,28 +2,6 @@ from __future__ import annotations
 
 from enum import Enum
 
-from dbt_semantic_interfaces.type_enums.aggregation_type import AggregationType
-
-
-def fill_nulls_with_0(agg_type: AggregationType) -> bool:
-    """Indicates if charts should show 0 instead of null where there are gaps in data."""
-    return agg_type in (
-        AggregationType.SUM,
-        AggregationType.COUNT_DISTINCT,
-        AggregationType.SUM_BOOLEAN,
-        AggregationType.COUNT,
-    )
-
-
-def can_limit_dimension_values(agg_type: AggregationType) -> bool:
-    """Indicates if we can limit dimension values in charts.
-
-    Currently, this means:
-    1. The dimensions we care about most are the ones with the highest numeric values
-    2. We can calculate the "other" column in the postprocessor (meaning the metric is expansive)
-    """
-    return agg_type in (AggregationType.SUM, AggregationType.SUM_BOOLEAN, AggregationType.COUNT)
-
 
 class AggregationState(Enum):
     """Represents how the measure is aggregated."""
