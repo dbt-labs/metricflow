9763cca587c373a00215d9eb5f6c81192fd9ee11
Paul Yang <github@ardorful.com>
2025-12-12 16:03:23 -0800

    Remove migrated test

--- tests_metricflow/engine/test_errors.py
+++ tests_metricflow/engine/test_errors.py
@@ -14,30 +14,6 @@ from tests_metricflow.snapshot_utils import assert_object_snapshot_equal
 logger = logging.getLogger(__name__)
 
 
-def test_simple_dimensions_for_metrics_with_invalid_metric(
-    request: FixtureRequest,
-    mf_test_configuration: MetricFlowTestConfiguration,
-    mf_engine_test_fixture_mapping: Mapping[SemanticManifestSetup, MetricFlowEngineTestFixture],
-) -> None:
-    """Check the exception raised when invalid metric names are provided to `simple_dimensions_for_metrics`."""
-    mf_engine = mf_engine_test_fixture_mapping[SemanticManifestSetup.SIMPLE_MANIFEST].metricflow_engine
-
-    with pytest.raises(Exception) as exception_info_for_one_invalid_metric:
-        mf_engine.simple_dimensions_for_metrics(["invalid_metric"])
-
-    with pytest.raises(Exception) as exception_info_for_multiple_invalid_metrics:
-        mf_engine.simple_dimensions_for_metrics(["invalid_metric_0", "invalid_metric_1"])
-
-    assert_object_snapshot_equal(
-        request=request,
-        mf_test_configuration=mf_test_configuration,
-        obj={
-            "one_invalid_metric": exception_info_for_one_invalid_metric.value,
-            "multiple_invalid_metrics": exception_info_for_multiple_invalid_metrics.value,
-        },
-    )
-
-
 def test_metadata_methods_with_invalid_metics(
     request: FixtureRequest,
     mf_test_configuration: MetricFlowTestConfiguration,
