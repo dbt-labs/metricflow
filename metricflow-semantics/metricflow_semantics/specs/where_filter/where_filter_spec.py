a8307c94f341e6e66766b2932da63c25b6484c47
Paul Yang <github@ardorful.com>
2026-02-26 11:28:24 -0800

    Remove `Mergeable` from `WhereFilterSpec`

--- metricflow-semantics/metricflow_semantics/specs/where_filter/where_filter_spec.py
+++ metricflow-semantics/metricflow_semantics/specs/where_filter/where_filter_spec.py
@@ -5,7 +5,6 @@ from functools import cached_property
 from typing import Tuple
 
 from dbt_semantic_interfaces.dataclass_serialization import SerializableDataclass
-from typing_extensions import override
 
 from metricflow_semantics.semantic_graph.attribute_resolution.group_by_item_set import (
     GroupByItemSet,
@@ -14,11 +13,10 @@ from metricflow_semantics.specs.instance_spec import LinkableInstanceSpec
 from metricflow_semantics.specs.linkable_spec_set import LinkableSpecSet
 from metricflow_semantics.specs.spec_set import InstanceSpecSet
 from metricflow_semantics.sql.sql_bind_parameters import SqlBindParameterSet
-from metricflow_semantics.toolkit.merger import Mergeable
 
 
 @dataclass(frozen=True)
-class WhereFilterSpec(Mergeable, SerializableDataclass):
+class WhereFilterSpec(SerializableDataclass):
     """Similar to the WhereFilter, but with the where_sql_template rendered and used elements extracted.
 
     For example:
@@ -63,32 +61,3 @@ class WhereFilterSpec(Mergeable, SerializableDataclass):
     @cached_property
     def instance_spec_set(self) -> InstanceSpecSet:  # noqa: D102
         return InstanceSpecSet.create_from_specs(self.linkable_specs)
-
-    def merge(self, other: WhereFilterSpec) -> WhereFilterSpec:  # noqa: D102
-        if self == WhereFilterSpec.empty_instance():
-            return other
-
-        if other == WhereFilterSpec.empty_instance():
-            return self
-
-        if self == other:
-            return self
-
-        return WhereFilterSpec(
-            where_sql=f"({self.where_sql}) AND ({other.where_sql})",
-            bind_parameters=self.bind_parameters.merge(other.bind_parameters),
-            element_set=self.element_set.union(other.element_set),
-        )
-
-    @classmethod
-    @override
-    def empty_instance(cls) -> WhereFilterSpec:
-        # Need to revisit making WhereFilterSpec Mergeable as it's current not a collection, and it's odd to return this
-        # no-op filter. Use cases would need to check whether a WhereSpec is a no-op before rendering it to avoid an
-        # un-necessary WHERE clause. Making WhereFilterSpec map to a WhereFilterIntersection would make this more in
-        # line with other cases of Mergeable.
-        return WhereFilterSpec(
-            where_sql="TRUE",
-            bind_parameters=SqlBindParameterSet(),
-            element_set=GroupByItemSet(),
-        )
