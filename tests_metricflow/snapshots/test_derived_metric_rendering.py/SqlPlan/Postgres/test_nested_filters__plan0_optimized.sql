2783e212cd5b062514b2cd065315667384446f21
Paul Yang <github@ardorful.com>
2026-03-06 22:26:40 -0800

    Update snapshots for other SQL engines

--- tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlPlan/Postgres/test_nested_filters__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_derived_metric_rendering.py/SqlPlan/Postgres/test_nested_filters__plan0_optimized.sql
@@ -6,6 +6,18 @@ sql_engine: Postgres
 ---
 -- Compute Metrics via Expressions
 -- Write to DataTable
+WITH sma_28009_cte AS (
+  -- Read Elements From Semantic Model 'bookings_source'
+  -- Metric Time Dimension 'ds'
+  SELECT
+    listing_id AS listing
+    , is_instant AS booking__is_instant
+    , 1 AS __bookings
+    , booking_value AS __average_booking_value
+    , booking_value AS __booking_value
+  FROM ***************************.fct_bookings bookings_source_src_28000
+)
+
 SELECT
   instant_lux_booking_value_rate AS instant_lux_booking_value_rate
 FROM (
@@ -17,7 +29,7 @@ FROM (
     SELECT
       MAX(subq_39.average_booking_value) AS average_booking_value
       , MAX(subq_39.bookings) AS bookings
-      , MAX(subq_46.booking_value) AS booking_value
+      , MAX(subq_45.booking_value) AS booking_value
     FROM (
       -- Constrain Output with WHERE
       -- Select: ['__average_booking_value', '__bookings']
@@ -30,24 +42,15 @@ FROM (
         -- Join Standard Outputs
         -- Select: ['__average_booking_value', '__bookings', 'listing__is_lux_latest', 'booking__is_instant']
         SELECT
-          subq_30.booking__is_instant AS booking__is_instant
+          sma_28009_cte.booking__is_instant AS booking__is_instant
           , listings_latest_src_28000.is_lux AS listing__is_lux_latest
-          , subq_30.__bookings AS bookings
-          , subq_30.__average_booking_value AS average_booking_value
-        FROM (
-          -- Read Elements From Semantic Model 'bookings_source'
-          -- Metric Time Dimension 'ds'
-          SELECT
-            listing_id AS listing
-            , is_instant AS booking__is_instant
-            , 1 AS __bookings
-            , booking_value AS __average_booking_value
-          FROM ***************************.fct_bookings bookings_source_src_28000
-        ) subq_30
+          , sma_28009_cte.__bookings AS bookings
+          , sma_28009_cte.__average_booking_value AS average_booking_value
+        FROM sma_28009_cte
         LEFT OUTER JOIN
           ***************************.dim_listings_latest listings_latest_src_28000
         ON
-          subq_30.listing = listings_latest_src_28000.listing_id
+          sma_28009_cte.listing = listings_latest_src_28000.listing_id
       ) subq_35
       WHERE (listing__is_lux_latest) AND (booking__is_instant)
     ) subq_39
@@ -59,15 +62,14 @@ FROM (
       SELECT
         SUM(booking_value) AS booking_value
       FROM (
-        -- Read Elements From Semantic Model 'bookings_source'
-        -- Metric Time Dimension 'ds'
+        -- Read From CTE For node_id=sma_28009
         -- Select: ['__booking_value', 'booking__is_instant']
         SELECT
-          is_instant AS booking__is_instant
-          , booking_value
-        FROM ***************************.fct_bookings bookings_source_src_28000
-      ) subq_42
+          booking__is_instant
+          , __booking_value AS booking_value
+        FROM sma_28009_cte
+      ) subq_41
       WHERE booking__is_instant
-    ) subq_46
-  ) subq_47
-) subq_48
+    ) subq_45
+  ) subq_46
+) subq_47
--- tests_metricflow/snapshots/test_query_rendering.py/SqlPlan/BigQuery/test_simple_metric_constraint__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_query_rendering.py/SqlPlan/BigQuery/test_simple_metric_constraint__plan0_optimized.sql
@@ -4,16 +4,28 @@ sql_engine: BigQuery
 ---
 -- Compute Metrics via Expressions
 -- Write to DataTable
+WITH sma_28009_cte AS (
+  -- Read Elements From Semantic Model 'bookings_source'
+  -- Metric Time Dimension 'ds'
+  SELECT
+    DATETIME_TRUNC(ds, day) AS metric_time__day
+    , listing_id AS listing
+    , 1 AS __bookings
+    , booking_value AS __average_booking_value
+    , booking_value AS __booking_value
+  FROM ***************************.fct_bookings bookings_source_src_28000
+)
+
 SELECT
-  metric_time__day
+  metric_time__day AS metric_time__day
   , average_booking_value * bookings / NULLIF(booking_value, 0) AS lux_booking_value_rate_expr
 FROM (
   -- Combine Aggregated Outputs
   SELECT
-    COALESCE(subq_37.metric_time__day, subq_43.metric_time__day) AS metric_time__day
+    COALESCE(subq_37.metric_time__day, subq_42.metric_time__day) AS metric_time__day
     , MAX(subq_37.average_booking_value) AS average_booking_value
     , MAX(subq_37.bookings) AS bookings
-    , MAX(subq_43.booking_value) AS booking_value
+    , MAX(subq_42.booking_value) AS booking_value
   FROM (
     -- Constrain Output with WHERE
     -- Select: ['__average_booking_value', '__bookings', 'metric_time__day']
@@ -27,45 +39,35 @@ FROM (
       -- Join Standard Outputs
       -- Select: ['__average_booking_value', '__bookings', 'listing__is_lux_latest', 'metric_time__day']
       SELECT
-        subq_28.metric_time__day AS metric_time__day
+        sma_28009_cte.metric_time__day AS metric_time__day
         , listings_latest_src_28000.is_lux AS listing__is_lux_latest
-        , subq_28.__bookings AS bookings
-        , subq_28.__average_booking_value AS average_booking_value
-      FROM (
-        -- Read Elements From Semantic Model 'bookings_source'
-        -- Metric Time Dimension 'ds'
-        SELECT
-          DATETIME_TRUNC(ds, day) AS metric_time__day
-          , listing_id AS listing
-          , 1 AS __bookings
-          , booking_value AS __average_booking_value
-        FROM ***************************.fct_bookings bookings_source_src_28000
-      ) subq_28
+        , sma_28009_cte.__bookings AS bookings
+        , sma_28009_cte.__average_booking_value AS average_booking_value
+      FROM sma_28009_cte
       LEFT OUTER JOIN
         ***************************.dim_listings_latest listings_latest_src_28000
       ON
-        subq_28.listing = listings_latest_src_28000.listing_id
+        sma_28009_cte.listing = listings_latest_src_28000.listing_id
     ) subq_33
     WHERE listing__is_lux_latest
     GROUP BY
       metric_time__day
   ) subq_37
   FULL OUTER JOIN (
-    -- Read Elements From Semantic Model 'bookings_source'
-    -- Metric Time Dimension 'ds'
+    -- Read From CTE For node_id=sma_28009
     -- Select: ['__booking_value', 'metric_time__day']
     -- Select: ['__booking_value', 'metric_time__day']
     -- Aggregate Inputs for Simple Metrics
     -- Compute Metrics via Expressions
     SELECT
-      DATETIME_TRUNC(ds, day) AS metric_time__day
-      , SUM(booking_value) AS booking_value
-    FROM ***************************.fct_bookings bookings_source_src_28000
+      metric_time__day
+      , SUM(__booking_value) AS booking_value
+    FROM sma_28009_cte
     GROUP BY
       metric_time__day
-  ) subq_43
+  ) subq_42
   ON
-    subq_37.metric_time__day = subq_43.metric_time__day
+    subq_37.metric_time__day = subq_42.metric_time__day
   GROUP BY
     metric_time__day
-) subq_44
+) subq_43
