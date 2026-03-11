48c246f775e9f545e3b37000378c3723368079e8
Paul Yang <github@ardorful.com>
2026-03-11 10:21:13 -0700

    Add test

--- tests_metricflow/plan_conversion/dataflow_to_sql/test_cte_sql.py
+++ tests_metricflow/plan_conversion/dataflow_to_sql/test_cte_sql.py
@@ -4,8 +4,6 @@ import logging
 from typing import FrozenSet, Mapping
 
 from _pytest.fixtures import FixtureRequest
-from metricflow_semantics.query.query_parser import MetricFlowQueryParser
-from metricflow_semantics.specs.column_assoc import ColumnAssociationResolver
 from metricflow_semantics.specs.simple_metric_input_spec import SimpleMetricInputSpec
 from metricflow_semantics.specs.spec_set import InstanceSpecSet
 from metricflow_semantics.test_helpers.config_helpers import MetricFlowTestConfiguration
@@ -15,7 +13,6 @@ from metricflow_semantics.test_helpers.snapshot_helpers import (
 )
 from metricflow_semantics.toolkit.string_helpers import mf_indent
 
-from metricflow.dataflow.builder.dataflow_plan_builder import DataflowPlanBuilder
 from metricflow.dataflow.dataflow_plan import (
     DataflowPlanNode,
 )
@@ -110,26 +107,30 @@ def test_cte_for_simple_dataflow_plan(
     )
 
 
-def test_cte_for_shared_metrics(
+def test_nested_cte(
     request: FixtureRequest,
     mf_test_configuration: MetricFlowTestConfiguration,
-    column_association_resolver: ColumnAssociationResolver,
-    dataflow_plan_builder: DataflowPlanBuilder,
-    query_parser: MetricFlowQueryParser,
-    dataflow_to_sql_converter: DataflowToSqlPlanConverter,
+    mf_engine_test_fixture_mapping: Mapping[SemanticManifestSetup, MetricFlowEngineTestFixture],
 ) -> None:
-    """Check common branches in a query that uses derived metrics defined from metrics that are also in the query."""
-    parse_result = query_parser.parse_and_validate_query(
-        metric_names=("bookings", "views", "bookings_per_view"),
-        group_by_names=("metric_time", "listing__country_latest"),
+    """Check generation of nested CTEs.
+
+    `metric_level_1_index_0` and `metric_level_1_index_1` share input metrics `metric_level_0_index_0` and
+    `metric_level_0_index_1`. This checks that CTEs are generated for `metric_level_0_index_0` and
+    `metric_level_0_index_1`. In addition, a CTE can be generated for `bookings` since it's an input metric to both.
+    """
+    engine_test_fixture = mf_engine_test_fixture_mapping[SemanticManifestSetup.DERIVED_METRICS_MANIFEST]
+
+    parse_result = engine_test_fixture.query_parser.parse_and_validate_query(
+        metric_names=["metric_level_1_index_0", "metric_level_1_index_1"],
+        group_by_names=["metric_time"],
     )
-    dataflow_plan = dataflow_plan_builder.build_plan(parse_result.query_spec)
+    dataflow_plan = engine_test_fixture.dataflow_plan_builder.build_plan(parse_result.query_spec)
     common_nodes = DataflowPlanAnalyzer.find_common_branches(dataflow_plan)
 
     convert_and_check(
         request=request,
         mf_test_configuration=mf_test_configuration,
-        dataflow_to_sql_converter=dataflow_to_sql_converter,
+        dataflow_to_sql_converter=engine_test_fixture.dataflow_to_sql_converter,
         node=dataflow_plan.sink_node,
         nodes_to_convert_to_cte=frozenset(common_nodes),
     )
