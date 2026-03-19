46c44cdb99550e8d398df3d803159e2330a80fa1
Paul Yang <github@ardorful.com>
2026-03-19 15:19:37 -0700

    Update DuckDB snapshots

--- tests_metricflow/snapshots/test_query_rendering.py/SqlPlan/DuckDB/test_multiple_metrics_no_dimensions__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_query_rendering.py/SqlPlan/DuckDB/test_multiple_metrics_no_dimensions__plan0_optimized.sql
@@ -5,8 +5,8 @@ sql_engine: DuckDB
 -- Combine Aggregated Outputs
 -- Write to DataTable
 SELECT
-  MAX(subq_23.bookings) AS bookings
-  , MAX(subq_30.listings) AS listings
+  MAX(subq_25.bookings) AS bookings
+  , MAX(subq_32.listings) AS listings
 FROM (
   -- Read Elements From Semantic Model 'bookings_source'
   -- Metric Time Dimension 'ds'
@@ -19,7 +19,7 @@ FROM (
     SUM(1) AS bookings
   FROM ***************************.fct_bookings bookings_source_src_28000
   WHERE DATE_TRUNC('day', ds) BETWEEN '2020-01-01' AND '2020-01-01'
-) subq_23
+) subq_25
 CROSS JOIN (
   -- Read Elements From Semantic Model 'listings_latest'
   -- Metric Time Dimension 'ds'
@@ -32,4 +32,4 @@ CROSS JOIN (
     SUM(1) AS listings
   FROM ***************************.dim_listings_latest listings_latest_src_28000
   WHERE DATE_TRUNC('day', created_at) BETWEEN '2020-01-01' AND '2020-01-01'
-) subq_30
+) subq_32
