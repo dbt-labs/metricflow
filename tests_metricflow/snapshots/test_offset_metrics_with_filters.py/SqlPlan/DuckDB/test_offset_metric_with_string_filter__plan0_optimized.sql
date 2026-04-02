70377d56372233449af56bf3e16f2585b8a5bc13
Paul Yang <github@ardorful.com>
2026-04-02 12:04:41 -0700

    Update snapshots

--- tests_metricflow/snapshots/test_offset_metrics_with_filters.py/SqlPlan/DuckDB/test_offset_metric_with_string_filter__plan0_optimized.sql
+++ tests_metricflow/snapshots/test_offset_metrics_with_filters.py/SqlPlan/DuckDB/test_offset_metric_with_string_filter__plan0_optimized.sql
@@ -14,46 +14,39 @@ SELECT
   metric_time__day
   , 2 * bookings AS bookings_offset_once
 FROM (
-  -- Join to Time Spine Dataset
+  -- Constrain Output with WHERE
   -- Compute Metrics via Expressions
   SELECT
-    subq_24.metric_time__day AS metric_time__day
-    , subq_19.__bookings AS bookings
+    metric_time__day
+    , bookings
   FROM (
-    -- Constrain Output with WHERE
-    -- Select: ['metric_time__day']
+    -- Join to Time Spine Dataset
     SELECT
-      metric_time__day
-    FROM (
-      -- Read From Time Spine 'mf_time_spine'
-      -- Change Column Aliases
-      -- Select: ['metric_time__day']
-      SELECT
-        ds AS metric_time__day
-      FROM ***************************.mf_time_spine time_spine_src_28006
-    ) subq_22
-    WHERE TRUE
-  ) subq_24
-  INNER JOIN (
-    -- Constrain Output with WHERE
-    -- Select: ['__bookings', 'metric_time__day']
-    -- Aggregate Inputs for Simple Metrics
-    SELECT
-      metric_time__day
-      , SUM(bookings) AS __bookings
-    FROM (
-      -- Read Elements From Semantic Model 'bookings_source'
-      -- Metric Time Dimension 'ds'
+      time_spine_src_28006.ds AS metric_time__day
+      , subq_19.__bookings AS bookings
+    FROM ***************************.mf_time_spine time_spine_src_28006
+    INNER JOIN (
+      -- Constrain Output with WHERE
       -- Select: ['__bookings', 'metric_time__day']
+      -- Aggregate Inputs for Simple Metrics
       SELECT
-        DATE_TRUNC('day', ds) AS metric_time__day
-        , 1 AS bookings
-      FROM ***************************.fct_bookings bookings_source_src_28000
-    ) subq_16
-    WHERE TRUE
-    GROUP BY
-      metric_time__day
-  ) subq_19
-  ON
-    subq_24.metric_time__day - INTERVAL 5 day = subq_19.metric_time__day
+        metric_time__day
+        , SUM(bookings) AS __bookings
+      FROM (
+        -- Read Elements From Semantic Model 'bookings_source'
+        -- Metric Time Dimension 'ds'
+        -- Select: ['__bookings', 'metric_time__day']
+        SELECT
+          DATE_TRUNC('day', ds) AS metric_time__day
+          , 1 AS bookings
+        FROM ***************************.fct_bookings bookings_source_src_28000
+      ) subq_16
+      WHERE TRUE
+      GROUP BY
+        metric_time__day
+    ) subq_19
+    ON
+      time_spine_src_28006.ds - INTERVAL 5 day = subq_19.metric_time__day
+  ) subq_24
+  WHERE TRUE
 ) subq_26
